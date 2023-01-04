4867ed42a0d9e3a10be28154ded88982d177c4c2
Rafael M Mudafort <rafmudaf@gmail.com>
2023-01-04 17:03:42 -0600

    Move the solver out of main and into library

--- src/heat_solve.cpp
+++ src/heat_solve.cpp
@@ -1,21 +1,90 @@
 
 #include "src/heat_solve.H"
+#include "src/utilities/debug_utils.H"
+
 #include <Kokkos_Core.hpp>
 
 namespace openturbine::heat_solve {
 
+
+double* heat_conduction_solver(
+    int axis_size,
+    double side_length,
+    int n_max,
+    double k,
+    double ic_x0,
+    double ic_x1,
+    double residual_tolerance)
+{
+    double dx = side_length/axis_size;                                   // Spatial step size
+    std::vector<double> axis_points = 
+        openturbine::heat_solve::linspace(0.0, side_length, axis_size);  // Spatial grid points for the plate
+    double dt = pow( 1.0 / axis_size, 2) / (2.0 * k);                    // Artificial time step to drive the solver until heat equilibrium
+
+    auto U = static_cast<double*>(std::malloc(axis_size * sizeof(double)));
+    auto U_im1 = static_cast<double*>(std::malloc(axis_size * sizeof(double)));
+    auto deltaU = static_cast<double*>(std::malloc(axis_size * sizeof(double)));
+
+    // Initialize
+    Kokkos::parallel_for( "U_init", axis_size, KOKKOS_LAMBDA ( int i ) {
+        U[ i ] = 0.0;
+    });
+    Kokkos::parallel_for( "U_im1_init", axis_size, KOKKOS_LAMBDA ( int i ) {
+        U_im1[ i ] = 0.0;
+    });
+    Kokkos::parallel_for( "deltaU_init", axis_size, KOKKOS_LAMBDA ( int i ) {
+        deltaU[ i ] = 0.0;
+    });
+
+    // Apply IC
+    U[0] = ic_x0;
+    U[axis_size-1] = ic_x1;
+
+    // Solve
+    for ( int n = 0; n < n_max; n++ ) {
+
+        // Copy values from U to U at i-1
+        for ( int i = 0; i < axis_size; i++) U_im1[i] = U[i];
+
+        deltaU = openturbine::heat_solve::kokkos_laplacian(axis_size, U_im1, dx);
+        U = openturbine::heat_solve::kokkos_1d_heat_conduction(axis_size, U_im1, dt, k, deltaU);
+
+        U[0] = U_im1[0];
+        U[axis_size-1] = U_im1[axis_size-1];
+
+        double residual = openturbine::heat_solve::kokkos_calculate_residual(axis_size, U, U_im1);
+        if (residual < residual_tolerance)
+        {
+            std::cout << "Converged in " << n << " iterations." << std::endl;
+            break;
+        }
+
+        // openturbine::debug::print_array(U, axis_size);
+    }
+
+    // Free memory
+    std::free(U_im1);
+    std::free(deltaU);
+
+    return U;
+}
+
+
 template<typename T>
 std::vector<double> linspace(T start_in, T end_in, int num_in)
 {
-    // Borrowed from @Akavall https://stackoverflow.com/questions/27028226/python-linspace-in-c
+    // Adapted from @Akavall https://stackoverflow.com/questions/27028226/python-linspace-in-c
     std::vector<double> linspaced;
 
     double start = static_cast<double>(start_in);
     double end = static_cast<double>(end_in);
     double num = static_cast<double>(num_in);
 
+    if (num < 0)
+        throw std::invalid_argument("linspace: num_in must be a positive integer");
+
     if (num == 0) { return linspaced; }
-    if (num == 1)  {
+    if (num == 1) {
         linspaced.push_back(start);
         return linspaced;
     }
@@ -30,6 +99,9 @@ std::vector<double> linspace(T start_in, T end_in, int num_in)
 }
 // Generate the template used in main; alternatively, move this implementation the corresponding .H
 template std::vector<double> linspace(double, double, int);
+// template std::vector<double> linspace(int, int, int);
+// template std::vector<double> linspace(int, double, int);
+// template std::vector<double> linspace(double, int, int);
 
 
 double* kokkos_laplacian(int axis_size, double* U, double dx)
@@ -50,6 +122,7 @@ double* kokkos_laplacian(int axis_size, double* U, double dx)
     return laplacian;
 }
 
+
 double* kokkos_1d_heat_conduction(int axis_size, double* U_im1, double dt, double k, double* dU)
 {
     // Heat conduction model in 1d with no time dependence.
@@ -62,6 +135,7 @@ double* kokkos_1d_heat_conduction(int axis_size, double* U_im1, double dt, doubl
     return heat;
 }
 
+
 double kokkos_calculate_residual(int axis_size, double* U, double* U_im1)
 {
     double residual;
@@ -70,5 +144,6 @@ double kokkos_calculate_residual(int axis_size, double* U, double* U_im1)
     },residual);
     return residual;
 }
+
 
 } // namespace openturbine::heat_solve
--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,9 @@
 #include "src/utilities/console_io.H"
 #include "src/utilities/debug_utils.H"
 
-#include <limits>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <sys/time.h>
 
 #include <Kokkos_Core.hpp>
 
@@ -33,72 +31,31 @@ int main( int argc, char* argv[] )
         }
     }
 
-    int axis_size = 10;                                                         // Size of the 1D grid
-    double side_length = 1.0;                                                   // Length of the 1D domain
-    double dx = side_length/axis_size;                                          // Spatial step size
-    std::vector<double> axis_points = 
-        openturbine::heat_solve::linspace(0.0, side_length, axis_size);         // Spatial grid points for the plate
-    double k = 1.011;                                                           // Thermal diffusivity of the material in units of cm^2/s
-    int n_max = 500;                                                            // Max iterations
-    double dt = pow( 1.0 / axis_size, 2) / (2.0 * k);                           // Artificial time step to drive the solver until heat equilibrium
-    double residual_tolerance = 1e-5;                                           // Iterative residual tolerance
+    int axis_size = 10;                // Size of the 1D grid
+    double side_length = 1.0;          // Length of the 1D domain
+    double k = 1.011;                  // Thermal diffusivity of the material in units of cm^2/s
+    double ic_x0 = 100.0;              // Initial temperature at the beginning of the domain
+    double ic_x1 = 100.0;              // Initial temperature at the end of the domain
+    int n_max = 500;                   // Max iterations
+    double residual_tolerance = 1e-5;  // Iterative residual tolerance
+    
+    auto U = static_cast<double*>(std::malloc(axis_size * sizeof(double)));
 
     Kokkos::initialize( argc, argv );
-
-    {
-        auto U = static_cast<double*>(std::malloc(axis_size * sizeof(double)));
-        auto U_im1 = static_cast<double*>(std::malloc(axis_size * sizeof(double)));
-        auto deltaU = static_cast<double*>(std::malloc(axis_size * sizeof(double)));
-        auto residual = static_cast<double*>(std::malloc(axis_size * sizeof(double)));
-
-        // Initialize
-        Kokkos::parallel_for( "U_init", axis_size, KOKKOS_LAMBDA ( int i ) {
-            U[ i ] = 0.0;
-        });
-        Kokkos::parallel_for( "U_im1_init", axis_size, KOKKOS_LAMBDA ( int i ) {
-            U_im1[ i ] = 0.0;
-        });
-        Kokkos::parallel_for( "deltaU_init", axis_size, KOKKOS_LAMBDA ( int i ) {
-            deltaU[ i ] = 0.0;
-        });
-        Kokkos::parallel_for( "deltaU_init", axis_size, KOKKOS_LAMBDA ( int i ) {
-            residual[ i ] = 0.0;
-        });
-
-        // Apply IC
-        U[0] = 100.0;
-        U[axis_size-1] = 100.0;
-
-        // Solve
-        for ( int n = 0; n < n_max; n++ ) {
-
-            // Copy values from U to U at i-1
-            for ( int i = 0; i < axis_size; i++) U_im1[i] = U[i];
-
-            deltaU = openturbine::heat_solve::kokkos_laplacian(axis_size, U_im1, dx);
-            U = openturbine::heat_solve::kokkos_1d_heat_conduction(axis_size, U_im1, dt, k, deltaU);
-
-            U[0] = U_im1[0];
-            U[axis_size-1] = U_im1[axis_size-1];
-
-            double residual = openturbine::heat_solve::kokkos_calculate_residual(axis_size, U, U_im1);
-            if (residual < residual_tolerance)
-            {
-                std::cout << "Converged in " << n << " iterations." << std::endl;
-                break;
-            }
-
-            openturbine::debug::print_array(U, axis_size);
-        }
-
-        // Free memory
-        std::free(U);
-        std::free(U_im1);
-        std::free(deltaU);
-        std::free(residual);
-    }
-
+    openturbine::heat_solve::heat_conduction_solver(
+        axis_size,
+        side_length,
+        n_max,
+        k,
+        ic_x0,
+        ic_x1,
+        residual_tolerance
+    );
     Kokkos::finalize();
+
+    openturbine::debug::print_array(U, axis_size);
+
+    std::free(U);
 
     return 0;
 }
