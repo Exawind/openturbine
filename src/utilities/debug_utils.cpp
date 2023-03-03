d128109c823153deaf3e2b7119b459701eb2bdcf
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2023-03-03 15:47:35 -0600

    Make minor adjustments to debug_utils header and cpp files - Change namespace for debug_utils from openturbine::debug -> openturbine::util - pass argument to print_vector by const &

--- src/utilities/debug_utils.cpp
+++ src/utilities/debug_utils.cpp
@@ -2,30 +2,30 @@
 
 #include "src/utilities/log.h"
 
-namespace openturbine::debug {
+namespace openturbine::util {
 
 void print_array(double* array, int array_len) {
     auto log = Log::Get();
     log->Info("array size: " + std::to_string(array_len) + "\n");
 
-    std::string array_to_print {"array elements:"};
+    std::string array_to_print{"array elements:"};
     for (int i = 0; i < array_len; i++) {
         array_to_print += " " + std::to_string(array[i]);
     }
     log->Info(array_to_print + "\n");
 }
 
-void print_vector(std::vector<double> vec) {
+void print_vector(const std::vector<double>& vec) {
     // Borrowed from @Akavall
     // https://stackoverflow.com/questions/27028226/python-linspace-in-c
     auto log = Log::Get();
     log->Info("vector size: " + std::to_string(vec.size()) + "\n");
 
-    std::string vector_to_print {"vector elements:"};
-    for (double d : vec) {
+    std::string vector_to_print{"vector elements:"};
+    for (const auto& d : vec) {
         vector_to_print += " " + std::to_string(d);
     }
     log->Info(vector_to_print + "\n");
 }
 
-}  // namespace openturbine::debug
+}  // namespace openturbine::util
