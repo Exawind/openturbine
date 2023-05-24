a5c784e0a342e568bc063b99ae0b48d686b23a4d
David Dement <ddement3@gmail.com>
2023-05-24 15:47:12 -0400

    Refactored unit tests to generalize vector creation

--- tests/unit_tests/test_solver.cpp
+++ tests/unit_tests/test_solver.cpp
@@ -10,7 +10,7 @@ namespace openturbine::rigid_pendulum::tests {
 Kokkos::View<double**, Kokkos::DefaultHostExecutionSpace> 
 create_diagonal_matrix(std::vector<double> diagonal)
 {
-  auto matrix = Kokkos::View<double**, Kokkos::DefaultHostExecutionSpace>("identity", diagonal.size(), diagonal.size());
+  auto matrix = Kokkos::View<double**, Kokkos::DefaultHostExecutionSpace>("matrix", diagonal.size(), diagonal.size());
   auto diagonal_entries = Kokkos::RangePolicy<Kokkos::DefaultHostExecutionSpace>(0, diagonal.size());
   auto fill_diagonal = [matrix, diagonal](int index) { matrix(index, index) = diagonal[index]; };
 
@@ -20,15 +20,23 @@ create_diagonal_matrix(std::vector<double> diagonal)
   return matrix;
 }
 
+Kokkos::View<double*, Kokkos::DefaultHostExecutionSpace>
+create_vector(std::vector<double> values)
+{
+  auto vector = Kokkos::View<double*, Kokkos::DefaultHostExecutionSpace>("vector", values.size());
+  auto entries = Kokkos::RangePolicy<Kokkos::DefaultHostExecutionSpace>(0, values.size());
+  auto fill_vector = [vector, values](int index) { vector(index) = values[index]; };
+
+  Kokkos::parallel_for(entries, fill_vector);
+
+  return vector;
+}
+
 TEST(LinearSolverTest, solve_1x1_identity)
 {
   auto identity = create_diagonal_matrix({1});
-  auto solution = Kokkos::View<double*>("solution", 1);
-  auto exactSolution = Kokkos::View<double*>("exact solution", 1);
-
-  solution(0) = 1.;
-  
-  Kokkos::deep_copy(exactSolution, solution);
+  auto solution = create_vector({1});
+  auto exactSolution = create_vector({1});
 
   openturbine::rigid_pendulum::solve_linear_system(identity, solution);
 
@@ -38,12 +46,8 @@ TEST(LinearSolverTest, solve_1x1_identity)
 TEST(LinearSolverTest, solve_3x3_identity)
 {
   auto identity = create_diagonal_matrix({1., 1., 1.});
-  auto solution = Kokkos::View<double*>("solution", 3);
-  auto exactSolution = Kokkos::View<double*>("exact solution", 3);
-
-  solution(0) = 1.;
-  solution(1) = 2.;
-  solution(2) = 3.;
+  auto solution = create_vector({1., 2., 3.});
+  auto exactSolution = create_vector({1., 2., 3.});
   
   Kokkos::deep_copy(exactSolution, solution);
 
@@ -57,12 +61,8 @@ TEST(LinearSolverTest, solve_3x3_identity)
 TEST(LinearSolverTest, solve_1x1_diagonal)
 {
   auto identity = create_diagonal_matrix({2.});
-  auto solution = Kokkos::View<double*>("solution", 3);
-  auto exactSolution = Kokkos::View<double*>("exact solution", 3);
-
-  solution(0) = 1.;
-  
-  exactSolution(0) = .5;
+  auto solution = create_vector({1.});
+  auto exactSolution = create_vector({.5});
 
   openturbine::rigid_pendulum::solve_linear_system(identity, solution);
 
@@ -72,16 +72,8 @@ TEST(LinearSolverTest, solve_1x1_diagonal)
 TEST(LinearSolverTest, solve_3x3_diagonal)
 {
   auto identity = create_diagonal_matrix({2., 8., 32.});
-  auto solution = Kokkos::View<double*>("solution", 3);
-  auto exactSolution = Kokkos::View<double*>("exact solution", 3);
-
-  solution(0) = 1.;
-  solution(1) = 2.;
-  solution(2) = 4.;
-  
-  exactSolution(0) = .5;
-  exactSolution(1) = .25;
-  exactSolution(2) = .125;
+  auto solution = create_vector({1., 2., 4.});
+  auto exactSolution = create_vector({.5, .25, .125});
 
   openturbine::rigid_pendulum::solve_linear_system(identity, solution);
 
