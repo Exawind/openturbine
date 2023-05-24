dffb5a30dbd0b016fb456b1d81758847ac4c964d
David Dement <ddement3@gmail.com>
2023-05-24 15:36:23 -0400

    Added ability to solve diagonal systems

--- src/rigid_pendulum_poc/solver.cpp
+++ src/rigid_pendulum_poc/solver.cpp
@@ -5,7 +5,10 @@ namespace openturbine::rigid_pendulum {
 
 void solve_linear_system([[maybe_unused]] Kokkos::View<double**> system, [[maybe_unused]] Kokkos::View<double*> solution)
 {
-
+  for(unsigned index = 0; index < solution.extent(0); ++index)
+  {
+    solution(index) /= system(index, index);
+  }
 }
 
 }  // namespace openturbine::rigid_pendulum
--- tests/unit_tests/test_solver.cpp
+++ tests/unit_tests/test_solver.cpp
@@ -1,5 +1,6 @@
-#include "gtest/gtest.h"
+#include <limits>
 
+#include <gtest/gtest.h>
 #include <Kokkos_Core.hpp>
 
 #include "src/rigid_pendulum_poc/solver.h"
@@ -10,13 +11,11 @@ Kokkos::View<double**, Kokkos::DefaultHostExecutionSpace>
 create_diagonal_matrix(std::vector<double> diagonal)
 {
   auto matrix = Kokkos::View<double**, Kokkos::DefaultHostExecutionSpace>("identity", diagonal.size(), diagonal.size());
-  Kokkos::deep_copy(matrix, 0);
+  auto diagonal_entries = Kokkos::RangePolicy<Kokkos::DefaultHostExecutionSpace>(0, diagonal.size());
+  auto fill_diagonal = [matrix, diagonal](int index) { matrix(index, index) = diagonal[index]; };
 
-  for(auto& entry : diagonal)
-  {
-    auto index = &entry - &diagonal[0];
-    matrix(index, index) = entry;
-  }
+  Kokkos::deep_copy(matrix, 0);
+  Kokkos::parallel_for(diagonal_entries, fill_diagonal);
 
   return matrix;
 }
@@ -53,6 +52,42 @@ TEST(LinearSolverTest, solve_3x3_identity)
   EXPECT_EQ(solution(0), exactSolution(0));
   EXPECT_EQ(solution(1), exactSolution(1));
   EXPECT_EQ(solution(2), exactSolution(2));
+}
+
+TEST(LinearSolverTest, solve_1x1_diagonal)
+{
+  auto identity = create_diagonal_matrix({2.});
+  auto solution = Kokkos::View<double*>("solution", 3);
+  auto exactSolution = Kokkos::View<double*>("exact solution", 3);
+
+  solution(0) = 1.;
+  
+  exactSolution(0) = .5;
+
+  openturbine::rigid_pendulum::solve_linear_system(identity, solution);
+
+  EXPECT_NEAR(solution(0), exactSolution(0), std::numeric_limits<double>::epsilon());
+}
+
+TEST(LinearSolverTest, solve_3x3_diagonal)
+{
+  auto identity = create_diagonal_matrix({2., 8., 32.});
+  auto solution = Kokkos::View<double*>("solution", 3);
+  auto exactSolution = Kokkos::View<double*>("exact solution", 3);
+
+  solution(0) = 1.;
+  solution(1) = 2.;
+  solution(2) = 4.;
+  
+  exactSolution(0) = .5;
+  exactSolution(1) = .25;
+  exactSolution(2) = .125;
+
+  openturbine::rigid_pendulum::solve_linear_system(identity, solution);
+
+  EXPECT_NEAR(solution(0), exactSolution(0), std::numeric_limits<double>::epsilon());
+  EXPECT_NEAR(solution(1), exactSolution(1), std::numeric_limits<double>::epsilon());
+  EXPECT_NEAR(solution(2), exactSolution(2), std::numeric_limits<double>::epsilon());
 }
 
 }  // namespace oturb_tests
