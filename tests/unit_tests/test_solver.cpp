a9603f30bdf9a45ee6b4b0ca2b8425a98ed77a47
David Dement <ddement3@gmail.com>
2023-05-24 14:09:31 -0400

    Added an additional test for 3x3 identity matrix

--- tests/unit_tests/test_solver.cpp
+++ tests/unit_tests/test_solver.cpp
@@ -22,5 +22,29 @@ TEST(LinearSolverTest, solve_1x1_identity)
   EXPECT_EQ(solution(0), exactSolution(0));
 }
 
+TEST(LinearSolverTest, solve_3x3_identity)
+{
+  auto identity = Kokkos::View<double**>("identity", 3, 3);
+  auto solution = Kokkos::View<double*>("solution", 3);
+  auto exactSolution = Kokkos::View<double*>("exact solution", 3);
+
+  Kokkos::deep_copy(identity, 0);
+  identity(0, 0) = 1.;
+  identity(1, 1) = 1.;
+  identity(2, 2) = 1.;
+
+  solution(0) = 1.;
+  solution(1) = 2.;
+  solution(2) = 3.;
+  
+  Kokkos::deep_copy(exactSolution, solution);
+
+  openturbine::rigid_pendulum::solve_linear_system(identity, solution);
+
+  EXPECT_EQ(solution(0), exactSolution(0));
+  EXPECT_EQ(solution(1), exactSolution(1));
+  EXPECT_EQ(solution(2), exactSolution(2));
+}
+
 }  // namespace oturb_tests
 
