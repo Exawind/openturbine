72c2364dfe46ed9b4f0fd58d837b7e945702e8b9
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2023-07-10 17:22:25 -0500

    Fix compile errors in Linux

--- src/rigid_pendulum_poc/quaternion.h
+++ src/rigid_pendulum_poc/quaternion.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <array>
+#include <cmath>
+#include <stdexcept>
 
 namespace openturbine::rigid_pendulum {
 
--- tests/unit_tests/rigid_pendulum_poc/test_quaternions.cpp
+++ tests/unit_tests/rigid_pendulum_poc/test_quaternions.cpp
@@ -4,8 +4,6 @@
 
 namespace openturbine::rigid_pendulum::tests {
 
-using namespace openturbine::rigid_pendulum;
-
 TEST(QuaternionTest, DefaultConstructor) {
     Quaternion q;
     std::array<double, 4> expected = {0., 0., 0., 0.};
@@ -30,7 +28,7 @@ TEST(QuaternionTest, ScalarVectorConstructor) {
 }
 
 TEST(QuaternionTest, IndexOperator) {
-    Quaternion q(std::array{1., 2., 3., 4.});
+    Quaternion q(std::array<double, 4>{1., 2., 3., 4.});
 
     ASSERT_EQ(q[0], 1.);
     ASSERT_EQ(q[1], 2.);
@@ -41,19 +39,19 @@ TEST(QuaternionTest, IndexOperator) {
 }
 
 TEST(QuaternionTest, Length) {
-    Quaternion q(std::array{1.0, 2.0, 3.0, 4.0});
+    Quaternion q(std::array<double, 4>{1.0, 2.0, 3.0, 4.0});
 
     ASSERT_DOUBLE_EQ(q.Length(), std::sqrt(30.0));
 }
 
 TEST(QuaternionTest, GetScalarComponent) {
-    Quaternion q(std::array{1., 2., 3., 4.});
+    Quaternion q(std::array<double, 4>{1., 2., 3., 4.});
 
     ASSERT_EQ(q.GetScalarComponent(), 1.);
 }
 
 TEST(QuaternionTest, GetVectorComponent) {
-    Quaternion q(std::array{1., 2., 3., 4.});
+    Quaternion q(std::array<double, 4>{1., 2., 3., 4.});
     std::array<double, 3> expected = {2., 3., 4.};
 
     ASSERT_EQ(q.GetVectorComponent(), expected);
@@ -87,84 +85,84 @@ TEST(QuaternionTest, CloseTo) {
 }
 
 TEST(QuaternionTest, AdditionOfTwoQuaternions) {
-    Quaternion q1(std::array{1., 2., 3., 4.});
-    Quaternion q2(std::array{5., 6., 7., 8.});
-    Quaternion expected(std::array{6., 8., 10., 12.});
+    Quaternion q1(std::array<double, 4>{1., 2., 3., 4.});
+    Quaternion q2(std::array<double, 4>{5., 6., 7., 8.});
+    Quaternion expected(std::array<double, 4>{6., 8., 10., 12.});
 
     ASSERT_EQ((q1 + q2).GetComponents(), expected.GetComponents());
 }
 
 TEST(QuaternionTest, AdditionOfThreeQuaternions) {
-    Quaternion q1(std::array{1., 2., 3., 4.});
-    Quaternion q2(std::array{5., 6., 7., 8.});
-    Quaternion q3(std::array{9., 10., 11., 12.});
-    Quaternion expected(std::array{15., 18., 21., 24.});
+    Quaternion q1(std::array<double, 4>{1., 2., 3., 4.});
+    Quaternion q2(std::array<double, 4>{5., 6., 7., 8.});
+    Quaternion q3(std::array<double, 4>{9., 10., 11., 12.});
+    Quaternion expected(std::array<double, 4>{15., 18., 21., 24.});
 
     ASSERT_EQ((q1 + q2 + q3).GetComponents(), expected.GetComponents());
 }
 
 TEST(QuaternionTest, SubtractionOfTwoQuaternions) {
-    Quaternion q1(std::array{1., 2., 3., 4.});
-    Quaternion q2(std::array{5., 6., 7., 8.});
-    Quaternion expected(std::array{-4., -4., -4., -4.});
+    Quaternion q1(std::array<double, 4>{1., 2., 3., 4.});
+    Quaternion q2(std::array<double, 4>{5., 6., 7., 8.});
+    Quaternion expected(std::array<double, 4>{-4., -4., -4., -4.});
 
     ASSERT_EQ((q1 - q2).GetComponents(), expected.GetComponents());
 }
 
 TEST(QuaternionTest, AdditionAndSubtractionOfThreeQuaternions) {
-    Quaternion q1(std::array{1., 2., 3., 4.});
-    Quaternion q2(std::array{5., 6., 7., 8.});
-    Quaternion q3(std::array{9., 10., 11., 12.});
-    Quaternion expected(std::array{-3., -2., -1., 0.});
+    Quaternion q1(std::array<double, 4>{1., 2., 3., 4.});
+    Quaternion q2(std::array<double, 4>{5., 6., 7., 8.});
+    Quaternion q3(std::array<double, 4>{9., 10., 11., 12.});
+    Quaternion expected(std::array<double, 4>{-3., -2., -1., 0.});
 
     ASSERT_EQ((q1 + q2 - q3).GetComponents(), expected.GetComponents());
 }
 
 TEST(QuaternionTest, MultiplicationOfTwoQuaternions_1) {
-    Quaternion q1(std::array{3., 1., -2., 1.});
-    Quaternion q2(std::array{2., -1., 2., 3.});
-    Quaternion expected(std::array{8., -9., -2., 11.});
+    Quaternion q1(std::array<double, 4>{3., 1., -2., 1.});
+    Quaternion q2(std::array<double, 4>{2., -1., 2., 3.});
+    Quaternion expected(std::array<double, 4>{8., -9., -2., 11.});
 
     ASSERT_EQ((q1 * q2).GetComponents(), expected.GetComponents());
 }
 
 TEST(QuaternionTest, MultiplicationOfTwoQuaternions_2) {
-    Quaternion q1(std::array{1., 2., 3., 4.});
-    Quaternion q2(std::array{5., 6., 7., 8.});
-    Quaternion expected(std::array{-60., 12., 30., 24.});
+    Quaternion q1(std::array<double, 4>{1., 2., 3., 4.});
+    Quaternion q2(std::array<double, 4>{5., 6., 7., 8.});
+    Quaternion expected(std::array<double, 4>{-60., 12., 30., 24.});
 
     ASSERT_EQ((q1 * q2).GetComponents(), expected.GetComponents());
 }
 
 TEST(QuaternionTest, MultiplicationOfQuaternionAndScalar) {
-    Quaternion q(std::array{1., 2., 3., 4.});
-    Quaternion expected(std::array{2., 4., 6., 8.});
+    Quaternion q(std::array<double, 4>{1., 2., 3., 4.});
+    Quaternion expected(std::array<double, 4>{2., 4., 6., 8.});
 
     ASSERT_EQ((q * 2.).GetComponents(), expected.GetComponents());
 }
 
 TEST(QuaternionTest, DivisionOfQuaternionAndScalar) {
-    Quaternion q(std::array{1., 2., 3., 4.});
-    Quaternion expected(std::array{0.5, 1., 1.5, 2.});
+    Quaternion q(std::array<double, 4>{1., 2., 3., 4.});
+    Quaternion expected(std::array<double, 4>{0.5, 1., 1.5, 2.});
 
     ASSERT_EQ((q / 2.).GetComponents(), expected.GetComponents());
 }
 
 TEST(QuaternionTest, ExpectNonUnitQuaternion) {
-    Quaternion q(std::array{1., 2., 3., 4.});
+    Quaternion q(std::array<double, 4>{1., 2., 3., 4.});
 
     ASSERT_FALSE(q.IsUnitQuaternion());
 }
 
 TEST(QuaternionTest, ExpectUnitQuaternion) {
     double l = std::sqrt(30.);
-    Quaternion q(std::array{1. / l, 2. / l, 3. / l, 4. / l});
+    Quaternion q(std::array<double, 4>{1. / l, 2. / l, 3. / l, 4. / l});
 
     ASSERT_TRUE(q.IsUnitQuaternion());
 }
 
 TEST(QuaternionTest, GetUnitQuaternion) {
-    Quaternion q(std::array{1., 2., 3., 4.});
+    Quaternion q(std::array<double, 4>{1., 2., 3., 4.});
     Quaternion expected(q / std::sqrt(30.));
 
     ASSERT_EQ(q.GetUnitQuaternion().GetComponents(), expected.GetComponents());
@@ -172,15 +170,15 @@ TEST(QuaternionTest, GetUnitQuaternion) {
 }
 
 TEST(QuaternionTest, GetConjugate) {
-    Quaternion q(std::array{1., 2., 3., 4.});
-    Quaternion expected(std::array{1., -2., -3., -4.});
+    Quaternion q(std::array<double, 4>{1., 2., 3., 4.});
+    Quaternion expected(std::array<double, 4>{1., -2., -3., -4.});
 
     ASSERT_EQ(q.GetConjugate().GetComponents(), expected.GetComponents());
 }
 
 TEST(QuaternionTest, GetInverse) {
-    Quaternion q(std::array{1., 2., 3., 4.});
-    Quaternion expected(std::array{1. / 30., -2. / 30., -3. / 30., -4. / 30.});
+    Quaternion q(std::array<double, 4>{1., 2., 3., 4.});
+    Quaternion expected(std::array<double, 4>{1. / 30., -2. / 30., -3. / 30., -4. / 30.});
 
     ASSERT_EQ(q.GetInverse().GetComponents(), expected.GetComponents());
     ASSERT_TRUE((q * q.GetInverse()).IsUnitQuaternion());
@@ -208,7 +206,7 @@ Quaternion quaternion_from_rotation_vector(const std::array<double, 3>& rotation
 
 TEST(QuaternionTest, GetQuaternionFromRotationVector_Set1) {
     std::array<double, 3> rotation_vector{1., 2., 3.};
-    Quaternion expected(std::array{-0.295551, 0.255322, 0.510644, 0.765966});
+    Quaternion expected(std::array<double, 4>{-0.295551, 0.255322, 0.510644, 0.765966});
 
     ASSERT_NEAR(
         quaternion_from_rotation_vector(rotation_vector).GetComponents()[0],
@@ -229,7 +227,7 @@ TEST(QuaternionTest, GetQuaternionFromRotationVector_Set1) {
 }
 
 TEST(QuaternionTest, GetRotationVectorFromQuaternion_Set1) {
-    Quaternion q(std::array{-0.295551, 0.255322, 0.510644, 0.765966});
+    Quaternion q(std::array<double, 4>{-0.295551, 0.255322, 0.510644, 0.765966});
     std::array<double, 3> expected{1., 2., 3.};
 
     ASSERT_NEAR(rotation_vector_from_quaternion(q)[0], expected[0], 1e-6);
@@ -239,7 +237,7 @@ TEST(QuaternionTest, GetRotationVectorFromQuaternion_Set1) {
 
 TEST(QuaternionTest, GetQuaternionFromRotationVector_Set2) {
     std::array<double, 3> rotation_vector{0.0, 0.0, 1.570796};
-    Quaternion expected(std::array{0.707107, 0., 0., 0.707107});
+    Quaternion expected(std::array<double, 4>{0.707107, 0., 0., 0.707107});
 
     ASSERT_NEAR(
         quaternion_from_rotation_vector(rotation_vector).GetComponents()[0],
@@ -260,7 +258,7 @@ TEST(QuaternionTest, GetQuaternionFromRotationVector_Set2) {
 }
 
 TEST(QuaternionTest, GetRotationVectorFromQuaternion_Set2) {
-    Quaternion q(std::array{0.707107, 0., 0., 0.707107});
+    Quaternion q(std::array<double, 4>{0.707107, 0., 0., 0.707107});
     std::array<double, 3> expected{0.0, 0.0, 1.570796};
 
     ASSERT_NEAR(rotation_vector_from_quaternion(q)[0], expected[0], 1e-6);
@@ -270,7 +268,7 @@ TEST(QuaternionTest, GetRotationVectorFromQuaternion_Set2) {
 
 TEST(QuaternionTest, GetQuaternionFromNullRotationVector) {
     std::array<double, 3> rotation_vector{0., 0., 0.};
-    Quaternion expected(std::array{1., 0., 0., 0.});
+    Quaternion expected(std::array<double, 4>{1., 0., 0., 0.});
 
     ASSERT_EQ(
         quaternion_from_rotation_vector(rotation_vector).GetComponents(), expected.GetComponents()
@@ -278,7 +276,7 @@ TEST(QuaternionTest, GetQuaternionFromNullRotationVector) {
 }
 
 TEST(QuaternionTest, GetRotationVectorFromNullQuaternion) {
-    Quaternion q(std::array{0., 0., 0., 0.});
+    Quaternion q(std::array<double, 4>{0., 0., 0., 0.});
     std::array<double, 3> expected{0., 0., 0.};
 
     ASSERT_EQ(rotation_vector_from_quaternion(q), expected);
