d797677c0de2cdff143dc06360ced3425fa0a6a8
Dement <ddement3@ae-artl-408101.ae.gatech.edu>
2023-09-29 12:43:29 -0400

    Added consistency checks for common invalid connectivities.
    These checks aren't exhaustive, but they're a start.  Caution should
    still be used when building a mesh.
    
    All mesh construction functions are now protected.  They can only
    be used from inside a friended scope or by intentionally circumventing
    these protections.

--- src/gebt_poc/mesh.h
+++ src/gebt_poc/mesh.h
@@ -9,13 +9,6 @@ public:
 
     int GetNumberOfNodes() { return number_of_nodes_; }
 
-    void SetNumberOfElements(int number_of_elements) { number_of_elements_ = number_of_elements; }
-    void SetNumberOfNodes(int number_of_nodes) { number_of_nodes_ = number_of_nodes; }
-
-    void InitializeElementNodeConnectivity(int number_of_elements, int nodes_per_element) {
-        connectivity_ = Kokkos::View<int**>("connectivity", number_of_elements, nodes_per_element);
-    }
-
     KOKKOS_FUNCTION
     Kokkos::View<const int*> GetNodesForElement(int elementID) {
         return Kokkos::subview(connectivity_, elementID, Kokkos::ALL);
@@ -24,6 +17,63 @@ public:
     friend Mesh create1DMesh(int number_of_elements, int nodes_per_element);
 
 protected:
+    void SetNumberOfElements(int number_of_elements) { number_of_elements_ = number_of_elements; }
+    void SetNumberOfNodes(int number_of_nodes) { number_of_nodes_ = number_of_nodes; }
+
+    void InitializeElementNodeConnectivity(int number_of_elements, int nodes_per_element) {
+        connectivity_ = Kokkos::View<int**>("connectivity", number_of_elements, nodes_per_element);
+    }
+
+    void CheckElementConsistency() {
+        if (static_cast<unsigned>(number_of_elements_) != connectivity_.extent(0)) {
+            throw std::domain_error("Connectivity does not contain expected number of elements");
+        }
+    }
+
+    void CheckNodeIDConsistency() {
+        auto range_policy = Kokkos::MDRangePolicy<Kokkos::Rank<2>>(
+            {0, 0}, {connectivity_.extent(0), connectivity_.extent(1)}
+        );
+        int max_node_id;
+        Kokkos::parallel_reduce(
+            range_policy,
+            KOKKOS_LAMBDA(int i, int j, int& local_max) {
+                if (connectivity_(i, j) > local_max) {
+                    local_max = connectivity_(i, j);
+                }
+            },
+            Kokkos::Max<int>(max_node_id)
+        );
+
+        if (max_node_id >= number_of_nodes_) {
+            throw std::domain_error("Connectivity references node ID above the expected range");
+        }
+    }
+
+    void CheckNodeUniquenessConsistency() {
+        for (std::size_t element = 0; element < connectivity_.extent(0); ++element) {
+            auto node_list = GetNodesForElement(element);
+            auto host_node_list = Kokkos::create_mirror(node_list);
+            Kokkos::deep_copy(host_node_list, node_list);
+            auto node_list_vector = std::vector<int>(host_node_list.extent(0));
+            for (std::size_t node = 0; node < node_list_vector.size(); ++node) {
+                node_list_vector[node] = host_node_list(node);
+            }
+            std::sort(std::begin(node_list_vector), std::end(node_list_vector));
+            auto first_duplicate =
+                std::adjacent_find(std::begin(node_list_vector), std::end(node_list_vector));
+            if (first_duplicate != end(node_list_vector)) {
+                throw std::domain_error("Nodes within an element must be unique");
+            }
+        }
+    }
+
+    void CheckConsistency() {
+        CheckElementConsistency();
+        CheckNodeIDConsistency();
+        CheckNodeUniquenessConsistency();
+    }
+
     int number_of_elements_;
     int number_of_nodes_;
     Kokkos::View<int**> connectivity_;
@@ -42,6 +92,8 @@ inline Mesh create1DMesh(int number_of_elements, int nodes_per_element) {
     };
 
     Kokkos::parallel_for(number_of_elements, set_element_connectivity);
+
+    mesh.CheckConsistency();
     return mesh;
 }
 }  // namespace openturbine
--- tests/unit_tests/gebt_poc/test_mesh.cpp
+++ tests/unit_tests/gebt_poc/test_mesh.cpp
@@ -116,3 +116,149 @@ TEST(MeshTest, Create1DMesh_3Element_3Node) {
         EXPECT_EQ(hostNodeList(2), 6);
     }
 }
+
+namespace openturbine::impl {
+class TestMesh : public openturbine::Mesh {
+public:
+    void TestSetNumberOfElements(int number_of_elements) { SetNumberOfElements(number_of_elements); }
+    void TestSetNumberOfNodes(int number_of_nodes) { SetNumberOfNodes(number_of_nodes); }
+
+    void TestInitializeConnectivity(int number_of_elements, int nodes_per_element) {
+        InitializeElementNodeConnectivity(number_of_elements, nodes_per_element);
+    }
+
+    void TestCheckConsistency() { CheckConsistency(); }
+
+    Kokkos::View<int**> TestGetConnectivity() { return connectivity_; }
+};
+}  // namespace openturbine::impl
+
+TEST(MeshTest, CheckConsistency_WrongNumberOfElements) {
+    openturbine::impl::TestMesh mesh;
+
+    mesh.TestSetNumberOfElements(2);
+    mesh.TestSetNumberOfNodes(5);
+
+    mesh.TestInitializeConnectivity(1, 5);
+
+    EXPECT_THROW(mesh.TestCheckConsistency(), std::domain_error);
+}
+
+TEST(MeshTest, CheckConsistency_InvalidNode) {
+    openturbine::impl::TestMesh mesh;
+
+    mesh.TestSetNumberOfElements(1);
+    mesh.TestSetNumberOfNodes(2);
+
+    mesh.TestInitializeConnectivity(1, 2);
+
+    auto connectivity = mesh.TestGetConnectivity();
+    Kokkos::parallel_for(
+        1,
+        KOKKOS_LAMBDA(int) {
+            connectivity(0, 0) = 0;
+            connectivity(0, 1) = 2;
+        }
+    );
+
+    EXPECT_THROW(mesh.TestCheckConsistency(), std::domain_error);
+}
+
+TEST(MeshTest, CheckConsistency_NonUniqueNode) {
+    openturbine::impl::TestMesh mesh;
+
+    mesh.TestSetNumberOfElements(1);
+    mesh.TestSetNumberOfNodes(3);
+
+    mesh.TestInitializeConnectivity(1, 3);
+
+    auto connectivity = mesh.TestGetConnectivity();
+    Kokkos::parallel_for(
+        1,
+        KOKKOS_LAMBDA(int) {
+            connectivity(0, 0) = 0;
+            connectivity(0, 1) = 1;
+            connectivity(0, 2) = 0;
+        }
+    );
+
+    EXPECT_THROW(mesh.TestCheckConsistency(), std::domain_error);
+}
+
+TEST(MeshTest, CheckConsistency_NonUniqueNode_2Element) {
+    openturbine::impl::TestMesh mesh;
+
+    mesh.TestSetNumberOfElements(2);
+    mesh.TestSetNumberOfNodes(5);
+
+    mesh.TestInitializeConnectivity(2, 3);
+
+    auto connectivity = mesh.TestGetConnectivity();
+    Kokkos::parallel_for(
+        1,
+        KOKKOS_LAMBDA(int) {
+            connectivity(0, 0) = 0;
+            connectivity(0, 1) = 1;
+            connectivity(0, 2) = 2;
+            connectivity(1, 0) = 2;
+            connectivity(1, 1) = 3;
+            connectivity(1, 2) = 3;
+        }
+    );
+
+    EXPECT_THROW(mesh.TestCheckConsistency(), std::domain_error);
+}
+
+TEST(MeshTest, CheckConsistency_Pass_Bar) {
+    openturbine::impl::TestMesh mesh;
+
+    mesh.TestSetNumberOfElements(2);
+    mesh.TestSetNumberOfNodes(5);
+
+    mesh.TestInitializeConnectivity(2, 3);
+
+    auto connectivity = mesh.TestGetConnectivity();
+    Kokkos::parallel_for(
+        1,
+        KOKKOS_LAMBDA(int) {
+            connectivity(0, 0) = 0;
+            connectivity(0, 1) = 1;
+            connectivity(0, 2) = 2;
+            connectivity(1, 0) = 2;
+            connectivity(1, 1) = 3;
+            connectivity(1, 2) = 4;
+        }
+    );
+
+    EXPECT_NO_THROW(mesh.TestCheckConsistency());
+}
+
+TEST(MeshTest, CheckConsistency_Pass_Plus) {
+    openturbine::impl::TestMesh mesh;
+
+    mesh.TestSetNumberOfElements(4);
+    mesh.TestSetNumberOfNodes(9);
+
+    mesh.TestInitializeConnectivity(4, 3);
+
+    auto connectivity = mesh.TestGetConnectivity();
+    Kokkos::parallel_for(
+        1,
+        KOKKOS_LAMBDA(int) {
+            connectivity(0, 0) = 0;
+            connectivity(0, 1) = 1;
+            connectivity(0, 2) = 2;
+            connectivity(1, 0) = 2;
+            connectivity(1, 1) = 3;
+            connectivity(1, 2) = 4;
+            connectivity(2, 0) = 2;
+            connectivity(2, 1) = 5;
+            connectivity(2, 2) = 6;
+            connectivity(3, 0) = 2;
+            connectivity(3, 1) = 7;
+            connectivity(3, 2) = 8;
+        }
+    );
+
+    EXPECT_NO_THROW(mesh.TestCheckConsistency());
+}
