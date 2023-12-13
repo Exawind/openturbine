cec225cb8107e90f552c547b3a6aeeaec66a77a6
dcdemen <dcdemen@sandia.gov>
2023-12-13 10:41:39 -0700

    Changed to use batched UTV solver instead of LAPACK.  This solve runs on GPU in "batched" mode.

--- src/gebt_poc/linear_solver.h
+++ src/gebt_poc/linear_solver.h
@@ -3,8 +3,10 @@
 #include <stdexcept>
 
 #include <KokkosBlas.hpp>
-
-#include "KokkosLapack_gesv.hpp"
+#include <KokkosLapack_gesv.hpp>
+#include <KokkosBatched_QR_WithColumnPivoting_Decl.hpp>
+#include <KokkosBatched_SolveUTV_Decl.hpp>
+#include <KokkosBatched_UTV_Decl.hpp>
 
 #include "src/utilities/log.h"
 
@@ -13,21 +15,51 @@ namespace openturbine::gebt_poc {
 static constexpr double kConvergenceTolerance = 1e-12;
 
 inline void solve_linear_system(Kokkos::View<double**> system, Kokkos::View<double*> solution) {
-    auto A =
-        Kokkos::View<double**, Kokkos::LayoutLeft>("system", system.extent(0), system.extent(1));
+    // Define the complex templated types used in the solver
+    using member_type = Kokkos::TeamPolicy<>::member_type;
+    using decompose = KokkosBatched::TeamVectorUTV<member_type, KokkosBatched::Algo::UTV::Unblocked>;
+    using solve = KokkosBatched::TeamVectorSolveUTV<member_type, KokkosBatched::Algo::UTV::Unblocked>;
+    using scratch_space = Kokkos::DefaultExecutionSpace::scratch_memory_space;
+    using unmanaged_memory = Kokkos::MemoryTraits<Kokkos::Unmanaged>;
+    using matrix = Kokkos::View<double**, scratch_space, unmanaged_memory>;
+    using vector = Kokkos::View<double*, scratch_space, unmanaged_memory>;
+    using scalar_vector = Kokkos::View<int*, scratch_space, unmanaged_memory>;
+    
+    // Create local copies of the system and solution to be used in the solver
+    auto A = Kokkos::View<double**>("A", system.extent(0), system.extent(1));
     Kokkos::deep_copy(A, system);
-    auto A_host = Kokkos::create_mirror(A);
-    Kokkos::deep_copy(A_host, A);
-    auto b = Kokkos::View<double*, Kokkos::LayoutLeft>("solution", solution.extent(0));
+    auto x = Kokkos::View<double*>("x", solution.extent(0));
+    auto b = Kokkos::View<double*>("b", solution.extent(0));
     Kokkos::deep_copy(b, solution);
-    auto b_host = Kokkos::create_mirror(b);
-    Kokkos::deep_copy(b_host, b);
-    auto pivots =
-        Kokkos::View<int*, Kokkos::LayoutLeft, Kokkos::HostSpace>("pivots", solution.extent(0));
 
-    KokkosLapack::gesv(A_host, b_host, pivots);
-    Kokkos::deep_copy(A, A_host);
-    Kokkos::deep_copy(b, b_host);
+    // Setup the scratch space needed for the solver
+    auto policy = Kokkos::TeamPolicy<>(1, Kokkos::AUTO(), Kokkos::AUTO());
+    auto n = A.extent(0);
+    auto scratch_size = 2 * matrix::shmem_size(n, n) + vector::shmem_size(3*n) + scalar_vector::shmem_size(n);
+    policy.set_scratch_size(0, Kokkos::PerTeam(scratch_size));
+
+    // Solve the system using UTV decomposition
+    Kokkos::parallel_for(policy, KOKKOS_LAMBDA(const member_type& member) {
+      int rank = 0;
+      auto U = matrix(member.team_scratch(0), n, n);
+      auto T = A;
+      auto V = matrix(member.team_scratch(0), n, n);
+      auto p = scalar_vector(member.team_scratch(0), n);
+      auto w = vector(member.team_scratch(0), 3*n);
+
+      if(decompose::invoke(member, A, p, U, V, w, rank)) {
+        Kokkos::abort("Decomposition Failed");
+      }
+      member.team_barrier();
+      if(solve::invoke(member, rank, U, T, V, p, x, b, w)) {
+        Kokkos::abort("Solve Failed");
+      }
+      member.team_barrier();
+    });
+    Kokkos::fence();
+
+    // Copy the solution into b to maintain consistency with debug checks
+    Kokkos::deep_copy(b, x);
 
     // Here we add some safety checks to make sure the returned solution and/or the provided
     // linear system is valid
