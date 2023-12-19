1d26d16103e929c91e6527cd902651495b4bec4c
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2023-12-18 21:15:10 -0700

    Add a method to calculate the elemental mass matrix for a beam element

--- src/gebt_poc/solver.h
+++ src/gebt_poc/solver.h
@@ -114,6 +114,12 @@ void NodalDynamicStiffnessMatrix(
     const MassMatrix& sectional_mass_matrix, Kokkos::View<double**> iteration_matrix
 );
 
+void ElementalMassMatrix(
+    const Kokkos::View<double*> position_vectors, const Kokkos::View<double*> gen_coords,
+    const MassMatrix& mass_matrix, const Quadrature& quadrature,
+    Kokkos::View<double**> element_mass_matrix
+);
+
 /// Calculates the constraint residual vector for a beam element
 void ElementalConstraintForcesResidual(
     const Kokkos::View<double*> gen_coords, const Kokkos::View<double*> constraints_residual
