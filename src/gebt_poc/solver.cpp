add8ffdda17a4607d14164b0f254bdda84bb08ca
dcdemen <dcdemen@sandia.gov>
2024-03-05 12:33:53 -0700

    Split out InterpolateNodalValueDerivatives

--- src/gebt_poc/solver.cpp
+++ src/gebt_poc/solver.cpp
@@ -6,46 +6,9 @@
 #include "src/gen_alpha_poc/quaternion.h"
 #include "src/gen_alpha_poc/utilities.h"
 #include "src/gebt_poc/InterpolateNodalValues.hpp"
+#include "src/gebt_poc/InterpolateNodalValueDerivatives.hpp"
 
 namespace openturbine::gebt_poc {
-
-void InterpolateNodalValueDerivatives(
-    View1D::const_type nodal_values, std::vector<double> interpolation_function, double jacobian,
-    View1D interpolated_values
-) {
-    if (jacobian == 0.) {
-        throw std::invalid_argument("jacobian must be nonzero");
-    }
-    const auto n_nodes = nodal_values.extent(0) / LieGroupComponents;
-    KokkosBlas::fill(interpolated_values, 0.);
-    for (std::size_t i = 0; i < n_nodes; ++i) {
-        auto index = i * LieGroupComponents;
-        KokkosBlas::axpy(
-            interpolation_function[i],
-            Kokkos::subview(nodal_values, Kokkos::pair(index, index + LieGroupComponents)),
-            interpolated_values
-        );
-    }
-    KokkosBlas::scal(interpolated_values, 1. / jacobian, interpolated_values);
-}
-
-void InterpolateNodalValueDerivatives(
-    View2D::const_type nodal_values, std::vector<double> interpolation_function, double jacobian,
-    View1D interpolated_values
-) {
-    if (jacobian == 0.) {
-        throw std::invalid_argument("jacobian must be nonzero");
-    }
-    const auto n_nodes = nodal_values.extent(0);
-    KokkosBlas::fill(interpolated_values, 0.);
-    for (std::size_t i = 0; i < n_nodes; ++i) {
-        KokkosBlas::axpy(
-            interpolation_function[i], Kokkos::subview(nodal_values, i, Kokkos::ALL),
-            interpolated_values
-        );
-    }
-    KokkosBlas::scal(interpolated_values, 1. / jacobian, interpolated_values);
-}
 
 void NodalCurvature(
     View1D_LieGroup::const_type gen_coords, View1D_LieGroup::const_type gen_coords_derivative,
