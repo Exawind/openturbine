ef0c4879da3c3b47b8ac125a5c4cfaa863773148
dcdemen <dcdemen@sandia.gov>
2024-03-05 13:10:01 -0700

    Split out SectionalStiffness

--- src/gebt_poc/solver.cpp
+++ src/gebt_poc/solver.cpp
@@ -9,39 +9,9 @@
 #include "src/gebt_poc/InterpolateNodalValueDerivatives.hpp"
 #include "src/gebt_poc/NodalCurvature.hpp"
 #include "src/gebt_poc/CalculateSectionalStrain.hpp"
+#include "src/gebt_poc/SectionalStiffness.hpp"
 
 namespace openturbine::gebt_poc {
-
-void SectionalStiffness(
-    const StiffnessMatrix& stiffness, View2D_3x3::const_type rotation_0,
-    View2D_3x3::const_type rotation, View2D_6x6 sectional_stiffness
-) {
-    auto total_rotation = View2D_3x3("total_rotation");
-    KokkosBlas::gemm("N", "N", 1., rotation, rotation_0, 0., total_rotation);
-
-    // rotation_matrix_6x6 = [
-    //    [total_rotation]          [0]_3x3
-    //        [0]_3x3           total_rotation
-    // ]
-    auto rotation_matrix = View2D_6x6("rotation_matrix");
-    Kokkos::deep_copy(rotation_matrix, 0.);
-    auto rotation_matrix_1 =
-        Kokkos::subview(rotation_matrix, Kokkos::make_pair(0, 3), Kokkos::make_pair(0, 3));
-    Kokkos::deep_copy(rotation_matrix_1, total_rotation);
-    auto rotation_matrix_2 =
-        Kokkos::subview(rotation_matrix, Kokkos::make_pair(3, 6), Kokkos::make_pair(3, 6));
-    Kokkos::deep_copy(rotation_matrix_2, total_rotation);
-
-    // Calculate the sectional stiffness matrix in inertial basis
-    Kokkos::deep_copy(sectional_stiffness, 0.);
-    auto stiffness_matrix_left_rot = View2D_6x6("temp");
-    KokkosBlas::gemm(
-        "N", "N", 1., rotation_matrix, stiffness.GetStiffnessMatrix(), 0., stiffness_matrix_left_rot
-    );
-    KokkosBlas::gemm(
-        "N", "T", 1., stiffness_matrix_left_rot, rotation_matrix, 0., sectional_stiffness
-    );
-}
 
 void NodalElasticForces(
     View1D_LieAlgebra::const_type sectional_strain, View2D_3x3::const_type rotation,
