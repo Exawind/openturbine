a209057fe563b929e3eeda4ccadc3651cc9ca5ec
dcdemen <dcdemen@sandia.gov>
2024-03-06 07:40:18 -0700

    Removed now vestigial functions that took "flattened" 1D structures instead of 2D fields

--- src/gebt_poc/ElementalConstraintForcesResidual.hpp
+++ src/gebt_poc/ElementalConstraintForcesResidual.hpp
@@ -10,34 +10,6 @@
 namespace openturbine::gebt_poc {
 
 inline void ElementalConstraintForcesResidual(
-    View1D::const_type gen_coords, View1D constraints_residual
-) {
-    Kokkos::deep_copy(constraints_residual, 0.);
-    // For the GEBT proof of concept problem (i.e. the clamped beam), the dofs are enforced to be
-    // zero at the left end of the beam, so the constraint residual is simply based on the
-    // generalized coordinates at the first node
-    Kokkos::parallel_for(
-        1,
-        KOKKOS_LAMBDA(std::size_t) {
-            // Construct rotation vector from root node rotation quaternion
-            auto rotation_vector = openturbine::gen_alpha_solver::rotation_vector_from_quaternion(
-                openturbine::gen_alpha_solver::Quaternion(
-                    gen_coords(3), gen_coords(4), gen_coords(5), gen_coords(6)
-                )
-            );
-            // Set residual as translation and rotation of root node
-            // TODO: update when position & rotations are prescribed
-            constraints_residual(0) = gen_coords(0);
-            constraints_residual(1) = gen_coords(1);
-            constraints_residual(2) = gen_coords(2);
-            constraints_residual(3) = rotation_vector.GetXComponent();
-            constraints_residual(4) = rotation_vector.GetYComponent();
-            constraints_residual(5) = rotation_vector.GetZComponent();
-        }
-    );
-}
-
-inline void ElementalConstraintForcesResidual(
     LieGroupFieldView::const_type gen_coords, View1D constraints_residual
 ) {
     Kokkos::deep_copy(constraints_residual, 0.);
--- tests/unit_tests/gebt_poc/test_ElementalConstraintForcesResidual.cpp
+++ tests/unit_tests/gebt_poc/test_ElementalConstraintForcesResidual.cpp
@@ -1,27 +1,12 @@
 #include <gtest/gtest.h>
 
 #include "src/gebt_poc/ElementalConstraintForcesResidual.hpp"
-#include "tests/unit_tests/gebt_poc/test_data.h"
+
 #include "tests/unit_tests/gen_alpha_poc/test_utilities.h"
 
 namespace openturbine::gebt_poc {
+
 TEST(SolverTest, ElementalConstraintForcesResidual) {
-    auto generalized_coords = gen_alpha_solver::create_vector(
-        {0.1, 0., 0.12, 0.9987502603949662, 0.049979169270678324, 0., 0.}
-    );
-
-    auto constraints_residual = Kokkos::View<double[6]>("constraints_residual");
-
-    ElementalConstraintForcesResidual(generalized_coords, constraints_residual);
-
-    openturbine::gen_alpha_solver::tests::expect_kokkos_view_1D_equal(
-        // constraints_residual should be same as the generalized_coords where
-        // q{0.9987502603949662, 0.049979169270678324, 0., 0.} -> v{0.1, 0., 0.}
-        constraints_residual, {0.1, 0., 0.12, 0.1, 0., 0.0}
-    );
-}
-
-TEST(SolverTest, ElementalConstraintForcesResidual2D) {
     auto generalized_coords = gen_alpha_solver::create_matrix(
         {{0.1, 0., 0.12, 0.9987502603949662, 0.049979169270678324, 0., 0.}}
     );
