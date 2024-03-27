4a127a624a739d33298da588dafef3e8f73189d6
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2024-03-26 22:59:14 -0600

    Add the ConstraintResidualForRotatingBeam for applying the rotational constraint to beam element

--- src/gebt_poc/ElementalConstraintForcesResidual.hpp
+++ src/gebt_poc/ElementalConstraintForcesResidual.hpp
@@ -54,4 +54,55 @@ inline void AxialVector(
     );
 }
 
+inline void ConstraintResidualForRotatingBeam(
+    LieGroupFieldView initial_position, LieGroupFieldView::const_type gen_coords,
+    View1D applied_motion, View1D constraints_residual
+) {
+    // Constraint residual force for a rotating beam element with a prescribed rotation with
+    // root/reference node positioned at hub radius of the rotor is defined as
+    // {phi}_6x1 = {
+    //     {phi_x}_3x1 = {u} + {X_0} - [R_BC] * {X_0},
+    //     {phi_R}_3x1 = axial([R] * [R_BC]^T)
+    // }
+    // where,
+    // {u} is the displacement vector of the constrained node,
+    // {X_0} is the initial position vector of the constrained node,
+    // [R_BC] is the relative rotation matrix of the reference node,
+    // [R] is the relative rotation matrix of the constrained node
+
+    // Relative rotation of reference node
+    auto R_BC = openturbine::gen_alpha_solver::EulerParameterToRotationMatrix(
+        Kokkos::subview(applied_motion, Kokkos::make_pair(3,7))
+    );
+
+    // Relative rotation of constrained node
+    auto R = openturbine::gen_alpha_solver::EulerParameterToRotationMatrix(
+        Kokkos::subview(gen_coords, 0, Kokkos::make_pair(3,7))
+    );
+
+    // {phi_x}_3x1 = {u} + {X_0} - [R_BC] * {X_0}
+    auto u = Kokkos::subview(gen_coords, 0, Kokkos::make_pair(0,3));
+    auto X0 = Kokkos::subview(initial_position, 0, Kokkos::make_pair(0,3));
+    auto R_BC_X0 = View1D_Vector("R_BC_X0");
+    KokkosBlas::gemv("N", 1., R_BC, X0, 0., R_BC_X0);
+
+    // {phi_R}_3x1 = axial([R] * [R_BC]^T)
+    auto R_R_BC_T = View2D_3x3("R_R_BC_T");
+    KokkosBlas::gemm("N", "T", 1., R, R_BC, 0., R_R_BC_T);
+    auto r_phi = View1D_Vector("r_phi");
+    AxialVector(R_R_BC_T, r_phi);
+
+    Kokkos::parallel_for(
+        1,
+        KOKKOS_LAMBDA(std::size_t) {
+            constraints_residual(0) = u(0) + X0(0) - R_BC_X0(0);
+            constraints_residual(1) = u(1) + X0(1) - R_BC_X0(1);
+            constraints_residual(2) = u(2) + X0(2) - R_BC_X0(2);
+            constraints_residual(3) = r_phi(0);
+            constraints_residual(4) = r_phi(1);
+            constraints_residual(5) = r_phi(2);
+        }
+    );
+}
+
 }  // namespace openturbine::gebt_poc
--- tests/unit_tests/gebt_poc/test_ElementalConstraintForcesResidual.cpp
+++ tests/unit_tests/gebt_poc/test_ElementalConstraintForcesResidual.cpp
@@ -22,14 +22,38 @@ TEST(SolverTest, ElementalConstraintForcesResidual) {
 }
 
 TEST(SolverTest, TestAxialVectorOfARotationMatrix) {
-    auto matrix = gen_alpha_solver::create_matrix(
-        {{1., 0., 0.}, {0., 0., -1.}, {0., 1., 0.}}
-    );
+    auto matrix = gen_alpha_solver::create_matrix({{1., 0., 0.}, {0., 0., -1.}, {0., 1., 0.}});
     auto axial_vector = Kokkos::View<double[3]>("axial_vector");
     AxialVector(matrix, axial_vector);
 
+    openturbine::gen_alpha_solver::tests::expect_kokkos_view_1D_equal(axial_vector, {1., 0., 0.});
+}
+
+TEST(SolverTest, TestConstraintResidualForRotatingBeam) {
+    auto initial_position = gen_alpha_solver::create_matrix({
+        {2., 0., 0., 1., 0., 0., 0.},                  // node 1
+        {3.7267316464601146, 0., 0., 1., 0., 0., 0.},  // node 2
+        {7., 0., 0., 1., 0., 0., 0.},                  // node 3
+        {10.273268353539885, 0., 0., 1., 0., 0., 0.},  // node 4
+        {12., 0., 0., 1., 0., 0., 0.}                  // node 5
+    });
+    auto gen_coords = gen_alpha_solver::create_matrix({
+        {0., 0., 0., 1., 0., 0., 0.},  // node 1
+        {0., 0., 0., 1., 0., 0., 0.},  // node 2
+        {0., 0., 0., 1., 0., 0., 0.},  // node 3
+        {0., 0., 0., 1., 0., 0., 0.},  // node 4
+        {0., 0., 0., 1., 0., 0., 0.}   // node 5
+    });
+    auto applied_motion = gen_alpha_solver::create_vector({0., 0., 0., 0.999997, 0., 0., 0.0025});
+    auto constraints_residual = Kokkos::View<double[6]>("constraints_residual");
+
+    ConstraintResidualForRotatingBeam(
+        initial_position, gen_coords, applied_motion, constraints_residual
+    );
+
     openturbine::gen_alpha_solver::tests::expect_kokkos_view_1D_equal(
-        axial_vector, {1., 0., 0.}
+        constraints_residual,
+        {2.4999947916715115e-5, -0.009999958333385416, 0.0, 0.0, 0.0, -0.004999979166692708}
     );
 }
 
