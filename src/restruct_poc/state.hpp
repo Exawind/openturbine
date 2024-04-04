dcd256c30a3f61dcbe8b0eefc51afeacff754e26
Derek Slaughter <deslaughter@gmail.com>
2024-04-04 14:24:50 -0400

    Break out State struct into its own header.

---
+++ src/restruct_poc/state.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "types.hpp"
+
+namespace oturb {
+
+struct State {
+    size_t num_system_nodes;
+    size_t num_constraint_nodes;
+    View_Nx6 q_delta;
+    View_Nx7 q_prev;
+    View_Nx7 q;
+    View_Nx6 v;
+    View_Nx6 vd;
+    View_Nx6 a;
+    View_N lambda;
+    State() {}
+    State(size_t num_system_nodes_, size_t num_constraint_nodes_)
+        : num_system_nodes(num_system_nodes_),
+          num_constraint_nodes(num_constraint_nodes_),
+          q_delta("q_delta", num_system_nodes),
+          q_prev("q_prev", num_system_nodes),
+          q("q", num_system_nodes),
+          v("v", num_system_nodes),
+          vd("vd", num_system_nodes),
+          a("a", num_system_nodes),
+          lambda("lambda", num_constraint_nodes * kLieAlgebraComponents) {
+        // Initialize q and q_prev rotation to identity
+        Kokkos::deep_copy(Kokkos::subview(this->q_prev, Kokkos::ALL, 3), 1.);
+        Kokkos::deep_copy(Kokkos::subview(this->q, Kokkos::ALL, 3), 1.);
+    }
+    State(
+        size_t num_system_nodes, size_t num_constraint_nodes, std::vector<std::array<double, 7>> q_,
+        std::vector<std::array<double, 6>> v_, std::vector<std::array<double, 6>> vd_
+    )
+        : State(num_system_nodes, num_constraint_nodes) {
+        if (q_.size() == 0 || v_.size() == 0 || vd_.size() == 0)
+            return;
+        // Create mirror of state views
+        auto host_q = Kokkos::create_mirror(this->q);
+        auto host_v = Kokkos::create_mirror(this->v);
+        auto host_vd = Kokkos::create_mirror(this->vd);
+
+        // Loop through number of nodes and copy data to host view
+        for (size_t i = 0; i < num_system_nodes; ++i) {
+            for (size_t j = 0; j < kLieGroupComponents; ++j) {
+                host_q(i, j) = q_[i][j];
+            }
+            for (size_t j = 0; j < kLieAlgebraComponents; ++j) {
+                host_v(i, j) = v_[i][j];
+                host_vd(i, j) = vd_[i][j];
+            }
+        }
+
+        // Transfer host view data to state views
+        Kokkos::deep_copy(this->q, host_q);
+        Kokkos::deep_copy(this->v, host_v);
+        Kokkos::deep_copy(this->vd, host_vd);
+
+        // Copy q to q_previous
+        Kokkos::deep_copy(this->q_prev, this->q);
+    }
+};
+
+}  // namespace oturb
