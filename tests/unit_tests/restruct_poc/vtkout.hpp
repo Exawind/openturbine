982bbc856f2ec1cd7e564359f516bdf6a81ff530
Derek Slaughter <deslaughter@gmail.com>
2024-04-11 14:42:59 -0400

    Add function to dump a 3D Kokkos view to vector

--- tests/unit_tests/restruct_poc/vtkout.hpp
+++ tests/unit_tests/restruct_poc/vtkout.hpp
@@ -32,6 +32,27 @@ inline std::vector<std::vector<double>> kokkos_view_2D_to_vector(Kokkos::View<do
     return values;
 }
 
+inline std::vector<std::vector<std::vector<double>>> kokkos_view_3D_to_vector(
+    Kokkos::View<double***> view
+) {
+    Kokkos::View<double***> view_contiguous(
+        "view_contiguous", view.extent(0), view.extent(1), view.extent(2)
+    );
+    Kokkos::deep_copy(view_contiguous, view);
+    auto view_host = Kokkos::create_mirror(view_contiguous);
+    Kokkos::deep_copy(view_host, view_contiguous);
+    std::vector<std::vector<std::vector<double>>> values(view.extent(0));
+    for (size_t i = 0; i < view_host.extent(0); ++i) {
+        for (size_t j = 0; j < view_host.extent(1); ++j) {
+            values[i].push_back(std::vector<double>());
+            for (size_t k = 0; k < view_host.extent(2); ++k) {
+                values[i][j].push_back(view_host(i, j, k));
+            }
+        }
+    }
+    return values;
+}
+
 inline void BeamsWriteVTK(Beams& beams, std::string filename) {
     // Get a copy of the beam element indices in the host space
     auto elem_indices = Kokkos::create_mirror(beams.elem_indices);
