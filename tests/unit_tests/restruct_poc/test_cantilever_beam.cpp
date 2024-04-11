acff0ebfd21f5c0553b2c7e7e5e2bf401da36f20
Derek Slaughter <deslaughter@gmail.com>
2024-04-11 12:03:45 -0400

    Added cantilever beam with sine loading test
    This test replicates the results in test_dynamic_beam.cpp

---
+++ tests/unit_tests/restruct_poc/test_cantilever_beam.cpp
@@ -0,0 +1,152 @@
+#include <fstream>
+#include <initializer_list>
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+#include "src/restruct_poc/beams.hpp"
+#include "src/restruct_poc/solver.hpp"
+#include "tests/unit_tests/gen_alpha_poc/test_utilities.h"
+
+namespace openturbine::restruct_poc::tests {
+
+template <typename T>
+void WriteMatrixToFile(const std::vector<std::vector<T>>& data, const std::string& filename) {
+    std::ofstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Unable to open file: " << filename << std::endl;
+        return;
+    }
+    for (const auto& innerVector : data) {
+        for (const auto& element : innerVector) {
+            file << element << ",";
+        }
+        file << std::endl;
+    }
+    file.close();
+}
+
+TEST(DynamicBeamTest, CantileverBeamSineLoad) {
+    // Mass matrix for uniform composite beam section
+    Array_6x6 mass_matrix = {{
+        {8.538e-2, 0., 0., 0., 0., 0.},
+        {0., 8.538e-2, 0., 0., 0., 0.},
+        {0., 0., 8.538e-2, 0., 0., 0.},
+        {0., 0., 0., 1.4433e-2, 0., 0.},
+        {0., 0., 0., 0., 0.40972e-2, 0.},
+        {0., 0., 0., 0., 0., 1.0336e-2},
+    }};
+
+    // Stiffness matrix for uniform composite beam section
+    Array_6x6 stiffness_matrix = {{
+        {1368.17e3, 0., 0., 0., 0., 0.},
+        {0., 88.56e3, 0., 0., 0., 0.},
+        {0., 0., 38.78e3, 0., 0., 0.},
+        {0., 0., 0., 16.9600e3, 17.6100e3, -0.3510e3},
+        {0., 0., 0., 17.6100e3, 59.1200e3, -0.3700e3},
+        {0., 0., 0., -0.3510e3, -0.3700e3, 141.470e3},
+    }};
+
+    // Node locations (GLL quadrature)
+    std::vector<double> node_s({0., 0.17267316464601146, 0.5, 0.8273268353539885, 1.});
+
+    // Element quadrature
+    BeamQuadrature quadrature{
+        {-0.9491079123427585, 0.1294849661688697},  {-0.7415311855993943, 0.27970539148927664},
+        {-0.40584515137739696, 0.3818300505051189}, {6.123233995736766e-17, 0.4179591836734694},
+        {0.4058451513773971, 0.3818300505051189},   {0.7415311855993945, 0.27970539148927664},
+        {0.9491079123427585, 0.1294849661688697},
+    };
+
+    std::vector<BeamSection> sections = {
+        BeamSection(0., mass_matrix, stiffness_matrix),
+        BeamSection(1., mass_matrix, stiffness_matrix),
+    };
+
+    // Gravity vector
+    std::array<double, 3> gravity = {0., 0., 0.};
+
+    // Build vector of nodes (straight along x axis, no rotation)
+    std::vector<BeamNode> nodes;
+    std::vector<std::array<double, 7>> displacement;
+    std::vector<std::array<double, 6>> velocity;
+    std::vector<std::array<double, 6>> acceleration;
+    for (const double s : node_s) {
+        nodes.push_back(BeamNode(s, {10 * s, 0., 0., 1., 0., 0., 0.}));
+        displacement.push_back({0., 0., 0., 1., 0., 0., 0.});
+        velocity.push_back({0., 0., 0., 0., 0., 0.});
+        acceleration.push_back({0., 0., 0., 0., 0., 0.});
+    }
+
+    // Define beam initialization
+    BeamsInput beams_input(
+        {
+            BeamElement(nodes, sections, quadrature),
+        },
+        gravity
+    );
+
+    // Initialize beams from element inputs
+    auto beams = CreateBeams(beams_input);
+
+    // Number of system nodes from number of beam nodes
+    const size_t num_system_nodes(beams.num_nodes);
+
+    // Constraint inputs
+    std::vector<ConstraintInput> constraint_inputs({ConstraintInput(-1, 0)});
+
+    // Solution parameters
+    const bool is_dynamic_solve(true);
+    const size_t max_iter(5);
+    const double step_size(0.005);  // seconds
+    const double rho_inf(0.0);
+
+    // Create solver
+    Solver solver(
+        is_dynamic_solve, max_iter, step_size, rho_inf, num_system_nodes, constraint_inputs,
+        displacement, velocity, acceleration
+    );
+
+    // Initialize constraints
+    InitializeConstraints(solver, beams);
+    solver.constraints.UpdateDisplacement(0, {0, 0, 0, 1, 0, 0, 0});
+
+    // First step
+    Kokkos::parallel_for(
+        1, KOKKOS_LAMBDA(int
+           ) { beams.node_FX(beams.num_nodes - 1, 2) = 100.0 * std::sin(10.0 * 0.005); }
+    );
+    auto converged = Step(solver, beams);
+    EXPECT_EQ(converged, true);
+
+    openturbine::gen_alpha_solver::tests::expect_kokkos_view_1D_equal(
+        Kokkos::subview(solver.state.q, 4, Kokkos::make_pair(0, 3)),
+        {-8.15173937E-08, -1.86549248E-07, 6.97278045E-04}
+    );
+
+    // Second step
+    Kokkos::parallel_for(
+        1, KOKKOS_LAMBDA(std::size_t
+           ) { beams.node_FX(beams.num_nodes - 1, 2) = 100.0 * std::sin(10.0 * 0.010); }
+    );
+    converged = Step(solver, beams);
+    EXPECT_EQ(converged, true);
+    openturbine::gen_alpha_solver::tests::expect_kokkos_view_1D_equal(
+        Kokkos::subview(solver.state.q, 4, Kokkos::make_pair(0, 3)),
+        {-1.00926258E-06, -7.91711079E-07, 2.65017558E-03}
+    );
+
+    // Third step
+    Kokkos::parallel_for(
+        1, KOKKOS_LAMBDA(std::size_t
+           ) { beams.node_FX(beams.num_nodes - 1, 2) = 100.0 * std::sin(10.0 * 0.015); }
+    );
+    converged = Step(solver, beams);
+    EXPECT_EQ(converged, true);
+    openturbine::gen_alpha_solver::tests::expect_kokkos_view_1D_equal(
+        Kokkos::subview(solver.state.q, 4, Kokkos::make_pair(0, 3)),
+        {-5.05830945E-06, -2.29457246E-06, 6.30508154E-03}
+    );
+}
+
+}  // namespace openturbine::restruct_poc::tests
