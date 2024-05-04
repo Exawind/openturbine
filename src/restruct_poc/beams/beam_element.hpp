e1863e568a2ea76f5ec36ba9f1d9ca7822138737
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2024-05-04 17:47:50 -0600

    Remove trailing _ from arguments in BeamElement c-tor since they do not represent private attributes

--- src/restruct_poc/beams/beam_element.hpp
+++ src/restruct_poc/beams/beam_element.hpp
@@ -10,16 +10,13 @@ namespace openturbine {
 
 struct BeamElement {
     using BeamQuadrature = std::vector<std::array<double, 2>>;
+
     std::vector<BeamNode> nodes;        // Element node positions/rotations in material frame
     std::vector<BeamSection> sections;  // Element mass/stiffness in material frame
     BeamQuadrature quadrature;          // Element quadrature points and weights
 
-    BeamElement(
-        std::vector<BeamNode> nodes_, std::vector<BeamSection> sections_, BeamQuadrature quadrature_
-    )
-        : nodes(std::move(nodes_)),
-          sections(std::move(sections_)),
-          quadrature(std::move(quadrature_)) {}
+    BeamElement(std::vector<BeamNode> n, std::vector<BeamSection> s, BeamQuadrature q)
+        : nodes(std::move(n)), sections(std::move(s)), quadrature(std::move(q)) {}
 };
 
 }  // namespace openturbine
