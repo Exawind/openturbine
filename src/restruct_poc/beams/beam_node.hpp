455aee56309143cfb306262e9299f0c0963d83a7
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2024-05-04 17:56:46 -0600

    Remove trailing underscores from BeamNode c-tor args

--- src/restruct_poc/beams/beam_node.hpp
+++ src/restruct_poc/beams/beam_node.hpp
@@ -8,10 +8,10 @@ struct BeamNode {
     double s;                 // Position of node in element on range [0, 1]
     std::array<double, 7> x;  // Node initial positions and rotations
 
-    BeamNode(std::array<double, 7> x_) : s(0.), x(std::move(x_)) {}
-    BeamNode(double s_, std::array<double, 7> x_) : s(s_), x(x_) {}
-    BeamNode(double s_, std::array<double, 3> p, std::array<double, 4> q)
-        : s(s_), x{p[0], p[1], p[2], q[0], q[1], q[2], q[3]} {}
+    BeamNode(std::array<double, 7> x) : s(0.), x(std::move(x)) {}
+    BeamNode(double s, std::array<double, 7> x) : s(s), x(x) {}
+    BeamNode(double s, std::array<double, 3> p, std::array<double, 4> q)
+        : s(s), x{p[0], p[1], p[2], q[0], q[1], q[2], q[3]} {}
 };
 
 }  // namespace openturbine
