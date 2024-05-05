528d031be66d8691b63d40c38f264deb4cb0df32
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2024-05-04 18:12:40 -0600

    Remove trailing underscores from arguments in BeamsInput + shorten some local variable names as appropriate

--- src/restruct_poc/beams/beams_input.hpp
+++ src/restruct_poc/beams/beams_input.hpp
@@ -12,39 +12,39 @@ struct BeamsInput {
     std::vector<BeamElement> elements;
     std::array<double, 3> gravity;
 
-    BeamsInput(std::vector<BeamElement> elements_, std::array<double, 3> gravity_)
-        : elements(std::move(elements_)), gravity(std::move(gravity_)) {}
+    BeamsInput(std::vector<BeamElement> elems, std::array<double, 3> g)
+        : elements(std::move(elems)), gravity(std::move(g)) {}
 
     size_t NumElements() const { return elements.size(); };
 
     size_t NumNodes() const {
         size_t num_nodes = 0;
-        for (const auto& input : this->elements) {
-            num_nodes += input.nodes.size();
+        for (const auto& e : this->elements) {
+            num_nodes += e.nodes.size();
         }
         return num_nodes;
     }
 
     size_t NumQuadraturePoints() const {
         size_t num_qps = 0;
-        for (const auto& input : this->elements) {
-            num_qps += input.quadrature.size();
+        for (const auto& e : this->elements) {
+            num_qps += e.quadrature.size();
         }
         return num_qps;
     }
 
     size_t MaxElemNodes() const {
         size_t max_elem_nodes = 0;
-        for (const auto& input : this->elements) {
-            max_elem_nodes = std::max(max_elem_nodes, input.nodes.size());
+        for (const auto& e : this->elements) {
+            max_elem_nodes = std::max(max_elem_nodes, e.nodes.size());
         }
         return max_elem_nodes;
     }
 
     size_t MaxElemQuadraturePoints() const {
         size_t max_elem_qps = 0;
-        for (const auto& input : this->elements) {
-            max_elem_qps = std::max(max_elem_qps, input.quadrature.size());
+        for (const auto& e : this->elements) {
+            max_elem_qps = std::max(max_elem_qps, e.quadrature.size());
         }
         return max_elem_qps;
     }
