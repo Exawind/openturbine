a66ea43e73ad023398f3f5ee0b850df7fb3833e3
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2024-05-04 18:25:56 -0600

    Remove trailing underscore from function arguments of InterpolateQPAcceleration_Translation, InterpolateQPAcceleration_Angular, InterpolateQPPosition, InterpolateQPRotationDerivative, and InterpolateQPRotationDerivative

--- src/restruct_poc/beams/interpolate_QP_rotation.hpp
+++ src/restruct_poc/beams/interpolate_QP_rotation.hpp
@@ -11,16 +11,17 @@ namespace openturbine {
 
 struct InterpolateQPRotation {
     Kokkos::View<Beams::ElemIndices*>::const_type elem_indices;  // Element indices
-    View_NxN::const_type shape_interp_;                          // Num Nodes x Num Quadrature points
-    View_Nx7::const_type node_pos_rot_;                          // Node global position vector
-    View_Nx4 qp_rot_;                                            // quadrature point rotation
+    View_NxN::const_type shape_interpolation;                    // Num Nodes x Num Quadrature points
+    View_Nx7::const_type node_position_rotation;                 // Node global position vector
+    View_Nx4 qp_rotation;                                        // quadrature point rotation
 
     KOKKOS_FUNCTION
     void operator()(const int i_elem) const {
         auto& idx = elem_indices[i_elem];
-        auto shape_interp = Kokkos::subview(shape_interp_, idx.node_range, idx.qp_shape_range);
-        auto node_rot = Kokkos::subview(node_pos_rot_, idx.node_range, Kokkos::make_pair(3, 7));
-        auto qp_rot = Kokkos::subview(qp_rot_, idx.qp_range, Kokkos::ALL);
+        auto shape_interp = Kokkos::subview(shape_interpolation, idx.node_range, idx.qp_shape_range);
+        auto node_rot =
+            Kokkos::subview(node_position_rotation, idx.node_range, Kokkos::make_pair(3, 7));
+        auto qp_rot = Kokkos::subview(qp_rotation, idx.qp_range, Kokkos::ALL);
 
         InterpQuaternion(shape_interp, node_rot, qp_rot);
     }
