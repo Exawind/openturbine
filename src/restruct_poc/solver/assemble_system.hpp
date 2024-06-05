c54fdb8ce3f99135035c92307d8e6e78e00440b9
dcdemen <dcdemen@sandia.gov>
2024-06-05 08:03:55 -0600

    Use a sparse matrix inside the AssembleSystem function for the main matrix multiply.
    Even with allocating new memory every iteration and filling the matrices in serial,
    this accelerates the 300 beam 15 MW rotor from 78.5s to 53.2s on V100S.

--- src/restruct_poc/solver/assemble_system.hpp
+++ src/restruct_poc/solver/assemble_system.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <KokkosBlas.hpp>
+#include <KokkosSparse.hpp>
 #include <Kokkos_Core.hpp>
 #include <Kokkos_Profiling_ScopedRegion.hpp>
 
@@ -36,7 +37,95 @@ void AssembleSystem(Solver& solver, Beams& beams, Subview_NxN St_11, Subview_N R
     AssembleElasticStiffnessMatrix(beams, solver.K);
     AssembleInertialStiffnessMatrix(beams, solver.K);
 
-    KokkosBlas::gemm("N", "N", 1.0, solver.K, solver.T, 0.0, St_11);
+    auto num_rows = solver.K.extent(0);
+    auto num_columns = solver.K.extent(1);
+    auto num_non_zero = (beams.max_elem_nodes * 6) * (beams.max_elem_nodes * 6) * beams.num_elems; //make this a reduction
+    auto row_ptrs = Kokkos::View<int*>("row_ptrs", num_rows+1);
+    auto indices = Kokkos::View<int*>("indices", num_non_zero);
+    auto K_values = Kokkos::View<double*>("K values", num_non_zero);
+    auto T_values = Kokkos::View<double*>("T values", num_non_zero);
+    Kokkos::parallel_for(1, KOKKOS_LAMBDA(int) {
+      auto rows_so_far = 0;
+      for(int i_elem = 0; i_elem < beams.num_elems; ++i_elem) {
+        auto elem_indices = beams.elem_indices;
+        auto idx = elem_indices[i_elem];
+        auto num_nodes = idx.num_nodes;
+        for(int i = 0; i < num_nodes*kLieAlgebraComponents; ++i) {
+            row_ptrs(rows_so_far + 1) = row_ptrs(rows_so_far) + num_nodes * kLieAlgebraComponents;
+            ++rows_so_far;
+        }
+        
+      }
+    });
+    Kokkos::parallel_for(1, KOKKOS_LAMBDA(int) {
+      auto entries_so_far = 0;
+      for(int i_elem = 0; i_elem < beams.num_elems; ++i_elem) {
+        auto node_state_indices = beams.node_state_indices;
+        auto elem_indices = beams.elem_indices;
+        auto idx = elem_indices[i_elem];
+        auto num_nodes = idx.num_nodes;
+        for(int j_index = 0; j_index < num_nodes; ++j_index) {
+          for(int n = 0; n < kLieAlgebraComponents; ++n) {
+            for(int i_index = 0; i_index < num_nodes; ++i_index) {
+              const auto i = i_index + idx.node_range.first;
+              const auto column_start = node_state_indices(i)*kLieAlgebraComponents;
+              for(int m = 0; m < kLieAlgebraComponents; ++m) {
+                  indices(entries_so_far) = column_start + m;
+                  ++entries_so_far;
+              }
+            }
+          }
+        }
+      }
+    });
+
+    using crs_matrix_type = KokkosSparse::CrsMatrix<double, int, Kokkos::Device<Kokkos::Cuda, Kokkos::CudaSpace>, void, int>;
+    auto K = crs_matrix_type("K", num_rows, num_columns, num_non_zero, K_values, row_ptrs, indices);
+    auto T = crs_matrix_type("T", num_rows, num_columns, num_non_zero, T_values, row_ptrs, indices);
+
+    auto sk = solver.K;
+    auto row_data = Kokkos::View<double*>("row_data", num_columns);
+    auto col_idx = Kokkos::View<int*>("row_data", num_columns);
+    Kokkos::parallel_for(1, KOKKOS_LAMBDA(int) {
+        for(int i = 0; i < num_rows; ++i) {
+            auto row = K.row(i);
+            auto row_map = K.graph.row_map;
+            auto cols = K.graph.entries;
+            for(int entry = 0; entry < row.length; ++entry) {
+                col_idx(entry) = cols(row_map(i) + entry);
+                row_data(entry) = sk(i, col_idx(entry));
+            }
+            K.replaceValues(i, col_idx.data(), row.length, row_data.data());
+        }
+    });
+    
+    auto st = solver.T;
+    Kokkos::parallel_for(1, KOKKOS_LAMBDA(int) {
+        for(int i = 0; i < num_rows; ++i) {
+            auto row = T.row(i);
+            auto row_map = T.graph.row_map;
+            auto cols = T.graph.entries;
+            for(int entry = 0; entry < row.length; ++entry) {
+                col_idx(entry) = cols(row_map(i) + entry);
+                row_data(entry) = st(i, col_idx(entry));
+            }
+            T.replaceValues(i, col_idx.data(), row.length, row_data.data());
+        }
+    });
+
+    Kokkos::deep_copy(St_11, 0.);
+    
+    crs_matrix_type local_St_11 = KokkosSparse::spgemm<crs_matrix_type>(K, false, T, false);
+    Kokkos::parallel_for(1, KOKKOS_LAMBDA(int) {
+        for(int i = 0; i < num_rows; ++i) {
+            auto row = local_St_11.row(i);
+            auto row_map = local_St_11.graph.row_map;
+            auto cols = local_St_11.graph.entries;
+            for(int entry = 0; entry < row.length; ++entry) {
+                St_11(i, cols(row_map(i) + entry)) = row.value(entry);
+            }
+        }
+    });   
 
     if (solver.is_dynamic_solve) {
         Kokkos::deep_copy(solver.K, 0.);
