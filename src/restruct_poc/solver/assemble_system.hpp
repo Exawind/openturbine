03d24e2d40a701608df13d55cf7a951977bb5e12
dcdemen <dcdemen@sandia.gov>
2024-06-05 11:39:43 -0600

    Moved to using separate symbolic and numeric steps for spgemm, use spadd instead of a dense update

--- src/restruct_poc/solver/assemble_system.hpp
+++ src/restruct_poc/solver/assemble_system.hpp
@@ -2,6 +2,7 @@
 
 #include <KokkosBlas.hpp>
 #include <KokkosSparse.hpp>
+#include <KokkosSparse_spadd.hpp>
 #include <Kokkos_Core.hpp>
 #include <Kokkos_Profiling_ScopedRegion.hpp>
 
@@ -17,9 +18,100 @@
 
 namespace openturbine {
 
+struct CopyIntoSparseMatrix {
+    using crs_matrix_type = KokkosSparse::CrsMatrix<double, int, Kokkos::Device<Kokkos::DefaultExecutionSpace, Kokkos::DefaultExecutionSpace::memory_space>, void, int>;
+    using row_data_type = Kokkos::View<double*, Kokkos::DefaultExecutionSpace::scratch_memory_space, Kokkos::MemoryTraits<Kokkos::Unmanaged>>;
+    using col_idx_type = Kokkos::View<int*, Kokkos::DefaultExecutionSpace::scratch_memory_space, Kokkos::MemoryTraits<Kokkos::Unmanaged>>;
+    crs_matrix_type sparse;
+    Kokkos::View<const double**> dense;
+
+    KOKKOS_FUNCTION
+    void operator()(Kokkos::TeamPolicy<>::member_type member) const {
+        auto i = member.league_rank();
+        auto row = sparse.row(i);
+        auto row_map = sparse.graph.row_map;
+        auto cols = sparse.graph.entries;
+        auto row_data = row_data_type(member.team_scratch(1), row.length);
+        auto col_idx = col_idx_type(member.team_scratch(1), row.length);
+        Kokkos::parallel_for(Kokkos::TeamThreadRange(member, row.length), [=](int entry) {
+            col_idx(entry) = cols(row_map(i) + entry);
+            row_data(entry) = dense(i, col_idx(entry));
+        });
+        member.team_barrier();
+        Kokkos::single(Kokkos::PerTeam(member), [=](){
+            sparse.replaceValues(i, col_idx.data(), row.length, row_data.data());
+        });
+    }
+};
+
+struct ComputeNumberOfNonZeros {
+    Kokkos::View<Beams::ElemIndices*>::const_type elem_indices;
+    
+    KOKKOS_FUNCTION
+    void operator()(int i_elem, int& update) const {
+        auto idx = elem_indices[i_elem];
+        auto num_nodes = idx.num_nodes;
+        update += (num_nodes*6) * (num_nodes*6);
+    }
+};
+
+struct PopulateSparseRowPtrs {
+    Kokkos::View<Beams::ElemIndices*>::const_type elem_indices;
+    Kokkos::View<int*> row_ptrs;
+
+    KOKKOS_FUNCTION
+    void operator()(int) const {
+      const auto num_elems = elem_indices.extent(0);
+      auto rows_so_far = 0;
+      for(int i_elem = 0; i_elem < num_elems; ++i_elem) {
+        auto idx = elem_indices[i_elem];
+        auto num_nodes = idx.num_nodes;
+        for(int i = 0; i < num_nodes*kLieAlgebraComponents; ++i) {
+            row_ptrs(rows_so_far + 1) = row_ptrs(rows_so_far) + num_nodes * kLieAlgebraComponents;
+            ++rows_so_far;
+        }
+        
+      }
+    }
+};
+
+struct PopulateSparseIndices {
+    Kokkos::View<Beams::ElemIndices*>::const_type elem_indices;
+    Kokkos::View<int*>::const_type node_state_indices;    
+    Kokkos::View<int*> indices;
+
+    KOKKOS_FUNCTION
+    void operator()(int) const {
+      const auto num_elems = elem_indices.extent(0);
+      auto entries_so_far = 0;
+      for(int i_elem = 0; i_elem < num_elems; ++i_elem) {
+        auto idx = elem_indices[i_elem];
+        auto num_nodes = idx.num_nodes;
+        for(int j_index = 0; j_index < num_nodes; ++j_index) {
+          for(int n = 0; n < kLieAlgebraComponents; ++n) {
+            for(int i_index = 0; i_index < num_nodes; ++i_index) {
+              const auto i = i_index + idx.node_range.first;
+              const auto column_start = node_state_indices(i)*kLieAlgebraComponents;
+              for(int m = 0; m < kLieAlgebraComponents; ++m) {
+                  indices(entries_so_far) = column_start + m;
+                  ++entries_so_far;
+              }
+            }
+          }
+        }
+      }
+    }
+
+};
+
 template <typename Subview_NxN, typename Subview_N>
 void AssembleSystem(Solver& solver, Beams& beams, Subview_NxN St_11, Subview_N R_system) {
     auto region = Kokkos::Profiling::ScopedRegion("Assemble System");
+    using KernelHandle = typename KokkosKernels::Experimental::KokkosKernelsHandle<int, int, double, Kokkos::DefaultExecutionSpace, Kokkos::DefaultExecutionSpace::memory_space, Kokkos::DefaultExecutionSpace::memory_space>;
+    KernelHandle kh;
+    kh.create_spgemm_handle();
+    kh.create_spadd_handle();
+
     Kokkos::deep_copy(solver.T, 0.);
     Kokkos::parallel_for(
         "TangentOperator", solver.num_system_nodes,
@@ -40,116 +132,56 @@ void AssembleSystem(Solver& solver, Beams& beams, Subview_NxN St_11, Subview_N R
     auto num_rows = solver.K.extent(0);
     auto num_columns = solver.K.extent(1);
     auto num_non_zero = 0;
-    Kokkos::parallel_reduce("compute number of non-zeros", beams.num_elems, KOKKOS_LAMBDA(int i_elem, int& update) {
-        auto elem_indices = beams.elem_indices;
-        auto idx = elem_indices[i_elem];
-        auto num_nodes = idx.num_nodes;
-        update += (num_nodes*6) * (num_nodes*6);
-    }, num_non_zero);
+    Kokkos::parallel_reduce("ComputeNumberOfNonZeros", beams.num_elems, ComputeNumberOfNonZeros{beams.elem_indices}, num_non_zero);
 
     auto row_ptrs = Kokkos::View<int*>("row_ptrs", num_rows+1);
     auto indices = Kokkos::View<int*>("indices", num_non_zero);
+    Kokkos::parallel_for("PopulateSparseRowPtrs", 1, PopulateSparseRowPtrs{beams.elem_indices, row_ptrs});
+    Kokkos::parallel_for("Populate Sparse Indices", 1, PopulateSparseIndices{beams.elem_indices, beams.node_state_indices, indices});
+
+    using crs_matrix_type = KokkosSparse::CrsMatrix<double, int, Kokkos::Device<Kokkos::DefaultExecutionSpace, Kokkos::DefaultExecutionSpace::memory_space>, void, int>;
+    Kokkos::fence();
     auto K_values = Kokkos::View<double*>("K values", num_non_zero);
-    auto T_values = Kokkos::View<double*>("T values", num_non_zero);
-    Kokkos::parallel_for("Populate Sparse Row Ptrs", 1, KOKKOS_LAMBDA(int) {
-      auto rows_so_far = 0;
-      for(int i_elem = 0; i_elem < beams.num_elems; ++i_elem) {
-        auto elem_indices = beams.elem_indices;
-        auto idx = elem_indices[i_elem];
-        auto num_nodes = idx.num_nodes;
-        for(int i = 0; i < num_nodes*kLieAlgebraComponents; ++i) {
-            row_ptrs(rows_so_far + 1) = row_ptrs(rows_so_far) + num_nodes * kLieAlgebraComponents;
-            ++rows_so_far;
-        }
-        
-      }
-    });
-    Kokkos::parallel_for("Populate Sparse Indices", 1, KOKKOS_LAMBDA(int) {
-      auto entries_so_far = 0;
-      for(int i_elem = 0; i_elem < beams.num_elems; ++i_elem) {
-        auto node_state_indices = beams.node_state_indices;
-        auto elem_indices = beams.elem_indices;
-        auto idx = elem_indices[i_elem];
-        auto num_nodes = idx.num_nodes;
-        for(int j_index = 0; j_index < num_nodes; ++j_index) {
-          for(int n = 0; n < kLieAlgebraComponents; ++n) {
-            for(int i_index = 0; i_index < num_nodes; ++i_index) {
-              const auto i = i_index + idx.node_range.first;
-              const auto column_start = node_state_indices(i)*kLieAlgebraComponents;
-              for(int m = 0; m < kLieAlgebraComponents; ++m) {
-                  indices(entries_so_far) = column_start + m;
-                  ++entries_so_far;
-              }
-            }
-          }
-        }
-      }
-    });
-
-    using crs_matrix_type = KokkosSparse::CrsMatrix<double, int, Kokkos::Device<Kokkos::Cuda, Kokkos::CudaSpace>, void, int>;
     auto K = crs_matrix_type("K", num_rows, num_columns, num_non_zero, K_values, row_ptrs, indices);
+    auto T_values = Kokkos::View<double*>("T values", num_non_zero);
     auto T = crs_matrix_type("T", num_rows, num_columns, num_non_zero, T_values, row_ptrs, indices);
-
-    auto sk = solver.K;
 
     auto row_data_size = Kokkos::View<double*>::shmem_size(num_columns);
     auto col_idx_size = Kokkos::View<int*>::shmem_size(num_columns);
     auto sparse_matrix_policy = Kokkos::TeamPolicy<>(num_rows, Kokkos::AUTO());
     sparse_matrix_policy.set_scratch_size(1, Kokkos::PerTeam(row_data_size + col_idx_size));
-    Kokkos::parallel_for("Copy into Sparse Matrix", sparse_matrix_policy, KOKKOS_LAMBDA(Kokkos::TeamPolicy<>::member_type member) {
-        auto i = member.league_rank();
-        auto row = K.row(i);
-        auto row_map = K.graph.row_map;
-        auto cols = K.graph.entries;
-        auto row_data = Kokkos::View<double*, Kokkos::DefaultExecutionSpace::scratch_memory_space, Kokkos::MemoryTraits<Kokkos::Unmanaged>>(member.team_scratch(1), num_columns);
-        auto col_idx = Kokkos::View<int*, Kokkos::DefaultExecutionSpace::scratch_memory_space, Kokkos::MemoryTraits<Kokkos::Unmanaged>>(member.team_scratch(1), num_columns);
-        Kokkos::parallel_for(Kokkos::TeamThreadRange(member, row.length), [=](int entry) {
-            col_idx(entry) = cols(row_map(i) + entry);
-            row_data(entry) = sk(i, col_idx(entry));
-        });
-        member.team_barrier();
-        Kokkos::single(Kokkos::PerTeam(member), [=](){
-            K.replaceValues(i, col_idx.data(), row.length, row_data.data());
-        });
-    });
-    
-    auto st = solver.T;
-    Kokkos::parallel_for("Copy into Sparse Matrix", sparse_matrix_policy, KOKKOS_LAMBDA(Kokkos::TeamPolicy<>::member_type member) {
-        auto i = member.league_rank();
-        auto row = T.row(i);
-        auto row_map = T.graph.row_map;
-        auto cols = T.graph.entries;
-        auto row_data = Kokkos::View<double*, Kokkos::DefaultExecutionSpace::scratch_memory_space, Kokkos::MemoryTraits<Kokkos::Unmanaged>>(member.team_scratch(1), num_columns);
-        auto col_idx = Kokkos::View<int*, Kokkos::DefaultExecutionSpace::scratch_memory_space, Kokkos::MemoryTraits<Kokkos::Unmanaged>>(member.team_scratch(1), num_columns);
-        Kokkos::parallel_for(Kokkos::TeamThreadRange(member, row.length), [=](int entry) {
-            col_idx(entry) = cols(row_map(i) + entry);
-            row_data(entry) = st(i, col_idx(entry));
-        });
-        member.team_barrier();
-        Kokkos::single(Kokkos::PerTeam(member), [=](){
-            T.replaceValues(i, col_idx.data(), row.length, row_data.data());
-        });
-    });
+
+    Kokkos::parallel_for("Copy into Sparse Matrix", sparse_matrix_policy, CopyIntoSparseMatrix{K, solver.K});
+    Kokkos::parallel_for("Copy into Sparse Matrix", sparse_matrix_policy, CopyIntoSparseMatrix{T, solver.T});
+
+    Kokkos::fence();
+    crs_matrix_type static_system_matrix;
+    KokkosSparse::spgemm_symbolic(kh, K, false, T, false, static_system_matrix);
+    KokkosSparse::spgemm_numeric(kh, K, false, T, false, static_system_matrix);
+
+    auto beta_prime = (solver.is_dynamic_solve) ? solver.beta_prime : 0.;
+    auto gamma_prime = (solver.is_dynamic_solve) ? solver.gamma_prime : 0.;
+    Kokkos::deep_copy(solver.K, 0.);
+    AssembleMassMatrix(beams, beta_prime, solver.K);
+    AssembleGyroscopicInertiaMatrix(beams, gamma_prime, solver.K);
+    Kokkos::parallel_for("Copy into Sparse Matrix", sparse_matrix_policy, CopyIntoSparseMatrix{K, solver.K});
+
+    Kokkos::fence();
+    crs_matrix_type system_matrix;
+    KokkosSparse::spadd_symbolic(&kh, K, static_system_matrix, system_matrix);
+    KokkosSparse::spadd_numeric(&kh, 1., K, 1., static_system_matrix, system_matrix);
 
     Kokkos::deep_copy(St_11, 0.);
-    
-    crs_matrix_type local_St_11 = KokkosSparse::spgemm<crs_matrix_type>(K, false, T, false);
+    Kokkos::fence();
     Kokkos::parallel_for("Copy into St_11", sparse_matrix_policy, KOKKOS_LAMBDA(Kokkos::TeamPolicy<>::member_type member) {
         auto i = member.league_rank();
-        auto row = local_St_11.row(i);
-        auto row_map = local_St_11.graph.row_map;
-        auto cols = local_St_11.graph.entries;
+        auto row = system_matrix.row(i);
+        auto row_map = system_matrix.graph.row_map;
+        auto cols = system_matrix.graph.entries;
         Kokkos::parallel_for(Kokkos::TeamThreadRange(member, row.length), [=](int entry) {
             St_11(i, cols(row_map(i) + entry)) = row.value(entry);
         });
     });   
-
-    if (solver.is_dynamic_solve) {
-        Kokkos::deep_copy(solver.K, 0.);
-        AssembleMassMatrix(beams, solver.beta_prime, solver.K);
-        AssembleGyroscopicInertiaMatrix(beams, solver.gamma_prime, solver.K);
-        KokkosBlas::update(0., solver.K, 1., solver.K, 1., St_11);
-    }
 }
 
 }  // namespace openturbine
