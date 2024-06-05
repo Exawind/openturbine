67123a9214f17ec86b4a34ea204cd9f86b26f94c
dcdemen <dcdemen@sandia.gov>
2024-06-05 09:19:30 -0600

    Parallelized loops used for populating sparse matrices
    This accelerated the 300 element benchmark from 53.2s to 34.1s.

--- src/restruct_poc/solver/assemble_system.hpp
+++ src/restruct_poc/solver/assemble_system.hpp
@@ -44,7 +44,7 @@ void AssembleSystem(Solver& solver, Beams& beams, Subview_NxN St_11, Subview_N R
     auto indices = Kokkos::View<int*>("indices", num_non_zero);
     auto K_values = Kokkos::View<double*>("K values", num_non_zero);
     auto T_values = Kokkos::View<double*>("T values", num_non_zero);
-    Kokkos::parallel_for(1, KOKKOS_LAMBDA(int) {
+    Kokkos::parallel_for("Populate Sparse Row Ptrs", 1, KOKKOS_LAMBDA(int) {
       auto rows_so_far = 0;
       for(int i_elem = 0; i_elem < beams.num_elems; ++i_elem) {
         auto elem_indices = beams.elem_indices;
@@ -57,7 +57,7 @@ void AssembleSystem(Solver& solver, Beams& beams, Subview_NxN St_11, Subview_N R
         
       }
     });
-    Kokkos::parallel_for(1, KOKKOS_LAMBDA(int) {
+    Kokkos::parallel_for("Populate Sparse Indices", 1, KOKKOS_LAMBDA(int) {
       auto entries_so_far = 0;
       for(int i_elem = 0; i_elem < beams.num_elems; ++i_elem) {
         auto node_state_indices = beams.node_state_indices;
@@ -84,47 +84,57 @@ void AssembleSystem(Solver& solver, Beams& beams, Subview_NxN St_11, Subview_N R
     auto T = crs_matrix_type("T", num_rows, num_columns, num_non_zero, T_values, row_ptrs, indices);
 
     auto sk = solver.K;
-    auto row_data = Kokkos::View<double*>("row_data", num_columns);
-    auto col_idx = Kokkos::View<int*>("row_data", num_columns);
-    Kokkos::parallel_for(1, KOKKOS_LAMBDA(int) {
-        for(int i = 0; i < num_rows; ++i) {
-            auto row = K.row(i);
-            auto row_map = K.graph.row_map;
-            auto cols = K.graph.entries;
-            for(int entry = 0; entry < row.length; ++entry) {
-                col_idx(entry) = cols(row_map(i) + entry);
-                row_data(entry) = sk(i, col_idx(entry));
-            }
+
+    auto row_data_size = Kokkos::View<double*>::shmem_size(num_columns);
+    auto col_idx_size = Kokkos::View<int*>::shmem_size(num_columns);
+    auto sparse_matrix_policy = Kokkos::TeamPolicy<>(num_rows, Kokkos::AUTO());
+    sparse_matrix_policy.set_scratch_size(1, Kokkos::PerTeam(row_data_size + col_idx_size));
+    Kokkos::parallel_for("Copy into Sparse Matrix", sparse_matrix_policy, KOKKOS_LAMBDA(Kokkos::TeamPolicy<>::member_type member) {
+        auto i = member.league_rank();
+        auto row = K.row(i);
+        auto row_map = K.graph.row_map;
+        auto cols = K.graph.entries;
+        auto row_data = Kokkos::View<double*, Kokkos::DefaultExecutionSpace::scratch_memory_space, Kokkos::MemoryTraits<Kokkos::Unmanaged>>(member.team_scratch(1), num_columns);
+        auto col_idx = Kokkos::View<int*, Kokkos::DefaultExecutionSpace::scratch_memory_space, Kokkos::MemoryTraits<Kokkos::Unmanaged>>(member.team_scratch(1), num_columns);
+        Kokkos::parallel_for(Kokkos::TeamThreadRange(member, row.length), [=](int entry) {
+            col_idx(entry) = cols(row_map(i) + entry);
+            row_data(entry) = sk(i, col_idx(entry));
+        });
+        member.team_barrier();
+        Kokkos::single(Kokkos::PerTeam(member), [=](){
             K.replaceValues(i, col_idx.data(), row.length, row_data.data());
-        }
+        });
     });
     
     auto st = solver.T;
-    Kokkos::parallel_for(1, KOKKOS_LAMBDA(int) {
-        for(int i = 0; i < num_rows; ++i) {
-            auto row = T.row(i);
-            auto row_map = T.graph.row_map;
-            auto cols = T.graph.entries;
-            for(int entry = 0; entry < row.length; ++entry) {
-                col_idx(entry) = cols(row_map(i) + entry);
-                row_data(entry) = st(i, col_idx(entry));
-            }
+    Kokkos::parallel_for("Copy into Sparse Matrix", sparse_matrix_policy, KOKKOS_LAMBDA(Kokkos::TeamPolicy<>::member_type member) {
+        auto i = member.league_rank();
+        auto row = T.row(i);
+        auto row_map = T.graph.row_map;
+        auto cols = T.graph.entries;
+        auto row_data = Kokkos::View<double*, Kokkos::DefaultExecutionSpace::scratch_memory_space, Kokkos::MemoryTraits<Kokkos::Unmanaged>>(member.team_scratch(1), num_columns);
+        auto col_idx = Kokkos::View<int*, Kokkos::DefaultExecutionSpace::scratch_memory_space, Kokkos::MemoryTraits<Kokkos::Unmanaged>>(member.team_scratch(1), num_columns);
+        Kokkos::parallel_for(Kokkos::TeamThreadRange(member, row.length), [=](int entry) {
+            col_idx(entry) = cols(row_map(i) + entry);
+            row_data(entry) = st(i, col_idx(entry));
+        });
+        member.team_barrier();
+        Kokkos::single(Kokkos::PerTeam(member), [=](){
             T.replaceValues(i, col_idx.data(), row.length, row_data.data());
-        }
+        });
     });
 
     Kokkos::deep_copy(St_11, 0.);
     
     crs_matrix_type local_St_11 = KokkosSparse::spgemm<crs_matrix_type>(K, false, T, false);
-    Kokkos::parallel_for(1, KOKKOS_LAMBDA(int) {
-        for(int i = 0; i < num_rows; ++i) {
-            auto row = local_St_11.row(i);
-            auto row_map = local_St_11.graph.row_map;
-            auto cols = local_St_11.graph.entries;
-            for(int entry = 0; entry < row.length; ++entry) {
-                St_11(i, cols(row_map(i) + entry)) = row.value(entry);
-            }
-        }
+    Kokkos::parallel_for("Copy into St_11", sparse_matrix_policy, KOKKOS_LAMBDA(Kokkos::TeamPolicy<>::member_type member) {
+        auto i = member.league_rank();
+        auto row = local_St_11.row(i);
+        auto row_map = local_St_11.graph.row_map;
+        auto cols = local_St_11.graph.entries;
+        Kokkos::parallel_for(Kokkos::TeamThreadRange(member, row.length), [=](int entry) {
+            St_11(i, cols(row_map(i) + entry)) = row.value(entry);
+        });
     });   
 
     if (solver.is_dynamic_solve) {
