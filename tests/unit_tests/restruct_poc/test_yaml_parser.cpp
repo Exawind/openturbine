52f195670f8857be15e8ee2c9204beefce5c3f57
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2024-08-02 14:33:20 -0600

    Remove unnecessary code from yaml testing + IO

--- tests/unit_tests/restruct_poc/test_yaml_parser.cpp
+++ tests/unit_tests/restruct_poc/test_yaml_parser.cpp
@@ -1,38 +1,25 @@
 #include <gtest/gtest.h>
+#include <yaml-cpp/yaml.h>
 
 #include "test_utilities.hpp"
 
-#include "src/io/read_windIO_input.hpp"
-#include "src/io/windio_mapped_structs.cpp"
+#include "src/utilities/scripts/windio_mapped_structs.cpp"
 
 namespace openturbine::restruct_poc::tests {
-
-TEST(ParserTest, ReadYAMLNode) {
-    try {
-        // Load the YAML file
-        YAML::Node config =
-            YAML::LoadFile("/Users/fbhuiyan/dev/openturbine/src/utilities/scripts/config.yaml");
-
-        // Print the contents of the YAML file
-        io::print_node(config);
-    } catch (const YAML::Exception& e) {
-        std::cerr << "Error loading YAML file: " << e.what() << std::endl;
-    }
-}
 
 TEST(ParserTest, ParseIEA15MW) {
     try {
         // Load the YAML file
-        YAML::Node config =
-            YAML::LoadFile("/Users/fbhuiyan/dev/openturbine/src/io/IEA-15-240-RWT.yaml");
+        YAML::Node config = YAML::LoadFile("../src/utilities/scripts/IEA-15-240-RWT.yaml");
 
         // Parse the top-level node
         Turbine turbine;
         turbine.parse(config);
 
-        // Add some assertions
+        // Assert the turbine name
         ASSERT_EQ(turbine.name, "IEA 15MW Offshore Reference Turbine, with taped chord tip design");
 
+        // Assert the assembly parameters
         ASSERT_EQ(turbine.assembly.turbine_class, "I");
         ASSERT_EQ(turbine.assembly.turbulence_class, "B");
         ASSERT_EQ(turbine.assembly.drivetrain, "direct_drive");
@@ -43,7 +30,7 @@ TEST(ParserTest, ParseIEA15MW) {
         ASSERT_EQ(turbine.assembly.rated_power, 15.e+6);
         ASSERT_EQ(turbine.assembly.lifetime, 25.);
 
-        // Assert there are 11 materials
+        // Assert the materials
         ASSERT_EQ(turbine.materials.size(), 11);
     } catch (const YAML::Exception& e) {
         std::cerr << "Error loading YAML file: " << e.what() << std::endl;
