59d3277b2259f37cea6bf89b947f60766a2a2cbc
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2024-08-08 17:57:18 -0600

    Refactor to increase clarity + readability

--- cmake/PreventInSourceBuilds.cmake
+++ cmake/PreventInSourceBuilds.cmake
@@ -1,16 +1,22 @@
 function(openturbine_assure_out_of_source_builds)
-  # make sure the user doesn't play dirty with symlinks
+  # Get the real paths of the source and binary directories
   get_filename_component(srcdir "${CMAKE_SOURCE_DIR}" REALPATH)
   get_filename_component(bindir "${CMAKE_BINARY_DIR}" REALPATH)
 
-  # disallow in-source builds
+  # Disallow in-source builds
   if("${srcdir}" STREQUAL "${bindir}")
-    message("######################################################")
-    message("Warning: in-source builds are disabled")
-    message("Please create a separate build directory and run cmake from there")
-    message("######################################################")
-    message(FATAL_ERROR "Quitting configuration")
+    message(FATAL_ERROR
+      "######################################################\n"
+      "Error: In-source builds are disabled.\n"
+      "Please create a separate build directory and run CMake from there.\n"
+      "Example:\n"
+      "  mkdir -p build\n"
+      "  cd build\n"
+      "  cmake ..\n"
+      "######################################################"
+    )
   endif()
 endfunction()
 
+# Invoke the function to enforce the out-of-source build
 openturbine_assure_out_of_source_builds()
