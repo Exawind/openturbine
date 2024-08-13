5df25f4b7aa2f7f941ff04ac4b29a39c6c2ee1aa
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2024-08-12 19:28:42 -0600

    Further simplify the tests

--- tests/unit_tests/restruct_poc/test_yaml_parser.cpp
+++ tests/unit_tests/restruct_poc/test_yaml_parser.cpp
@@ -8,44 +8,32 @@
 namespace openturbine::restruct_poc::tests {
 
 TEST(ParserTest, ParseIEA15MWBasicInfo) {
-    try {
-        const YAML::Node config = YAML::LoadFile("../src/utilities/scripts/IEA-15-240-RWT.yaml");
-        Turbine turbine;
-        turbine.parse(config);
-        ASSERT_EQ(turbine.name, "IEA 15MW Offshore Reference Turbine, with taped chord tip design");
-    } catch (const YAML::Exception& e) {
-        std::cerr << "Error loading YAML file: " << e.what() << "\n";
-    }
+    const YAML::Node config = YAML::LoadFile("../src/utilities/scripts/IEA-15-240-RWT.yaml");
+    Turbine turbine;
+    turbine.parse(config);
+    ASSERT_EQ(turbine.name, "IEA 15MW Offshore Reference Turbine, with taped chord tip design");
 }
 
 TEST(ParserTest, ParseIEA15MWAssembly) {
-    try {
-        const YAML::Node config = YAML::LoadFile("../src/utilities/scripts/IEA-15-240-RWT.yaml");
-        Turbine turbine;
-        turbine.parse(config);
-        ASSERT_EQ(turbine.assembly.turbine_class, "I");
-        ASSERT_EQ(turbine.assembly.turbulence_class, "B");
-        ASSERT_EQ(turbine.assembly.drivetrain, "direct_drive");
-        ASSERT_EQ(turbine.assembly.rotor_orientation, "Upwind");
-        ASSERT_EQ(turbine.assembly.number_of_blades, 3);
-        ASSERT_EQ(turbine.assembly.hub_height, 150.);
-        ASSERT_EQ(turbine.assembly.rotor_diameter, 241.94);
-        ASSERT_EQ(turbine.assembly.rated_power, 15.e+6);
-        ASSERT_EQ(turbine.assembly.lifetime, 25.);
-    } catch (const YAML::Exception& e) {
-        std::cerr << "Error loading YAML file: " << e.what() << "\n";
-    }
+    const YAML::Node config = YAML::LoadFile("../src/utilities/scripts/IEA-15-240-RWT.yaml");
+    Turbine turbine;
+    turbine.parse(config);
+    ASSERT_EQ(turbine.assembly.turbine_class, "I");
+    ASSERT_EQ(turbine.assembly.turbulence_class, "B");
+    ASSERT_EQ(turbine.assembly.drivetrain, "direct_drive");
+    ASSERT_EQ(turbine.assembly.rotor_orientation, "Upwind");
+    ASSERT_EQ(turbine.assembly.number_of_blades, 3);
+    ASSERT_EQ(turbine.assembly.hub_height, 150.);
+    ASSERT_EQ(turbine.assembly.rotor_diameter, 241.94);
+    ASSERT_EQ(turbine.assembly.rated_power, 15.e+6);
+    ASSERT_EQ(turbine.assembly.lifetime, 25.);
 }
 
 TEST(ParserTest, ParseIEA15MWMaterials) {
-    try {
-        const YAML::Node config = YAML::LoadFile("../src/utilities/scripts/IEA-15-240-RWT.yaml");
-        Turbine turbine;
-        turbine.parse(config);
-        ASSERT_EQ(turbine.materials.size(), 11);
-    } catch (const YAML::Exception& e) {
-        std::cerr << "Error loading YAML file: " << e.what() << "\n";
-    }
+    const YAML::Node config = YAML::LoadFile("../src/utilities/scripts/IEA-15-240-RWT.yaml");
+    Turbine turbine;
+    turbine.parse(config);
+    ASSERT_EQ(turbine.materials.size(), 11);
 }
 
 }  // namespace openturbine::restruct_poc::tests
