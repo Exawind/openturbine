6487c297a1db2bfb7c5ba30ca3f3ae103fc89503
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2024-08-12 20:20:51 -0600

    Make the build type checking more robust and output the build type to console

--- cmake/StandardProjectSettings.cmake
+++ cmake/StandardProjectSettings.cmake
@@ -5,7 +5,26 @@ if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
 
   # Set the possible values of build type for cmake-gui, ccmake
   set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
+
+# Validate the build type if it was specified
+elseif(CMAKE_BUILD_TYPE)
+  # Make build type case-insensitive and check for supported values
+  set(SUPPORTED_BUILD_TYPES Debug Release RelWithDebInfo MinSizeRel)
+  string(REPLACE ";" " " SUPPORTED_BUILD_TYPES_STRING "${SUPPORTED_BUILD_TYPES}")
+
+  string(TOUPPER "${CMAKE_BUILD_TYPE}" BUILD_TYPE_UPPER)
+  string(TOUPPER "${SUPPORTED_BUILD_TYPES}" SUPPORTED_BUILD_TYPES_UPPER)
+
+  if(NOT BUILD_TYPE_UPPER IN_LIST SUPPORTED_BUILD_TYPES_UPPER)
+    message(WARNING "Build type ${CMAKE_BUILD_TYPE} is NOT supported - reverting to RelWithDebInfo.")
+    set(CMAKE_BUILD_TYPE "RelWithDebInfo" CACHE STRING "Choose the type of build." FORCE)
+  endif()
+
 endif()
+
+# Print the build type to the console
+string(TOUPPER "${CMAKE_BUILD_TYPE}" CMAKE_BUILD_TYPE_UPPER)
+message(STATUS "Build type: ${CMAKE_BUILD_TYPE_UPPER}")
 
 # Generate compile_commands.json to make it easier to work with clang-based tools
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
