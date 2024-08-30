a77429cdf3551f23d131cd6c0d0174e231bf73c4
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2024-08-30 14:35:23 -0600

    Remove control* from DeviceData struct of Constraints since not required

--- src/restruct_poc/solver/constraints.hpp
+++ src/restruct_poc/solver/constraints.hpp
@@ -26,7 +26,6 @@ struct Constraints {
         double axis_x[3];          // Unit vector representing x rotation axis
         double axis_y[3];          // Unit vector representing y rotation axis
         double axis_z[3];          // Unit vector representing z rotation axis
-        double* control;           //< Control signal
     };
 
     /// @brief HostData struct holds constraint data on the host
