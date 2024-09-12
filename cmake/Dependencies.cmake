d5785cf701e97a2a0886477822c95f9394398099
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2024-09-11 22:01:05 -0600

    Move all cmake helper files into the cmake folder to keep things tidy

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -5,18 +5,18 @@ function(openturbine_setup_dependencies)
   find_package(yaml-cpp REQUIRED)
 
   # Add external project to build the OpenFAST/AerodynInflow(ADI) library
-  include(ExternalProject)
-  ExternalProject_Add(OpenFAST_ADI
-    PREFIX ${CMAKE_BINARY_DIR}/../OpenFAST_ADI
-    GIT_REPOSITORY https://github.com/OpenFAST/openfast.git
-    GIT_TAG dev # Use the branch dev
-    GIT_SHALLOW TRUE # Only clone the latest commit
-    GIT_SUBMODULES_RECURSE FALSE # Do not clone submodules
-    CMAKE_ARGS -DBUILD_TESTING=OFF # Do not build tests
-    BUILD_IN_SOURCE FALSE # Build in a separate directory
-    BUILD_COMMAND ${CMAKE_COMMAND} --build . --target aerodyn_inflow_c_binding # Build only the ADI library
-    INSTALL_COMMAND ""
-  )
+  # include(ExternalProject)
+  # ExternalProject_Add(OpenFAST_ADI
+  #   PREFIX ${CMAKE_BINARY_DIR}/../OpenFAST_ADI
+  #   GIT_REPOSITORY https://github.com/OpenFAST/openfast.git
+  #   GIT_TAG dev # Use the branch dev
+  #   GIT_SHALLOW TRUE # Only clone the latest commit
+  #   GIT_SUBMODULES_RECURSE FALSE # Do not clone submodules
+  #   CMAKE_ARGS -DBUILD_TESTING=OFF # Do not build tests
+  #   BUILD_IN_SOURCE FALSE # Build in a separate directory
+  #   BUILD_COMMAND ${CMAKE_COMMAND} --build . --target aerodyn_inflow_c_binding # Build only the ADI library
+  #   INSTALL_COMMAND ""
+  # )
 
   # Optionally find and link MKL if available
   if(TARGET Kokkos::MKL)
