711bf58f36bc52721d020ccc825feb554117794b
Derek Slaughter <deslaughter@gmail.com>
2024-10-11 16:38:17 +0000

    Put IEA15 files in folder

---
+++ tests/unit_tests/external/IEA-15-240-RWT/wind.hh
@@ -0,0 +1,6 @@
+! Wind increasing from rated @ 10.59 m/s to 25 m/s over 1 minute.
+! Time  Wind    Wind    Vert.   Horiz.  Vert.   LinV    Gust
+!       Speed   Dir     Speed   Shear   Shear   Shear   Speed
+  0.0   10.59   0.0     0.0     0.0     0.0     0.0     0.0
+ 10.0   10.59   0.0     0.0     0.0     0.0     0.0     0.0
+ 20.0   25.00   0.0     0.0     0.0     0.0     0.0     0.0
