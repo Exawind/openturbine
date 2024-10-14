225fc07bee13279aed3493884b407f38c0a23ef3
Derek Slaughter <deslaughter@gmail.com>
2024-10-14 21:41:17 +0000

    Reduce duration of IEA15RotorAeroController test

--- tests/unit_tests/external/test_rotor_aero_controller.cpp
+++ tests/unit_tests/external/test_rotor_aero_controller.cpp
@@ -78,8 +78,8 @@ TEST(Milestone, IEA15RotorAeroController) {
     constexpr size_t max_iter{6};
     constexpr double step_size{0.01};  // seconds
     constexpr double rho_inf{0.0};
-    // constexpr double t_end{5.0 * M_PI / rotor_speed_init};  // 3 revolutions
-    constexpr double t_end{60.0};  // seconds
+    // constexpr double t_end{60.0};  // seconds
+    constexpr double t_end{1.0};  // seconds
     constexpr auto num_steps{static_cast<size_t>(t_end / step_size + 1.)};
 
     // Create model for adding nodes and constraints
