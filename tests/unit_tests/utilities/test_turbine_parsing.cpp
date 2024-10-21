8c3d1e5c001b5236360d0e1330511f5d16031b83
dcdemen <dcdemen@sandia.gov>
2024-10-21 12:33:07 -0600

    Moved parsing regression tests to be unit tests with no external file dependency

--- tests/unit_tests/utilities/test_turbine_parsing.cpp
+++ tests/unit_tests/utilities/test_turbine_parsing.cpp
@@ -1,25 +1,21 @@
 #include <gtest/gtest.h>
 #include <yaml-cpp/yaml.h>
 
-#include "test_utilities.hpp"
-
 #include "src/utilities/scripts/windio_mapped_structs.hpp"
 
 namespace openturbine::tests {
 
-TEST(ParserTest, ParseIEA15MWBasicInfo) {
-    const std::filesystem::path projectRoot = FindProjectRoot();
-    const std::filesystem::path yamlPath = projectRoot / "src/utilities/scripts/IEA-15-240-RWT.yaml";
-    const YAML::Node config = YAML::LoadFile(yamlPath.string());
+TEST(ParserTest, ParseIEA15MWName) {
+    const std::string input = "name: IEA 15MW Offshore Reference Turbine, with taped chord tip design";
+    const YAML::Node config = YAML::Load(input);
     Turbine turbine;
     turbine.parse(config);
     ASSERT_EQ(turbine.name, "IEA 15MW Offshore Reference Turbine, with taped chord tip design");
 }
 
 TEST(ParserTest, ParseIEA15MWAssembly) {
-    const std::filesystem::path projectRoot = FindProjectRoot();
-    const std::filesystem::path yamlPath = projectRoot / "src/utilities/scripts/IEA-15-240-RWT.yaml";
-    const YAML::Node config = YAML::LoadFile(yamlPath.string());
+    const std::string input = "assembly:\n   turbine_class: I\n   turbulence_class: B\n   drivetrain: direct_drive\n   rotor_orientation: Upwind\n   number_of_blades: 3\n   hub_height: 150\n   rotor_diameter: 241.94\n   rated_power: 15.e+6\n   lifetime: 25\n";
+    const YAML::Node config = YAML::Load(input);
     Turbine turbine;
     turbine.parse(config);
     ASSERT_EQ(turbine.assembly.turbine_class, "I");
@@ -33,13 +29,14 @@ TEST(ParserTest, ParseIEA15MWAssembly) {
     ASSERT_EQ(turbine.assembly.lifetime, 25.);
 }
 
-TEST(ParserTest, ParseIEA15MWMaterials) {
-    const std::filesystem::path projectRoot = FindProjectRoot();
-    const std::filesystem::path yamlPath = projectRoot / "src/utilities/scripts/IEA-15-240-RWT.yaml";
-    const YAML::Node config = YAML::LoadFile(yamlPath.string());
+TEST(ParserTest, ParseIEA15MWMultipleMaterials) {
+    const std::string input = "materials:\n   [name: first, name: second]"; 
+    const YAML::Node config = YAML::Load(input);
     Turbine turbine;
     turbine.parse(config);
-    ASSERT_EQ(turbine.materials.size(), 11);
+    EXPECT_EQ(turbine.materials.size(), 2);
+    EXPECT_EQ(turbine.materials[0].name, "first");
+    EXPECT_EQ(turbine.materials[1].name, "second");
 }
 
 }  // namespace openturbine::tests
