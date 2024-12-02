7b8aab47fb94107d5d573c4518ae1838257ea8f2
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2024-12-01 21:15:42 -0700

    Add some helpful annotations to InterpolateQPPosition to increase readability of this function object

--- src/elements/beams/interpolate_QP_position.hpp
+++ src/elements/beams/interpolate_QP_position.hpp
@@ -4,18 +4,27 @@
 
 namespace openturbine {
 
+/**
+ * @brief Interpolates quadrature point positions from nodal positions using shape functions
+ *
+ * This functor computes the global positions of quadrature points by interpolating
+ * from the nodal positions using shape functions:
+ * - Takes nodal positions and rotations for each element
+ * - Uses pre-computed shape function weights for interpolation
+ * - Computes the position vector (x,y,z) for each quadrature point
+ */
 struct InterpolateQPPosition {
     Kokkos::View<size_t*>::const_type num_nodes_per_element;
     Kokkos::View<size_t*>::const_type num_qps_per_element;
-    Kokkos::View<double***>::const_type shape_interpolation;  // Num Nodes x Num Quadrature points
-    Kokkos::View<double** [7]>::const_type node_position_rotation;  // Node global position vector
-    Kokkos::View<double** [3]> qp_position;                         // quadrature point position
+    Kokkos::View<double***>::const_type shape_interpolation;  //< num_elem x num_nodes x num_qps
+    Kokkos::View<double** [7]>::const_type
+        node_position_rotation;  //< node position vector/generalized coordinates in global csys
+    Kokkos::View<double** [3]> qp_position;  //< quadrature point position - x, y, z (computed)
 
     KOKKOS_FUNCTION
     void operator()(const int i_elem) const {
         const auto num_nodes = num_nodes_per_element(i_elem);
         const auto num_qps = num_qps_per_element(i_elem);
-
         for (auto j = 0U; j < num_qps; ++j) {
             auto local_result = Kokkos::Array<double, 3>{};
             for (auto i = 0U; i < num_nodes; ++i) {
