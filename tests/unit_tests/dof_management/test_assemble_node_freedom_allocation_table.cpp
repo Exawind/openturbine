0f9caebe55addb2f8ac640fdc7393afc7616bfe4
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2024-12-05 12:11:07 -0700

    Add a no beam element, 1 mass element test case to the unit test suite of assemble_node_freedom_allocation_table()

--- tests/unit_tests/dof_management/test_assemble_node_freedom_allocation_table.cpp
+++ tests/unit_tests/dof_management/test_assemble_node_freedom_allocation_table.cpp
@@ -4,14 +4,13 @@
 
 namespace openturbine::tests {
 
-TEST(TestAssembleNodeFreedomAllocationTable, OneBeamOneNode_NoMass) {
-    auto state = State(2U);
+TEST(TestAssembleNodeFreedomAllocationTable, OneBeamElementWithOneNode_NoMassElement) {
+    auto state = State(1U);  // 1 node in the system
 
-    // Create Beams object and wrap in Elements
-    auto beams = std::make_shared<Beams>(1U, 1U, 1U);
+    auto beams = std::make_shared<Beams>(1U, 1U, 1U);  // 1 beam element with 1 node per element
     Kokkos::deep_copy(beams->node_state_indices, 0U);
     Kokkos::deep_copy(beams->num_nodes_per_element, 1U);
-    auto elements = Elements{beams};
+    auto elements = Elements{beams};  // No mass elements in the model
 
     auto constraints = Constraints(std::vector<std::shared_ptr<Constraint>>{});
 
@@ -24,14 +23,13 @@ TEST(TestAssembleNodeFreedomAllocationTable, OneBeamOneNode_NoMass) {
     EXPECT_EQ(host_node_freedom_allocation_table(0), FreedomSignature::AllComponents);
 }
 
-TEST(TestAssembleNodeFreedomAllocationTable, OneBeamOneNode_OneMassOneNode) {
-    auto state = State(2U);
+TEST(TestAssembleNodeFreedomAllocationTable, OneBeamElementWithOneNode_OneMassElementWithOneNode) {
+    auto state = State(2U);  // 2 nodes in the system
 
-    // Create Beams and Masses objects and wrap in Elements
-    auto beams = std::make_shared<Beams>(1U, 1U, 1U);
+    auto beams = std::make_shared<Beams>(1U, 1U, 1U);  // 1 beam element with 1 node per element
     Kokkos::deep_copy(beams->node_state_indices, 0U);
     Kokkos::deep_copy(beams->num_nodes_per_element, 1U);
-    auto masses = std::make_shared<Masses>(1U);
+    auto masses = std::make_shared<Masses>(1U);  // 1 mass element with 1 node
     Kokkos::deep_copy(masses->state_indices, 1U);
     auto elements = Elements{beams, masses};
 
@@ -47,10 +45,28 @@ TEST(TestAssembleNodeFreedomAllocationTable, OneBeamOneNode_OneMassOneNode) {
     EXPECT_EQ(host_node_freedom_allocation_table(1), FreedomSignature::AllComponents);
 }
 
-TEST(TestAssembleNodeFreedomAllocationTable, OneBeamTwoNodes_NoMass) {
-    auto state = State(2U);
+TEST(TestAssembleNodeFreedomAllocationTable, NoBeamElements_OneMassElementWithOneNode) {
+    auto state = State(1U);  // 1 node in the system
 
-    auto beams = std::make_shared<Beams>(1U, 2U, 1U);
+    auto masses = std::make_shared<Masses>(1U);  // 1 mass element with 1 node
+    Kokkos::deep_copy(masses->state_indices, 0U);
+    auto elements = Elements{nullptr, masses};  // No beam elements in the model
+
+    auto constraints = Constraints(std::vector<std::shared_ptr<Constraint>>{});
+
+    assemble_node_freedom_allocation_table(state, elements, constraints);
+
+    const auto host_node_freedom_allocation_table =
+        Kokkos::create_mirror(state.node_freedom_allocation_table);
+    Kokkos::deep_copy(host_node_freedom_allocation_table, state.node_freedom_allocation_table);
+
+    EXPECT_EQ(host_node_freedom_allocation_table(0), FreedomSignature::AllComponents);
+}
+
+TEST(TestAssembleNodeFreedomAllocationTable, OneBeamElementWithTwoNodes_NoMassElement) {
+    auto state = State(2U);  // 2 nodes in the system
+
+    auto beams = std::make_shared<Beams>(1U, 2U, 1U);  // 1 beam element with 2 nodes per element
     constexpr auto host_node_state_indices_data = std::array{0UL, 1UL};
     const auto host_node_state_indices =
         Kokkos::View<size_t[1][2], Kokkos::HostSpace>::const_type(host_node_state_indices_data.data()
@@ -59,7 +75,7 @@ TEST(TestAssembleNodeFreedomAllocationTable, OneBeamTwoNodes_NoMass) {
     Kokkos::deep_copy(mirror_node_state_indices, host_node_state_indices);
     Kokkos::deep_copy(beams->node_state_indices, mirror_node_state_indices);
     Kokkos::deep_copy(beams->num_nodes_per_element, 2U);
-    auto elements = Elements{beams};
+    auto elements = Elements{beams};  // No mass elements in the model
 
     auto constraints = Constraints(std::vector<std::shared_ptr<Constraint>>{});
 
@@ -73,10 +89,10 @@ TEST(TestAssembleNodeFreedomAllocationTable, OneBeamTwoNodes_NoMass) {
     EXPECT_EQ(host_node_freedom_allocation_table(1), FreedomSignature::AllComponents);
 }
 
-TEST(TestAssembleNodeFreedomAllocationTable, TwoBeamsOneNode_NoMass) {
-    auto state = State(2U);
+TEST(TestAssembleNodeFreedomAllocationTable, TwoBeamElementsWithOneNode_NoMassElement) {
+    auto state = State(2U);  // 2 nodes in the system
 
-    auto beams = std::make_shared<Beams>(2U, 1U, 1U);
+    auto beams = std::make_shared<Beams>(2U, 1U, 1U);  // 2 beam elements with 1 node per element
     constexpr auto host_node_state_indices_data = std::array{0UL, 1UL};
     const auto host_node_state_indices =
         Kokkos::View<size_t[2][1], Kokkos::HostSpace>::const_type(host_node_state_indices_data.data()
@@ -85,7 +101,8 @@ TEST(TestAssembleNodeFreedomAllocationTable, TwoBeamsOneNode_NoMass) {
     Kokkos::deep_copy(mirror_node_state_indices, host_node_state_indices);
     Kokkos::deep_copy(beams->node_state_indices, mirror_node_state_indices);
     Kokkos::deep_copy(beams->num_nodes_per_element, 1U);
-    auto elements = Elements{beams};
+    auto elements = Elements{beams};  // No mass elements in the model
+
     auto constraints = Constraints(std::vector<std::shared_ptr<Constraint>>{});
 
     assemble_node_freedom_allocation_table(state, elements, constraints);
