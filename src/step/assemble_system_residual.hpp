11e8043d3b8c210ab1d8160129b5e8e57975e90d
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2024-12-09 13:48:07 -0700

    Refactor AssembleSystemResidual to separate out method for assembling the Beams residual

--- src/step/assemble_system_residual.hpp
+++ src/step/assemble_system_residual.hpp
@@ -3,26 +3,25 @@
 #include <Kokkos_Core.hpp>
 #include <Kokkos_Profiling_ScopedRegion.hpp>
 
-#include "src/elements/beams/beams.hpp"
-#include "src/solver/contribute_elements_to_vector.hpp"
+#include "assemble_system_residual_beams.hpp"
+
+#include "src/elements/elements.hpp"
 #include "src/solver/solver.hpp"
 
 namespace openturbine {
 
-inline void AssembleSystemResidual(Solver& solver, Beams& beams) {
+inline void AssembleSystemResidual(Solver& solver, Elements& elements) {
     auto region = Kokkos::Profiling::ScopedRegion("Assemble System Residual");
 
     const auto num_rows = solver.num_system_dofs;
-
     Kokkos::deep_copy(Kokkos::subview(solver.R, Kokkos::make_pair(size_t{0U}, num_rows)), 0.);
-    auto vector_policy = Kokkos::TeamPolicy<>(static_cast<int>(beams.num_elems), Kokkos::AUTO());
-    Kokkos::parallel_for(
-        "ContributeElementsToVector", vector_policy,
-        ContributeElementsToVector{
-            beams.num_nodes_per_element, beams.element_freedom_table, beams.residual_vector_terms,
-            solver.R
-        }
-    );
+
+    // Assemble Beams residual
+    if (elements.beams) {
+        AssembleSystemResidualBeams(solver, *elements.beams);
+    }
+
+    // TODO: Assemble Masses residual
 }
 
 }  // namespace openturbine
