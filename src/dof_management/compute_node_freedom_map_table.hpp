f84791d1ed837dd6b66e6cee495ef15409169f04
dcdemen <dcdemen@sandia.gov>
2024-12-09 11:07:42 -0700

    Parallelized compute_node_freedom_map_table

--- src/dof_management/compute_node_freedom_map_table.hpp
+++ src/dof_management/compute_node_freedom_map_table.hpp
@@ -9,17 +9,25 @@
 
 namespace openturbine {
 
-inline void compute_node_freedom_map_table(State& state) {
-    Kokkos::parallel_for(
-        "Compute Node Freedom Map Table", 1,
-        KOKKOS_LAMBDA(size_t) {
-            state.node_freedom_map_table(0) = 0U;
-            for (auto i = 1U; i < state.num_system_nodes; ++i) {
-                const auto num_dof = count_active_dofs(state.node_freedom_allocation_table(i - 1));
-                state.node_freedom_map_table(i) = state.node_freedom_map_table(i - 1) + num_dof;
-            }
+struct ComputeNodeFreedomMapTable {
+    Kokkos::View<FreedomSignature*>::const_type node_freedom_allocation_table;
+    Kokkos::View<size_t*> node_freedom_map_table;
+
+    KOKKOS_FUNCTION
+    void operator()(size_t i, size_t& update, bool is_final) const {
+        const auto num_dof = count_active_dofs(node_freedom_allocation_table(i));
+        update += num_dof;
+        if (is_final) {
+            node_freedom_map_table(i+1) = update;
         }
-    );
+
+    }
+};
+
+inline void compute_node_freedom_map_table(State& state) {
+    Kokkos::deep_copy(state.node_freedom_map_table, 0UL);
+    auto result = 0UL;
+    Kokkos::parallel_scan("Compute Node Freedom Map Table", state.num_system_nodes, ComputeNodeFreedomMapTable{state.node_freedom_allocation_table, state.node_freedom_map_table}, result);
 }
 
 }  // namespace openturbine
