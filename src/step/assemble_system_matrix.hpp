f97a1b494ade0a7512a1ed4348341b44ba32cd70
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2024-12-09 13:59:43 -0700

    Refactor AssembleSystemMatrix to separate out the logic for assembling Beams system matrix into a separate function

--- src/step/assemble_system_matrix.hpp
+++ src/step/assemble_system_matrix.hpp
@@ -1,63 +1,24 @@
 #pragma once
 
-#include <KokkosBlas.hpp>
-#include <KokkosSparse.hpp>
-#include <KokkosSparse_spadd.hpp>
 #include <Kokkos_Core.hpp>
 #include <Kokkos_Profiling_ScopedRegion.hpp>
 
-#include "src/elements/beams/beams.hpp"
-#include "src/solver/contribute_elements_to_sparse_matrix.hpp"
-#include "src/solver/copy_into_sparse_matrix.hpp"
+#include "assemble_system_matrix_beams.hpp"
+
+#include "src/elements/elements.hpp"
 #include "src/solver/solver.hpp"
 
 namespace openturbine {
 
-inline void AssembleSystemMatrix(Solver& solver, Beams& beams) {
+inline void AssembleSystemMatrix(Solver& solver, Elements& elements) {
     auto region = Kokkos::Profiling::ScopedRegion("Assemble System Matrix");
 
-    const auto num_beam_dofs = 6U;
-    const auto row_data_size = Kokkos::View<double*>::shmem_size(num_beam_dofs);
-    const auto col_idx_size = Kokkos::View<int*>::shmem_size(num_beam_dofs);
-    auto sparse_matrix_policy =
-        Kokkos::TeamPolicy<>(static_cast<int>(beams.num_elems), Kokkos::AUTO());
-
-    sparse_matrix_policy.set_scratch_size(1, Kokkos::PerThread(row_data_size + col_idx_size));
-    Kokkos::deep_copy(solver.K.values, 0.);
-    Kokkos::parallel_for(
-        "ContributeElementsToSparseMatrix", sparse_matrix_policy,
-        ContributeElementsToSparseMatrix<Solver::CrsMatrixType>{
-            beams.num_nodes_per_element, beams.element_freedom_signature,
-            beams.element_freedom_table, beams.stiffness_matrix_terms, solver.K
-        }
-    );
-
-    Kokkos::fence();
-    {
-        auto static_region = Kokkos::Profiling::ScopedRegion("Assemble Static System Matrix");
-        KokkosSparse::spgemm_numeric(
-            solver.system_spgemm_handle, solver.K, false, solver.T, false,
-            solver.static_system_matrix
-        );
+    // Assemble Beams matrix
+    if (elements.beams) {
+        AssembleSystemMatrixBeams(solver, *elements.beams);
     }
 
-    Kokkos::deep_copy(solver.K.values, 0.);
-    Kokkos::parallel_for(
-        "ContributeElementsToSparseMatrix", sparse_matrix_policy,
-        ContributeElementsToSparseMatrix<Solver::CrsMatrixType>{
-            beams.num_nodes_per_element, beams.element_freedom_signature,
-            beams.element_freedom_table, beams.inertia_matrix_terms, solver.K
-        }
-    );
-
-    Kokkos::fence();
-    {
-        auto system_region = Kokkos::Profiling::ScopedRegion("Assemble System Matrix");
-        KokkosSparse::spadd_numeric(
-            &solver.system_spadd_handle, 1., solver.K, 1., solver.static_system_matrix,
-            solver.system_matrix
-        );
-    }
+    // TODO: Assemble Masses matrix
 }
 
 }  // namespace openturbine
