6892fd83cb7abe9127d0bd66f049cbb3c333b0cf
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2024-12-10 16:20:56 -0700

    Revert "Split UpdateNodeStateElement -> UpdateNodeStateBeamElement and newly added UpdateNodeStateMassElement to handle updating the state for Masses"
    This reverts commit 0cce9f96665d5e7e6fc43719e4bb741e69de0446.

--- src/system/update_node_state.hpp
+++ src/system/update_node_state.hpp
@@ -5,9 +5,9 @@
 namespace openturbine {
 
 /**
- * @brief Functor to update nodal states for a single beamelement from global state vectors
+ * @brief Functor to update nodal states for a single element from global state vectors
  */
-struct UpdateNodeStateBeamElement {
+struct UpdateNodeStateElement {
     size_t i_elem;
     Kokkos::View<size_t**>::const_type node_state_indices;
     Kokkos::View<double** [7]> node_u;
@@ -55,37 +55,10 @@ struct UpdateNodeState {
 
         Kokkos::parallel_for(
             Kokkos::TeamThreadRange(member, num_nodes),
-            UpdateNodeStateBeamElement{
+            UpdateNodeStateElement{
                 i_elem, node_state_indices, node_u, node_u_dot, node_u_ddot, Q, V, A
             }
         );
-    }
-};
-
-/**
- * @brief Functor to update node states for a single mass element from global state vectors
- */
-struct UpdateNodeStateMassElement {
-    size_t i_elem;
-    Kokkos::View<size_t*>::const_type state_indices;
-    Kokkos::View<double* [7]> u;
-    Kokkos::View<double* [6]> u_dot;
-    Kokkos::View<double* [6]> u_ddot;
-
-    Kokkos::View<double* [7]>::const_type Q;
-    Kokkos::View<double* [6]>::const_type V;
-    Kokkos::View<double* [6]>::const_type A;
-
-    KOKKOS_FUNCTION
-    void operator()(const size_t) const {
-        const auto j = state_indices(i_elem);
-        for (auto k = 0U; k < 7U; ++k) {
-            u(i_elem, k) = Q(j, k);
-        }
-        for (auto k = 0U; k < 6U; ++k) {
-            u_dot(i_elem, k) = V(j, k);
-            u_ddot(i_elem, k) = A(j, k);
-        }
     }
 };
 
--- tests/unit_tests/system/test_update_node_state.cpp
+++ tests/unit_tests/system/test_update_node_state.cpp
@@ -52,8 +52,8 @@ TEST(UpdateNodeStateTests, TwoNodes_InOrder) {
     const auto node_u_ddot = Kokkos::View<double[1][2][6]>("node_u_ddot");
 
     Kokkos::parallel_for(
-        "UpdateNodeStateBeamElement", 2,
-        UpdateNodeStateBeamElement{0U, indices, node_u, node_u_dot, node_u_ddot, Q, V, A}
+        "UpdateNodeStateElement", 2,
+        UpdateNodeStateElement{0U, indices, node_u, node_u_dot, node_u_ddot, Q, V, A}
     );
 
     const auto node_u_mirror = Kokkos::create_mirror(node_u);
@@ -116,8 +116,8 @@ TEST(UpdateNodeStateTests, TwoNodes_OutOfOrder) {
     const auto node_u_ddot = Kokkos::View<double[1][2][6]>("node_u_ddot");
 
     Kokkos::parallel_for(
-        "UpdateNodeStateBeamElement", 2,
-        UpdateNodeStateBeamElement{0U, indices, node_u, node_u_dot, node_u_ddot, Q, V, A}
+        "UpdateNodeStateElement", 2,
+        UpdateNodeStateElement{0U, indices, node_u, node_u_dot, node_u_ddot, Q, V, A}
     );
 
     const auto node_u_mirror = Kokkos::create_mirror(node_u);
@@ -131,63 +131,6 @@ TEST(UpdateNodeStateTests, TwoNodes_OutOfOrder) {
     const auto node_u_ddot_mirror = Kokkos::create_mirror(node_u_ddot);
     Kokkos::deep_copy(node_u_ddot_mirror, node_u_ddot);
     CompareOutOfOrder(node_u_ddot_mirror, A_host);
-}
-
-TEST(UpdateNodeStateTests, SingleMassElement) {
-    const auto indices = Kokkos::View<size_t[1]>("state_indices");
-    constexpr auto indices_data = std::array<size_t, 1>{0U};
-    const auto indices_host = Kokkos::View<const size_t[1], Kokkos::HostSpace>(indices_data.data());
-    const auto indices_mirror = Kokkos::create_mirror(indices);
-    Kokkos::deep_copy(indices_mirror, indices_host);
-    Kokkos::deep_copy(indices, indices_mirror);
-
-    const auto Q = Kokkos::View<double[1][7]>("Q");
-    constexpr auto Q_data = std::array{1., 2., 3., 4., 5., 6., 7.};
-    const auto Q_host = Kokkos::View<const double[1][7], Kokkos::HostSpace>(Q_data.data());
-    const auto Q_mirror = Kokkos::create_mirror(Q);
-    Kokkos::deep_copy(Q_mirror, Q_host);
-    Kokkos::deep_copy(Q, Q_mirror);
-
-    const auto V = Kokkos::View<double[1][6]>("V");
-    constexpr auto V_data = std::array{8., 9., 10., 11., 12., 13.};
-    const auto V_host = Kokkos::View<const double[1][6], Kokkos::HostSpace>(V_data.data());
-    const auto V_mirror = Kokkos::create_mirror(V);
-    Kokkos::deep_copy(V_mirror, V_host);
-    Kokkos::deep_copy(V, V_mirror);
-
-    const auto A = Kokkos::View<double[1][6]>("A");
-    constexpr auto A_data = std::array{14., 15., 16., 17., 18., 19.};
-    const auto A_host = Kokkos::View<const double[1][6], Kokkos::HostSpace>(A_data.data());
-    const auto A_mirror = Kokkos::create_mirror(A);
-    Kokkos::deep_copy(A_mirror, A_host);
-    Kokkos::deep_copy(A, A_mirror);
-
-    const auto u = Kokkos::View<double[1][7]>("u");
-    const auto u_dot = Kokkos::View<double[1][6]>("u_dot");
-    const auto u_ddot = Kokkos::View<double[1][6]>("u_ddot");
-
-    Kokkos::parallel_for(
-        "UpdateNodeStateMassElement", 1,
-        UpdateNodeStateMassElement{0U, indices, u, u_dot, u_ddot, Q, V, A}
-    );
-
-    const auto u_mirror = Kokkos::create_mirror(u);
-    Kokkos::deep_copy(u_mirror, u);
-    for (auto j = 0U; j < 7U; ++j) {
-        EXPECT_EQ(u_mirror(0, j), Q_host(0, j));
-    }
-
-    const auto u_dot_mirror = Kokkos::create_mirror(u_dot);
-    Kokkos::deep_copy(u_dot_mirror, u_dot);
-    for (auto j = 0U; j < 6U; ++j) {
-        EXPECT_EQ(u_dot_mirror(0, j), V_host(0, j));
-    }
-
-    const auto u_ddot_mirror = Kokkos::create_mirror(u_ddot);
-    Kokkos::deep_copy(u_ddot_mirror, u_ddot);
-    for (auto j = 0U; j < 6U; ++j) {
-        EXPECT_EQ(u_ddot_mirror(0, j), A_host(0, j));
-    }
 }
 
 }  // namespace openturbine::tests
