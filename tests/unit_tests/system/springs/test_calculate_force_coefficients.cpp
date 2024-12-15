f2e2a033e9f3b444e0d7e08458ebf6ea6a64760f
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2024-12-15 11:53:51 -0800

    Add kernel to calculate the force calc. coeffs c1 and c2

---
+++ tests/unit_tests/system/springs/test_calculate_force_coefficients.cpp
@@ -0,0 +1,64 @@
+#include <Kokkos_Core.hpp>
+#include <gtest/gtest.h>
+
+#include "src/system/springs/calculate_force_coefficients.hpp"
+
+namespace openturbine::tests {
+
+TEST(CalculateForceCalculationCoefficientsTests, ThreeElements) {
+    const auto k = Kokkos::View<double[3]>("k");
+    const auto l_ref = Kokkos::View<double[3]>("l_ref");
+    const auto l = Kokkos::View<double[3]>("l");
+    const auto c1 = Kokkos::View<double[3]>("c1");
+    const auto c2 = Kokkos::View<double[3]>("c2");
+
+    constexpr auto k_data = std::array{100., 200., 300.};  // Spring constants
+    constexpr auto l_ref_data = std::array{1., 5., 3.};    // Reference lengths
+    constexpr auto l_data = std::array{2., 4., 6.};        // Current lengths
+
+    const auto k_host = Kokkos::View<const double[3], Kokkos::HostSpace>(k_data.data());
+    const auto l_ref_host = Kokkos::View<const double[3], Kokkos::HostSpace>(l_ref_data.data());
+    const auto l_host = Kokkos::View<const double[3], Kokkos::HostSpace>(l_data.data());
+
+    const auto k_mirror = Kokkos::create_mirror(k);
+    const auto l_ref_mirror = Kokkos::create_mirror(l_ref);
+    const auto l_mirror = Kokkos::create_mirror(l);
+
+    Kokkos::deep_copy(k_mirror, k_host);
+    Kokkos::deep_copy(l_ref_mirror, l_ref_host);
+    Kokkos::deep_copy(l_mirror, l_host);
+
+    Kokkos::deep_copy(k, k_mirror);
+    Kokkos::deep_copy(l_ref, l_ref_mirror);
+    Kokkos::deep_copy(l, l_mirror);
+
+    Kokkos::parallel_for(
+        "CalculateForceCoefficients", 3, CalculateForceCalculationCoefficients{c1, c2, k, l_ref, l}
+    );
+
+    constexpr auto c1_exact_data = std::array{
+        100. * (1. / 2. - 1.),  // -50
+        200. * (5. / 4. - 1.),  // 50
+        300. * (3. / 6. - 1.)   // -150
+    };
+    constexpr auto c2_exact_data = std::array{
+        100. * 1. / (2. * 2. * 2.),  // 12.5
+        200. * 5. / (4. * 4. * 4.),  // 3.125
+        300. * 3. / (6. * 6. * 6.)   // 1.389
+    };
+
+    const auto c1_exact = Kokkos::View<const double[3], Kokkos::HostSpace>(c1_exact_data.data());
+    const auto c2_exact = Kokkos::View<const double[3], Kokkos::HostSpace>(c2_exact_data.data());
+
+    const auto c1_result = Kokkos::create_mirror(c1);
+    const auto c2_result = Kokkos::create_mirror(c2);
+    Kokkos::deep_copy(c1_result, c1);
+    Kokkos::deep_copy(c2_result, c2);
+
+    for (int i = 0; i < 3; ++i) {
+        EXPECT_NEAR(c1_result(i), c1_exact(i), 1e-10);
+        EXPECT_NEAR(c2_result(i), c2_exact(i), 1e-10);
+    }
+}
+
+}  // namespace openturbine::tests
