071f608916e15f9086b128385e3aece6d04f784a
Derek Slaughter <deslaughter@gmail.com>
2025-01-07 12:19:03 -0500

    Fix time calculation in SpringMassSystemTest.FinalDisplacement test so that middle and end displacements are closer to theoretical values

--- tests/regression_tests/regression/test_spring_mass_system.cpp
+++ tests/regression_tests/regression/test_spring_mass_system.cpp
@@ -23,11 +23,15 @@ inline auto SetUpSpringMassSystem() {
     // Add two nodes for the spring element
     model.AddNode(
         {0., 0., 0., 1., 0., 0., 0.},  // First node at origin
-        {0., 0., 0., 1., 0., 0., 0.}, {0., 0., 0., 0., 0., 0.}, {0., 0., 0., 0., 0., 0.}
+        {0., 0., 0., 1., 0., 0., 0.},  //
+        {0., 0., 0., 0., 0., 0.},      //
+        {0., 0., 0., 0., 0., 0.}
     );
     model.AddNode(
-        {2., 0., 0., 1., 0., 0., 0.},  // Second node at (2., 0., 0.) initially
-        {0., 0., 0., 1., 0., 0., 0.}, {0., 0., 0., 0., 0., 0.}, {0., 0., 0., 0., 0., 0.}
+        {2., 0., 0., 1., 0., 0., 0.},  // Second node at (2,0,0)
+        {0., 0., 0., 1., 0., 0., 0.},  //
+        {0., 0., 0., 0., 0., 0.},      //
+        {0., 0., 0., 0., 0., 0.}
     );
 
     // No beams
@@ -35,20 +39,26 @@ inline auto SetUpSpringMassSystem() {
     auto beams = CreateBeams(beams_input);
 
     // We need to add a mass element with identity for mass matrix to create a spring-mass system
+    constexpr auto m = 1.;
+    constexpr auto j = 1.;
     constexpr auto mass_matrix = std::array{
-        std::array{1., 0., 0., 0., 0., 0.}, std::array{0., 1., 0., 0., 0., 0.},
-        std::array{0., 0., 1., 0., 0., 0.}, std::array{0., 0., 0., 1., 0., 0.},
-        std::array{0., 0., 0., 0., 1., 0.}, std::array{0., 0., 0., 0., 0., 1.},
+        std::array{m, 0., 0., 0., 0., 0.},  //
+        std::array{0., m, 0., 0., 0., 0.},  //
+        std::array{0., 0., m, 0., 0., 0.},  //
+        std::array{0., 0., 0., j, 0., 0.},  //
+        std::array{0., 0., 0., 0., j, 0.},  //
+        std::array{0., 0., 0., 0., 0., j},
     };
     const auto masses_input =
         MassesInput({MassElement(model.GetNode(1), mass_matrix)}, {0., 0., 0.});
     auto masses = CreateMasses(masses_input);
 
     // Create springs
+    const auto k = 10.;
     const auto springs_input = SpringsInput({SpringElement(
         std::array{model.GetNode(0), model.GetNode(1)},
-        10.,  // Spring stiffness coeff.
-        0.    // Undeformed length
+        k,  // Spring stiffness coeff.
+        0.  // Undeformed length
     )});
     auto springs = CreateSprings(springs_input);
 
@@ -73,40 +83,40 @@ inline auto SetUpSpringMassSystem() {
     );
 
     // The spring-mass system should move periodically between -2 and 2 (position of the second node)
-    // with simple harmonic motion where the time period is 2 * pi * sqrt(m / k) = 1.98691765316 s
-    constexpr double T = 1.98691765316;
+    // with simple harmonic motion where the time period is 2 * pi * sqrt(m / k) = 1.98691765316s
+    const double T = 2. * M_PI / sqrt(k / m);
+
+    constexpr auto num_steps = 1000;
 
     // Set up step parameters
     constexpr bool is_dynamic_solve(true);
     constexpr size_t max_iter(6);
-    constexpr double step_size(T / 1000.);  // 1,000 steps per T
-    constexpr double rho_inf(0.);           // No damping
+    constexpr double rho_inf(0.);                                // No damping
+    const double step_size(T / static_cast<double>(num_steps));  // Calculate step size
     auto parameters = StepParameters(is_dynamic_solve, max_iter, step_size, rho_inf);
 
-    // Run simulation for ~T/2 seconds
-    for (auto time_step = 0U; time_step <= 500; ++time_step) {
-        auto converged = Step(parameters, solver, elements, state, constraints);
-        EXPECT_TRUE(converged);
-    }
-
     auto q = Kokkos::create_mirror(state.q);
-    Kokkos::deep_copy(q, state.q);
-    EXPECT_EQ(q(0, 0), 0.);  // First node is fixed
-    EXPECT_NEAR(
-        q(1, 0), -3.9999201563071107, 1.e-12
-    );  // Second node should have displcement close to -4.0 after T/2 seconds
 
-    // Run simulation for a total of ~T seconds
-    for (auto time_step = 0U; time_step <= 500; ++time_step) {
+    // Run simulation for T seconds
+    for (auto time_step = 1U; time_step <= 1000; ++time_step) {
         auto converged = Step(parameters, solver, elements, state, constraints);
         EXPECT_TRUE(converged);
+        // Kokkos::deep_copy(q, state.q);
+        // std::cout << time_step * step_size << " ," << q(1, 0) << '\n';
+        if (time_step == num_steps / 2) {  // T/2
+            Kokkos::deep_copy(q, state.q);
+            EXPECT_EQ(q(0, 0), 0.);  // First node is fixed
+            EXPECT_NEAR(
+                q(1, 0), -3.9999200547674469, 1.e-12
+            );  // Second node should have displacement close to -4.0 after T/2
+        } else if (time_step == num_steps) {  // T
+            Kokkos::deep_copy(q, state.q);
+            EXPECT_EQ(q(0, 0), 0.);  // First node is fixed
+            EXPECT_NEAR(
+                q(1, 0), -8.0949125285126051e-05, 1.e-12
+            );  // Second node should have displacement close to 0. after T
+        }
     }
-
-    Kokkos::deep_copy(q, state.q);
-    EXPECT_EQ(q(0, 0), 0.);  // First node is fixed
-    EXPECT_NEAR(
-        q(1, 0), -0.00015948103228367424, 1.e-12
-    );  // Second node should have displcement close to 0. after T seconds
 }
 
 TEST(SpringMassSystemTest, FinalDisplacement) {
