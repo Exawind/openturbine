9d5e588f79e6ab168322f1a60e071011be58e256
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2025-01-11 21:27:44 -0700

    Add annotations to FreedomSignature and related functions to increase readability

--- src/dof_management/freedom_signature.hpp
+++ src/dof_management/freedom_signature.hpp
@@ -6,19 +6,41 @@
 
 namespace openturbine {
 
+/**
+ * @brief Represents the active degrees of freedom for a node or element
+ *
+ * Each bit in the 6-bit signature represents a degree of freedom:
+ * - Bits 5-3: Translational DOFs (x, y, z positions)
+ * - Bits 2-0: Rotational DOFs (rx, ry, rz rotations)
+ *
+ * Binary format: 0b00[Tz][Ty][Tx][Rz][Ry][Rx]
+ */
 enum class FreedomSignature : std::uint8_t {
-    AllComponents = 0b00111111,
-    JustPosition = 0b00111000,
-    JustRotation = 0b00000111,
-    NoComponents = 0b00000000
+    AllComponents = 0b00111111,  //< Enable all 6 degrees of freedom
+    JustPosition = 0b00111000,   //< Enable only translational DOFs
+    JustRotation = 0b00000111,   //< Enable only rotational DOFs
+    NoComponents = 0b00000000    //< Disable all degrees of freedom
 };
 
+/**
+ * @brief Combines two freedom signatures using bitwise OR
+ *
+ * @param x First freedom signature
+ * @param y Second freedom signature
+ * @return Combined freedom signature with all DOFs from both inputs enabled
+ */
 KOKKOS_INLINE_FUNCTION
 FreedomSignature operator|(FreedomSignature x, FreedomSignature y) {
     using T = std::underlying_type_t<FreedomSignature>;
     return static_cast<FreedomSignature>(static_cast<T>(x) | static_cast<T>(y));
 }
 
+/**
+ * @brief Counts the number of active degrees of freedom in a signature
+ *
+ * @param x The freedom signature to count
+ * @return The number of active degrees of freedom
+ */
 KOKKOS_INLINE_FUNCTION
 size_t count_active_dofs(FreedomSignature x) {
     using T = std::underlying_type_t<FreedomSignature>;
