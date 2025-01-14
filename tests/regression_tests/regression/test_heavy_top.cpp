2cb3d8fa837851ad5af91c03076fd6760ee8eb0d
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2025-01-14 12:25:58 -0700

    Take care of the TODO item in heavy top test

--- tests/regression_tests/regression/test_heavy_top.cpp
+++ tests/regression_tests/regression/test_heavy_top.cpp
@@ -43,9 +43,11 @@ inline auto SetUpHeavyTopTest() {
                       }}
     );
 
-    // TODO: Make the ground node 3-DOF only
-    // Add ground node and constraints
-    auto ground_node_id = model.AddNode().SetPosition(0., 0., 0., 1., 0., 0., 0.).Build();
+    // Add ground node with 3 DOFs
+    auto ground_node_id =
+        model.AddNode().SetPosition(0., 0., 0.).Build();  // Only translational DOFs
+
+    // Add rigid joint constraint and fixed BC
     model.AddRigidJointConstraint({mass_node_id, ground_node_id}, {6U, 3U});
     model.AddPrescribedBC(ground_node_id, {6U, 3U});
 
