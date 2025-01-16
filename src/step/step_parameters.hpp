368065ca243cddcd70dfe44601897d5f7cb5cf32
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2025-01-16 15:44:24 -0700

    Fix clang-tidy error

--- src/step/step_parameters.hpp
+++ src/step/step_parameters.hpp
@@ -16,7 +16,10 @@ struct StepParameters {
     double absolute_convergence_tol;
     double relative_convergence_tol;
 
-    StepParameters(bool is_dynamic_solve_, size_t max_iter_, double h_, double rho_inf)
+    StepParameters(
+        bool is_dynamic_solve_, size_t max_iter_, double h_, double rho_inf, double a_tol = 1e-7,
+        double r_tol = 1e-5
+    )
         : is_dynamic_solve(is_dynamic_solve_),
           max_iter(max_iter_),
           h(h_),
@@ -27,8 +30,8 @@ struct StepParameters {
           gamma_prime((is_dynamic_solve) ? gamma / (h * beta) : 0.),
           beta_prime((is_dynamic_solve) ? (1. - alpha_m) / (h * h * beta * (1. - alpha_f)) : 0.),
           conditioner(beta * h * h),
-          absolute_convergence_tol(1e-7),
-          relative_convergence_tol(1e-5) {}
+          absolute_convergence_tol(a_tol),
+          relative_convergence_tol(r_tol) {}
 };
 
 }  // namespace openturbine
