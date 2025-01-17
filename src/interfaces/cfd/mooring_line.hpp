f8fb8b935e7229a0fb0a1134c4e5ed7d3de507d2
Derek Slaughter <deslaughter@gmail.com>
2025-01-17 16:10:17 -0500

    clang-tidy fixes

--- src/interfaces/cfd/mooring_line.hpp
+++ src/interfaces/cfd/mooring_line.hpp
@@ -12,13 +12,13 @@ struct MooringLine {
     NodeData anchor_node;
 
     /// @brief Fixed constraint identifier for the anchor node
-    size_t fixed_constraint_id;
+    size_t fixed_constraint_id{0U};
 
     /// @brief Rigid constraint identifier between fairlead and platform nodes
-    size_t rigid_constraint_id;
+    size_t rigid_constraint_id{0U};
 
     /// @brief Spring element identifier between fairlead and anchor nodes
-    size_t spring_element_id;
+    size_t spring_element_id{0U};
 };
 
 }  // namespace openturbine::cfd
