d06def0a1c264879548009b5a4d38025edf9868b
Derek Slaughter <deslaughter@gmail.com>
2025-02-07 17:55:15 -0500

    clang-tidy fixes

--- tests/unit_tests/elements/beams/test_beam_quadrature.cpp
+++ tests/unit_tests/elements/beams/test_beam_quadrature.cpp
@@ -4,44 +4,26 @@
 
 namespace openturbine::tests {
 
-TEST(BeamQuadratureTest, CheckCreateTrapezoidalQuadrature) {
-    struct TestData {
-        std::vector<double> grid;
-        BeamQuadrature q_exp;
+TEST(BeamQuadratureTest, CheckCreateTrapezoidalQuadrature_1) {
+    const auto q_act = CreateTrapezoidalQuadrature({0., 0.2, 0.4, 0.6, 0.8, 1.0});
+    const BeamQuadrature q_exp{
+        {-1., 0.2}, {-0.6, 0.4}, {-0.2, 0.4}, {0.2, 0.4}, {0.6, 0.4}, {1., 0.2},
     };
+    for (auto i = 0U; i < q_exp.size(); ++i) {
+        for (auto j = 0U; j < 2U; ++j) {
+            EXPECT_NEAR(q_act[i][j], q_exp[i][j], 1e-14);
+        }
+    }
+}
 
-    const std::vector<TestData> test_data{
-        {
-            {0., 0.2, 0.4, 0.6, 0.8, 1.0},  // Grid
-            {
-                {-1., 0.2},
-                {-0.6, 0.4},
-                {-0.2, 0.4},
-                {0.2, 0.4},
-                {0.6, 0.4},
-                {1., 0.2},
-            },  // Quadrature
-        },
-        {
-            {-5., -3., -1., 0., 3., 4., 5.},  // Grid
-            {
-                {-1., 0.2},
-                {-0.6, 0.4},
-                {-0.2, 0.3},
-                {0., 0.4},
-                {0.6, 0.4},
-                {0.8, 0.2},
-                {1., 0.1},
-            },  // Quadrature
-        },
+TEST(BeamQuadratureTest, CheckCreateTrapezoidalQuadrature_2) {
+    const auto q_act = CreateTrapezoidalQuadrature({-5., -3., -1., 0., 3., 4., 5.});
+    const BeamQuadrature q_exp{
+        {-1., 0.2}, {-0.6, 0.4}, {-0.2, 0.3}, {0., 0.4}, {0.6, 0.4}, {0.8, 0.2}, {1., 0.1},
     };
-
-    for (const TestData& td : test_data) {
-        const auto q_act = CreateTrapezoidalQuadrature(td.grid);
-        for (auto i = 0U; i < td.q_exp.size(); ++i) {
-            for (auto j = 0U; j < 2U; ++j) {
-                EXPECT_NEAR(q_act[i][j], td.q_exp[i][j], 1e-14);
-            }
+    for (auto i = 0U; i < q_exp.size(); ++i) {
+        for (auto j = 0U; j < 2U; ++j) {
+            EXPECT_NEAR(q_act[i][j], q_exp[i][j], 1e-14);
         }
     }
 }
