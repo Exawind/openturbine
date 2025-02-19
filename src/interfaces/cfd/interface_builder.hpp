d2aefee03fc911b2b701036b98236569492de126
dcdemen <dcdemen@sandia.gov>
2025-02-19 16:15:18 -0700

    Addressed formatting feedback

--- src/interfaces/cfd/interface_builder.hpp
+++ src/interfaces/cfd/interface_builder.hpp
@@ -9,122 +9,96 @@ namespace openturbine::cfd {
 
 struct InterfaceBuilder {
     InterfaceBuilder& SetGravity(const std::array<double, 3>& gravity) {
-        interface_in.gravity = gravity;
+        interface_input.gravity = gravity;
         return *this;
     }
 
     InterfaceBuilder& SetMaximumNonlinearIterations(size_t max_iter) {
-        interface_in.max_iter = max_iter;
+        interface_input.max_iter = max_iter;
         return *this;
     }
 
     InterfaceBuilder& SetTimeStep(double time_step) {
-        interface_in.time_step = time_step;
+        interface_input.time_step = time_step;
         return *this;
     }
 
     InterfaceBuilder& SetDampingFactor(double rho_inf) {
-        interface_in.rho_inf = rho_inf;
+        interface_input.rho_inf = rho_inf;
         return *this;
     }
 
     InterfaceBuilder& EnableFloatingPlatform(bool enable) {
-        interface_in.turbine.floating_platform.enable = enable;
+        interface_input.turbine.floating_platform.enable = enable;
         return *this;
     }
 
-    InterfaceBuilder& SetFloatingPlatformPosition(const std::array<double, 7>& position) {
-        interface_in.turbine.floating_platform.position = position;
+    InterfaceBuilder& SetFloatingPlatformPosition(const std::array<double, 7>& p) {
+        interface_input.turbine.floating_platform.position = p;
         return *this;
     }
 
-    InterfaceBuilder& SetFloatingPlatformVelocity(const std::array<double, 6>& velocity) {
-        interface_in.turbine.floating_platform.velocity = velocity;
+    InterfaceBuilder& SetFloatingPlatformVelocity(const std::array<double, 6>& v) {
+        interface_input.turbine.floating_platform.velocity = v;
         return *this;
     }
 
-    InterfaceBuilder& SetFloatingPlatformAcceleration(const std::array<double, 6>& acceleration) {
-        interface_in.turbine.floating_platform.acceleration = acceleration;
+    InterfaceBuilder& SetFloatingPlatformAcceleration(const std::array<double, 6>& a) {
+        interface_input.turbine.floating_platform.acceleration = a;
         return *this;
     }
 
     InterfaceBuilder& SetFloatingPlatformMassMatrix(
         const std::array<std::array<double, 6>, 6>& mass_matrix
     ) {
-        interface_in.turbine.floating_platform.mass_matrix = mass_matrix;
+        interface_input.turbine.floating_platform.mass_matrix = mass_matrix;
         return *this;
     }
 
-    InterfaceBuilder& SetNumberOfMooringLines(size_t number_of_mooring_lines) {
-        interface_in.turbine.floating_platform.mooring_lines.resize(number_of_mooring_lines);
+    InterfaceBuilder& SetNumberOfMooringLines(size_t number) {
+        interface_input.turbine.floating_platform.mooring_lines.resize(number);
         return *this;
     }
 
-    InterfaceBuilder& SetMooringLineStiffness(size_t line_number, double stiffness) {
-        interface_in.turbine.floating_platform.mooring_lines[line_number].stiffness = stiffness;
+    InterfaceBuilder& SetMooringLineStiffness(size_t line, double stiffness) {
+        interface_input.turbine.floating_platform.mooring_lines[line].stiffness = stiffness;
         return *this;
     }
 
-    InterfaceBuilder& SetMooringLineUndeformedLength(size_t line_number, double length) {
-        interface_in.turbine.floating_platform.mooring_lines[line_number].undeformed_length = length;
+    InterfaceBuilder& SetMooringLineUndeformedLength(size_t line, double length) {
+        interface_input.turbine.floating_platform.mooring_lines[line].undeformed_length = length;
         return *this;
     }
 
-    InterfaceBuilder& SetMooringLineFairleadPosition(
-        size_t line_number, const std::array<double, 3>& position
-    ) {
-        interface_in.turbine.floating_platform.mooring_lines[line_number].fairlead_position =
-            position;
+    InterfaceBuilder& SetMooringLineFairleadPosition(size_t line, const std::array<double, 3>& p) {
+        interface_input.turbine.floating_platform.mooring_lines[line].fairlead_position = p;
         return *this;
     }
 
-    InterfaceBuilder& SetMooringLineFairleadVelocity(
-        size_t line_number, const std::array<double, 3>& velocity
-    ) {
-        interface_in.turbine.floating_platform.mooring_lines[line_number].fairlead_velocity =
-            velocity;
+    InterfaceBuilder& SetMooringLineAnchorPosition(size_t line, const std::array<double, 3>& p) {
+        interface_input.turbine.floating_platform.mooring_lines[line].anchor_position = p;
         return *this;
     }
 
-    InterfaceBuilder& SetMooringLineFairleadAcceleration(
-        size_t line_number, const std::array<double, 3>& acceleration
-    ) {
-        interface_in.turbine.floating_platform.mooring_lines[line_number].fairlead_acceleration =
-            acceleration;
+    InterfaceBuilder& SetMooringLineAnchorVelocity(size_t line, const std::array<double, 3>& v) {
+        interface_input.turbine.floating_platform.mooring_lines[line].anchor_velocity = v;
         return *this;
     }
 
-    InterfaceBuilder& SetMooringLineAnchorPosition(
-        size_t line_number, const std::array<double, 3>& position
-    ) {
-        interface_in.turbine.floating_platform.mooring_lines[line_number].anchor_position = position;
-        return *this;
-    }
-
-    InterfaceBuilder& SetMooringLineAnchorVelocity(
-        size_t line_number, const std::array<double, 3>& velocity
-    ) {
-        interface_in.turbine.floating_platform.mooring_lines[line_number].anchor_velocity = velocity;
-        return *this;
-    }
-
-    InterfaceBuilder& SetMooringLineAnchorAcceleration(
-        size_t line_number, const std::array<double, 3>& acceleration
-    ) {
-        interface_in.turbine.floating_platform.mooring_lines[line_number].anchor_acceleration =
-            acceleration;
+    InterfaceBuilder& SetMooringLineAnchorAcceleration(size_t line, const std::array<double, 3>& a) {
+        interface_input.turbine.floating_platform.mooring_lines[line].anchor_acceleration = a;
         return *this;
     }
 
     InterfaceBuilder& SetTurbine(const TurbineInput& turbine_in) {
-        interface_in.turbine = turbine_in;
+        interface_input.turbine = turbine_in;
         return *this;
     }
 
-    [[nodiscard]] Interface Build() const { return Interface(interface_in); }
+    [[nodiscard]] Interface Build() const { return Interface(interface_input); }
 
 private:
-    InterfaceInput interface_in;
+    InterfaceInput interface_input;
 };
 
 }  // namespace openturbine::cfd
