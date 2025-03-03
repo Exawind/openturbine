0b13ffd5fc016a3ac2713b0f00b9d26f25c968ee
dcdemen <dcdemen@sandia.gov>
2025-03-03 16:13:37 -0700

    More clean up

--- src/solver/create_full_matrix.hpp
+++ src/solver/create_full_matrix.hpp
@@ -36,29 +36,6 @@ template <typename GlobalCrsMatrixType>
     using KernelHandle = typename KokkosKernels::Experimental::KokkosKernelsHandle<
         typename CrsMatrixType::const_size_type, typename CrsMatrixType::const_ordinal_type,
         typename CrsMatrixType::const_value_type, ExecutionSpace, MemorySpace, MemorySpace>;
-    //    KernelHandle spc_spadd_handle;
-    //    KernelHandle full_system_spadd_handle;
-
-    //    const auto constraints_matrix = CreateBMatrix<CrsMatrixType>(num_system_dofs,
-    //    num_constraint_dofs, constraint_type, base_node_freedom_table, target_node_freedom_table,
-    //    row_range, base_node_col_range, target_node_col_range); const auto B_t =
-    //    CreateBtMatrix<CrsMatrixType>(num_system_dofs, num_constraint_dofs, constraint_type,
-    //    base_node_freedom_table, target_node_freedom_table, row_range, base_node_col_range,
-    //    target_node_col_range); const auto system_matrix =
-    //    CreateKMatrix<CrsMatrixType>(num_system_dofs, node_freedom_allocation_table,
-    //    node_freedom_map_table, num_nodes_per_element, node_state_indices); const auto
-    //    system_matrix_full = CreateSystemMatrixFull(num_system_dofs, num_dofs, system_matrix);
-    //    const auto constraints_matrix_full = CreateConstraintsMatrixFull(num_system_dofs, num_dofs,
-    //    constraints_matrix); const auto transpose_matrix_full =
-    //    CreateTransposeMatrixFull(num_system_dofs, num_dofs, B_t); const auto
-    //    system_plus_constraints =
-    // CreateMatrixSpadd(CreateSystemMatrixFull(num_system_dofs, num_dofs, system_matrix),
-    //                                         CreateConstraintsMatrixFull(num_system_dofs, num_dofs,
-    //                                         constraints_matrix), spc_spadd_handle);
-
-    //    auto comm = Teuchos::createSerialComm<LocalOrdinalType>();
-    //    auto row_map = ;
-    //    auto col_map = Tpetra::createLocalMap<LocalOrdinalType, GlobalOrdinalType>(num_dofs, comm);
 
     // clang-format off
     return Teuchos::make_rcp<GlobalCrsMatrixType>(
