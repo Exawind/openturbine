27f85e360d0748420c7d7b6f1b630b5518ff1626
dcdemen <dcdemen@sandia.gov>
2025-03-05 14:41:00 -0700

    Revert "Prototyped "step builder" approach to the interface builder for feedback"
    This reverts commit 1447cbc88e86bdf9509c6b24afdf14e28b1f9716.

--- src/interfaces/cfd/interface_builder.hpp
+++ src/interfaces/cfd/interface_builder.hpp
@@ -6,82 +6,8 @@
 #include "mooring_line_input.hpp"
 
 namespace openturbine::cfd {
+
 struct InterfaceBuilder {
-    struct FloatingPlatformBuilder {
-        FloatingPlatformBuilder& SetPosition(const std::array<double, 7>& p) {
-            i_builder->interface_input.turbine.floating_platform.position = p;
-            return *this;
-        }
-
-        FloatingPlatformBuilder& SetVelocity(const std::array<double, 6>& v) {
-            i_builder->interface_input.turbine.floating_platform.velocity = v;
-            return *this;
-        }
-
-        FloatingPlatformBuilder& SetAcceleration(const std::array<double, 6>& a) {
-            i_builder->interface_input.turbine.floating_platform.acceleration = a;
-            return *this;
-        }
-
-        FloatingPlatformBuilder& SetMassMatrix(
-            const std::array<std::array<double, 6>, 6>& mass_matrix
-        ) {
-            i_builder->interface_input.turbine.floating_platform.mass_matrix = mass_matrix;
-            return *this;
-        }
-
-        [[nodiscard]] InterfaceBuilder& EndFloatingPlatform() const {
-            i_builder->interface_input.turbine.floating_platform.enable = true;
-            return *i_builder;
-        }
-
-        InterfaceBuilder* i_builder;
-    };
-
-    struct MooringLineBuilder {
-        MooringLineBuilder& SetStiffness(double stiffness) {
-            i_builder->interface_input.turbine.floating_platform.mooring_lines.back().stiffness =
-                stiffness;
-            return *this;
-        }
-
-        MooringLineBuilder& SetUndeformedLength(double length) {
-            i_builder->interface_input.turbine.floating_platform.mooring_lines.back()
-                .undeformed_length = length;
-            return *this;
-        }
-
-        MooringLineBuilder& SetFairleadPosition(const std::array<double, 3>& p) {
-            i_builder->interface_input.turbine.floating_platform.mooring_lines.back()
-                .fairlead_position = p;
-            return *this;
-        }
-
-        MooringLineBuilder& SetAnchorPosition(const std::array<double, 3>& p) {
-            i_builder->interface_input.turbine.floating_platform.mooring_lines.back()
-                .anchor_position = p;
-            return *this;
-        }
-
-        MooringLineBuilder& SetAnchorVelocity(const std::array<double, 3>& v) {
-            i_builder->interface_input.turbine.floating_platform.mooring_lines.back()
-                .anchor_velocity = v;
-            return *this;
-        }
-
-        MooringLineBuilder& SetAnchorAcceleration(const std::array<double, 3>& a) {
-            i_builder->interface_input.turbine.floating_platform.mooring_lines.back()
-                .anchor_acceleration = a;
-            return *this;
-        }
-
-        [[nodiscard]] InterfaceBuilder& EndMooringLine() const { return *i_builder; }
-
-        InterfaceBuilder* i_builder;
-    };
-
-    InterfaceBuilder() : interface_input{}, fp_builder{this}, ml_builder{this} {}
-
     InterfaceBuilder& SetGravity(const std::array<double, 3>& gravity) {
         interface_input.gravity = gravity;
         return *this;
@@ -95,13 +21,6 @@ struct InterfaceBuilder {
     InterfaceBuilder& SetTimeStep(double time_step) {
         interface_input.time_step = time_step;
         return *this;
-    }
-
-    FloatingPlatformBuilder& StartFloatingPlatform() { return fp_builder; }
-
-    MooringLineBuilder& AddMooringLine() {
-        interface_input.turbine.floating_platform.mooring_lines.push_back({});
-        return ml_builder;
     }
 
     InterfaceBuilder& SetDampingFactor(double rho_inf) {
@@ -171,17 +90,15 @@ struct InterfaceBuilder {
         return *this;
     }
 
+    InterfaceBuilder& SetTurbine(const TurbineInput& turbine_in) {
+        interface_input.turbine = turbine_in;
+        return *this;
+    }
+
     [[nodiscard]] Interface Build() const { return Interface(interface_input); }
 
-    friend FloatingPlatformBuilder;
-    friend MooringLineBuilder;
-
-protected:
-    InterfaceInput interface_input;
-
 private:
-    FloatingPlatformBuilder fp_builder;
-    MooringLineBuilder ml_builder;
+    InterfaceInput interface_input;
 };
 
 }  // namespace openturbine::cfd
