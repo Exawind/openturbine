7f735d1c1df2e08e2233e6e4f66dfb5757ef2c01
dcdemen <dcdemen@sandia.gov>
2025-03-05 07:18:18 -0700

    Added profiling line to ResetSolver

--- src/step/reset_solver.hpp
+++ src/step/reset_solver.hpp
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <Kokkos_Core.hpp>
+#include <Kokkos_Profiling_ScopedRegion.hpp>
 
 #include "solver/solver.hpp"
 
 namespace openturbine {
 
 inline void ResetSolver(Solver& solver) {
+    auto region = Kokkos::Profiling::ScopedRegion("Reset Solver");
     Kokkos::deep_copy(solver.A->getLocalMatrixDevice().values, 0.);
     Kokkos::deep_copy(solver.b->getLocalViewDevice(Tpetra::Access::OverwriteAll), 0.);
 }
