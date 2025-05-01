81e00ff6ef7ecb5807ba857c022ea4f1f90bce24
dcdemen <dcdemen@sandia.gov>
2025-05-01 14:14:45 -0600

    Templated Springs on Kokkos::Device.
    This is in support of the goal to decouple OpenTurbine's choice of execution/memory space from the Kokkos configuration.

--- src/elements/springs/springs.hpp
+++ src/elements/springs/springs.hpp
@@ -11,20 +11,21 @@ namespace openturbine {
  * @brief Contains field variables for spring elements to compute per-element
  * contributions to the residual vector and system/iteration matrix
  */
+template <typename DeviceType>
 struct Springs {
     size_t num_elems;  //< Total number of elements
 
-    Kokkos::View<size_t*> num_nodes_per_element;  //< This is always 2 for springs
-    Kokkos::View<size_t* [2]> node_state_indices;
-    Kokkos::View<FreedomSignature* [2]> element_freedom_signature;
-    Kokkos::View<size_t* [2][3]> element_freedom_table;  //< Only translational DOFs for springs
+    Kokkos::View<size_t*, DeviceType> num_nodes_per_element;  //< This is always 2 for springs
+    Kokkos::View<size_t* [2], DeviceType> node_state_indices;
+    Kokkos::View<FreedomSignature* [2], DeviceType> element_freedom_signature;
+    Kokkos::View<size_t* [2][3], DeviceType> element_freedom_table;  //< Only translational DOFs for springs
 
-    Kokkos::View<double* [3]> x0;  //< Initial distance vector between nodes
-    Kokkos::View<double*> l_ref;   //< Initial length of springs
-    Kokkos::View<double*> k;       //< Spring stiffness coefficients
+    Kokkos::View<double* [3], DeviceType> x0;  //< Initial distance vector between nodes
+    Kokkos::View<double*, DeviceType> l_ref;   //< Initial length of springs
+    Kokkos::View<double*, DeviceType> k;       //< Spring stiffness coefficients
 
-    Kokkos::View<double* [2][3]> residual_vector_terms;
-    Kokkos::View<double* [2][2][3][3]> stiffness_matrix_terms;
+    Kokkos::View<double* [2][3], DeviceType> residual_vector_terms;
+    Kokkos::View<double* [2][2][3][3], DeviceType> stiffness_matrix_terms;
 
     explicit Springs(const size_t n_spring_elems)
         : num_elems(n_spring_elems),
--- src/model/model.hpp
+++ src/model/model.hpp
@@ -301,8 +301,9 @@ public:
     [[nodiscard]] size_t NumSpringElements() const { return this->spring_elements_.size(); }
 
     /// Returns Springs struct initialized from spring elements
-    [[nodiscard]] Springs CreateSprings() const {
-        return openturbine::CreateSprings(SpringsInput(this->spring_elements_), this->nodes_);
+    template <typename DeviceType>
+    [[nodiscard]] Springs<DeviceType> CreateSprings() const {
+        return openturbine::CreateSprings<DeviceType>(SpringsInput(this->spring_elements_), this->nodes_);
     }
 
     //--------------------------------------------------------------------------
@@ -315,7 +316,7 @@ public:
         return {
             this->CreateBeams<DeviceType>(),
             this->CreateMasses<DeviceType>(),
-            this->CreateSprings(),
+            this->CreateSprings<DeviceType>(),
         };
     }
 
