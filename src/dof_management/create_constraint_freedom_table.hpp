aa3bbfc00ad00ccc9c4b6e3a34591e36f81349cd
dcdemen <dcdemen@sandia.gov>
2025-05-08 13:11:39 -0600

    Fix CreateConstraintFreedomTable to work on host

--- src/dof_management/create_constraint_freedom_table.hpp
+++ src/dof_management/create_constraint_freedom_table.hpp
@@ -8,14 +8,15 @@
 
 namespace openturbine {
 
+template <typename DeviceType>
 struct CreateConstraintFreedomTable {
-    Kokkos::View<ConstraintType*>::const_type type;
-    Kokkos::View<size_t*>::const_type target_node_index;
-    Kokkos::View<size_t*>::const_type base_node_index;
-    Kokkos::View<size_t*>::const_type active_dofs;
-    Kokkos::View<size_t*>::const_type node_freedom_map_table;
-    Kokkos::View<size_t* [6]> target_node_freedom_table;
-    Kokkos::View<size_t* [6]> base_node_freedom_table;
+    typename Kokkos::View<ConstraintType*, DeviceType>::const_type type;
+    typename Kokkos::View<size_t*, DeviceType>::const_type target_node_index;
+    typename Kokkos::View<size_t*, DeviceType>::const_type base_node_index;
+    typename Kokkos::View<size_t*, DeviceType>::const_type active_dofs;
+    typename Kokkos::View<size_t*, DeviceType>::const_type node_freedom_map_table;
+    Kokkos::View<size_t* [6], DeviceType> target_node_freedom_table;
+    Kokkos::View<size_t* [6], DeviceType> base_node_freedom_table;
 
     KOKKOS_FUNCTION
     void operator()(size_t i) const {
@@ -45,8 +46,8 @@ inline void create_constraint_freedom_table(
     Constraints<DeviceType>& constraints, const State<DeviceType>& state
 ) {
     Kokkos::parallel_for(
-        "Create Constraint Node Freedom Table", constraints.num_constraints,
-        CreateConstraintFreedomTable{
+        "Create Constraint Node Freedom Table", Kokkos::RangePolicy<typename DeviceType::execution_space>(0, constraints.num_constraints),
+        CreateConstraintFreedomTable<DeviceType>{
             constraints.type, constraints.target_node_index, constraints.base_node_index,
             state.active_dofs, state.node_freedom_map_table, constraints.target_node_freedom_table,
             constraints.base_node_freedom_table
