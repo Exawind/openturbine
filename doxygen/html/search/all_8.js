369382bf907e04a4a71f995c0224f6a6654abea6
ddement <ddement@users.noreply.github.com>
2025-05-15 13:53:20 +0000

    deploy: f5cf8c65957b8a5df4bacc0709593b7ee52f38cc

--- doxygen/html/search/all_8.js
+++ doxygen/html/search/all_8.js
@@ -5,15 +5,11 @@ var searchData=
   ['has_5fsymbol_2',['has_symbol',['../classopenturbine_1_1util_1_1dylib.html#a268b929e73c218571162e6baf867237b',1,'openturbine::util::dylib::has_symbol(const char *symbol_name) const noexcept'],['../classopenturbine_1_1util_1_1dylib.html#abe184bea79af69518e02ed934b08bc86',1,'openturbine::util::dylib::has_symbol(const std::string &amp;symbol) const noexcept']]],
   ['hh_5fvel_3',['hh_vel',['../structopenturbine_1_1util_1_1TurbineData.html#a445f611ba7b171ba7eb57afb2ac39033',1,'openturbine::util::TurbineData']]],
   ['horizontal_5fwind_5fspeed_4',['horizontal_wind_speed',['../structopenturbine_1_1util_1_1ControllerIO.html#a06e9882b23fcf85073284b2616b8f395',1,'openturbine::util::ControllerIO']]],
-  ['host_5ff_5',['host_f',['../structopenturbine_1_1State.html#ae829fbd677276a053a30084c68ae495d',1,'openturbine::State']]],
-  ['host_5finput_6',['host_input',['../structopenturbine_1_1Constraints.html#a10a8ce1ea59ca89d15ad28b74af89450',1,'openturbine::Constraints']]],
-  ['host_5foutput_7',['host_output',['../structopenturbine_1_1Constraints.html#a9a6ec52bf722b30770f62d2cf6e85071',1,'openturbine::Constraints']]],
+  ['host_5finput_5',['host_input',['../structopenturbine_1_1Constraints.html#a10a8ce1ea59ca89d15ad28b74af89450',1,'openturbine::Constraints']]],
+  ['host_5foutput_6',['host_output',['../structopenturbine_1_1Constraints.html#a9a6ec52bf722b30770f62d2cf6e85071',1,'openturbine::Constraints']]],
+  ['host_5fstate_7',['host_state',['../classopenturbine_1_1cfd_1_1Interface.html#a6c12ace3e706791a2181a5cb733a18ce',1,'openturbine::cfd::Interface']]],
   ['host_5fstate_2ehpp_8',['host_state.hpp',['../host__state_8hpp.html',1,'']]],
-  ['host_5fstate_5fq_9',['host_state_q',['../classopenturbine_1_1cfd_1_1Interface.html#a16a375f25888ff6f3a880b7e10e5dbb2',1,'openturbine::cfd::Interface']]],
-  ['host_5fstate_5fv_10',['host_state_v',['../classopenturbine_1_1cfd_1_1Interface.html#a0554a17000eead27c42b864eb152d5bc',1,'openturbine::cfd::Interface']]],
-  ['host_5fstate_5fvd_11',['host_state_vd',['../classopenturbine_1_1cfd_1_1Interface.html#ae7905d0a8f1d707e61d10df6eb8681f8',1,'openturbine::cfd::Interface']]],
-  ['host_5fstate_5fx_12',['host_state_x',['../classopenturbine_1_1cfd_1_1Interface.html#a8ad4b209ff40e62740ffee4d6cb135ee',1,'openturbine::cfd::Interface']]],
-  ['hoststate_13',['hoststate',['../structopenturbine_1_1interfaces_1_1HostState.html',1,'openturbine::interfaces::HostState'],['../structopenturbine_1_1interfaces_1_1HostState.html#afb3eff409026205365633a34e0f3e73e',1,'openturbine::interfaces::HostState::HostState()']]],
-  ['hub_14',['hub',['../structopenturbine_1_1util_1_1TurbineData.html#a7b561da2d208abbfb520fb55b0334ec6',1,'openturbine::util::TurbineData']]],
-  ['hub_5finitial_5fposition_15',['hub_initial_position',['../structopenturbine_1_1util_1_1TurbineConfig.html#a7b553dc41a7e5af7ed4f466755cb0c18',1,'openturbine::util::TurbineConfig']]]
+  ['hoststate_9',['hoststate',['../structopenturbine_1_1interfaces_1_1HostState.html',1,'openturbine::interfaces::HostState'],['../structopenturbine_1_1interfaces_1_1HostState.html#afb3eff409026205365633a34e0f3e73e',1,'openturbine::interfaces::HostState::HostState()']]],
+  ['hub_10',['hub',['../structopenturbine_1_1util_1_1TurbineData.html#a7b561da2d208abbfb520fb55b0334ec6',1,'openturbine::util::TurbineData']]],
+  ['hub_5finitial_5fposition_11',['hub_initial_position',['../structopenturbine_1_1util_1_1TurbineConfig.html#a7b553dc41a7e5af7ed4f466755cb0c18',1,'openturbine::util::TurbineConfig']]]
 ];
--- doxygen/html/search/variables_7.js
+++ doxygen/html/search/variables_7.js
@@ -1,16 +1,12 @@
 var searchData=
 [
-  ['h_0',['h',['../structopenturbine_1_1CalculateDisplacement.html#a2759bbcee6edde5d3d4422f47873acf2',1,'openturbine::CalculateDisplacement::h'],['../structopenturbine_1_1CalculateNextState.html#ad244d83f21cf2d8a5ff64aa3bc8e74ff',1,'openturbine::CalculateNextState::h'],['../structopenturbine_1_1UpdateDynamicPrediction.html#ac935a187a9081742d5d8e3b89c46be21',1,'openturbine::UpdateDynamicPrediction::h'],['../structopenturbine_1_1UpdateStaticPrediction.html#a3c23fe41d2273c72940d84c1e877222f',1,'openturbine::UpdateStaticPrediction::h'],['../structopenturbine_1_1StepParameters.html#a40a84c9729594b734d88ee774de01fe3',1,'openturbine::StepParameters::h'],['../structopenturbine_1_1CalculateTangentOperator.html#a03738eb2a6c3f60ccefdf93482a510ac',1,'openturbine::CalculateTangentOperator::h'],['../structopenturbine_1_1CalculateSystemErrorSumSquares.html#aec8c1afdfd4a91d14517a985ce944b4c',1,'openturbine::CalculateSystemErrorSumSquares::h']]],
+  ['h_0',['h',['../structopenturbine_1_1CalculateSystemErrorSumSquares.html#aec8c1afdfd4a91d14517a985ce944b4c',1,'openturbine::CalculateSystemErrorSumSquares::h'],['../structopenturbine_1_1CalculateDisplacement.html#a2759bbcee6edde5d3d4422f47873acf2',1,'openturbine::CalculateDisplacement::h'],['../structopenturbine_1_1CalculateNextState.html#ad244d83f21cf2d8a5ff64aa3bc8e74ff',1,'openturbine::CalculateNextState::h'],['../structopenturbine_1_1UpdateDynamicPrediction.html#ac935a187a9081742d5d8e3b89c46be21',1,'openturbine::UpdateDynamicPrediction::h'],['../structopenturbine_1_1UpdateStaticPrediction.html#a3c23fe41d2273c72940d84c1e877222f',1,'openturbine::UpdateStaticPrediction::h'],['../structopenturbine_1_1StepParameters.html#a40a84c9729594b734d88ee774de01fe3',1,'openturbine::StepParameters::h'],['../structopenturbine_1_1CalculateTangentOperator.html#a03738eb2a6c3f60ccefdf93482a510ac',1,'openturbine::CalculateTangentOperator::h']]],
   ['handle_1',['handle',['../structopenturbine_1_1Solver.html#aa9d6ffd3cceae22c4ae0b1782650af2c',1,'openturbine::Solver']]],
   ['hh_5fvel_2',['hh_vel',['../structopenturbine_1_1util_1_1TurbineData.html#a445f611ba7b171ba7eb57afb2ac39033',1,'openturbine::util::TurbineData']]],
   ['horizontal_5fwind_5fspeed_3',['horizontal_wind_speed',['../structopenturbine_1_1util_1_1ControllerIO.html#a06e9882b23fcf85073284b2616b8f395',1,'openturbine::util::ControllerIO']]],
-  ['host_5ff_4',['host_f',['../structopenturbine_1_1State.html#ae829fbd677276a053a30084c68ae495d',1,'openturbine::State']]],
-  ['host_5finput_5',['host_input',['../structopenturbine_1_1Constraints.html#a10a8ce1ea59ca89d15ad28b74af89450',1,'openturbine::Constraints']]],
-  ['host_5foutput_6',['host_output',['../structopenturbine_1_1Constraints.html#a9a6ec52bf722b30770f62d2cf6e85071',1,'openturbine::Constraints']]],
-  ['host_5fstate_5fq_7',['host_state_q',['../classopenturbine_1_1cfd_1_1Interface.html#a16a375f25888ff6f3a880b7e10e5dbb2',1,'openturbine::cfd::Interface']]],
-  ['host_5fstate_5fv_8',['host_state_v',['../classopenturbine_1_1cfd_1_1Interface.html#a0554a17000eead27c42b864eb152d5bc',1,'openturbine::cfd::Interface']]],
-  ['host_5fstate_5fvd_9',['host_state_vd',['../classopenturbine_1_1cfd_1_1Interface.html#ae7905d0a8f1d707e61d10df6eb8681f8',1,'openturbine::cfd::Interface']]],
-  ['host_5fstate_5fx_10',['host_state_x',['../classopenturbine_1_1cfd_1_1Interface.html#a8ad4b209ff40e62740ffee4d6cb135ee',1,'openturbine::cfd::Interface']]],
-  ['hub_11',['hub',['../structopenturbine_1_1util_1_1TurbineData.html#a7b561da2d208abbfb520fb55b0334ec6',1,'openturbine::util::TurbineData']]],
-  ['hub_5finitial_5fposition_12',['hub_initial_position',['../structopenturbine_1_1util_1_1TurbineConfig.html#a7b553dc41a7e5af7ed4f466755cb0c18',1,'openturbine::util::TurbineConfig']]]
+  ['host_5finput_4',['host_input',['../structopenturbine_1_1Constraints.html#a10a8ce1ea59ca89d15ad28b74af89450',1,'openturbine::Constraints']]],
+  ['host_5foutput_5',['host_output',['../structopenturbine_1_1Constraints.html#a9a6ec52bf722b30770f62d2cf6e85071',1,'openturbine::Constraints']]],
+  ['host_5fstate_6',['host_state',['../classopenturbine_1_1cfd_1_1Interface.html#a6c12ace3e706791a2181a5cb733a18ce',1,'openturbine::cfd::Interface']]],
+  ['hub_7',['hub',['../structopenturbine_1_1util_1_1TurbineData.html#a7b561da2d208abbfb520fb55b0334ec6',1,'openturbine::util::TurbineData']]],
+  ['hub_5finitial_5fposition_8',['hub_initial_position',['../structopenturbine_1_1util_1_1TurbineConfig.html#a7b553dc41a7e5af7ed4f466755cb0c18',1,'openturbine::util::TurbineConfig']]]
 ];
