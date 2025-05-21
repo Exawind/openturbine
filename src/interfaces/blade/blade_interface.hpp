07a9dc2e928100b4bcd0d80bcd5144443341d84b
faisal-bhuiyan <faisalhbhuiyan@gmail.com>
2025-05-21 15:51:39 -0600

    Use Outputs class in BladeInterface to avoid redundant code

--- src/interfaces/blade/blade_interface.hpp
+++ src/interfaces/blade/blade_interface.hpp
@@ -6,6 +6,7 @@
 #include "interfaces/components/blade_input.hpp"
 #include "interfaces/components/solution_input.hpp"
 #include "interfaces/host_state.hpp"
+#include "interfaces/outputs.hpp"
 #include "model/model.hpp"
 #include "state/clone_state.hpp"
 #include "state/copy_state_data.hpp"
@@ -51,9 +52,9 @@ public:
             // Create output directory if it doesn't exist
             std::filesystem::create_directories(solution_input.output_file_path);
 
-            // Initialize NetCDF writer
-            this->output_writer_ = std::make_unique<util::NodeStateWriter>(
-                solution_input.output_file_path + "/blade_interface.nc", true, blade.nodes.size()
+            // Initialize outputs
+            this->outputs_ = std::make_unique<Outputs>(
+                solution_input.output_file_path + "/blade_interface.nc", blade.nodes.size()
             );
 
             // Write mesh connectivity to YAML file
@@ -90,8 +91,8 @@ public:
         UpdateNodeMotion();
 
         // Write outputs and increment timestep counter
-        if (this->output_writer_) {
-            WriteOutputs();
+        if (this->outputs_) {
+            outputs_->WriteNodeOutputsAtTimestep(this->host_state, this->current_timestep_);
         }
         this->current_timestep_++;
 
@@ -130,7 +131,7 @@ private:
     State state_save;             ///< OpenTurbine class state class for temporarily saving state
     HostState host_state;         ///< Host local copy of node state data
     size_t current_timestep_{0};  ///< Current timestep index
-    std::unique_ptr<util::NodeStateWriter> output_writer_;  ///< NetCDF output writer
+    std::unique_ptr<Outputs> outputs_;  ///< handle to Output for writing to NetCDF
 
     /// @brief  Updates motion data for all nodes (root and blade) in the interface
     void UpdateNodeMotion() {
@@ -142,70 +143,6 @@ private:
         for (auto& node : this->blade.nodes) {
             node.UpdateMotion(this->host_state);
         }
-    }
-
-    //------------------------------------------------------------------------------
-    // Write outputs
-    //------------------------------------------------------------------------------
-    void WriteOutputs() const {
-        const size_t num_blade_nodes = this->blade.nodes.size();
-        std::vector<double> x(num_blade_nodes);
-        std::vector<double> y(num_blade_nodes);
-        std::vector<double> z(num_blade_nodes);
-        std::vector<double> i(num_blade_nodes);
-        std::vector<double> j(num_blade_nodes);
-        std::vector<double> k(num_blade_nodes);
-        std::vector<double> w(num_blade_nodes);
-
-        // write position
-        for (size_t idx = 0; idx < num_blade_nodes; ++idx) {
-            const auto& node = this->blade.nodes[idx];
-            x[idx] = this->host_state.x(node.id, 0);
-            y[idx] = this->host_state.x(node.id, 1);
-            z[idx] = this->host_state.x(node.id, 2);
-            i[idx] = this->host_state.x(node.id, 3);
-            j[idx] = this->host_state.x(node.id, 4);
-            k[idx] = this->host_state.x(node.id, 5);
-            w[idx] = this->host_state.x(node.id, 6);
-        }
-        output_writer_->WriteStateDataAtTimestep(current_timestep_, "x", x, y, z, i, j, k, w);
-
-        // write displacement
-        for (size_t idx = 0; idx < num_blade_nodes; ++idx) {
-            const auto& node = this->blade.nodes[idx];
-            x[idx] = this->host_state.q(node.id, 0);
-            y[idx] = this->host_state.q(node.id, 1);
-            z[idx] = this->host_state.q(node.id, 2);
-            w[idx] = this->host_state.q(node.id, 3);
-            i[idx] = this->host_state.q(node.id, 4);
-            j[idx] = this->host_state.q(node.id, 5);
-            k[idx] = this->host_state.q(node.id, 6);
-        }
-        output_writer_->WriteStateDataAtTimestep(current_timestep_, "u", x, y, z, i, j, k, w);
-
-        // write velocity
-        for (size_t idx = 0; idx < num_blade_nodes; ++idx) {
-            const auto& node = this->blade.nodes[idx];
-            x[idx] = this->host_state.v(node.id, 0);
-            y[idx] = this->host_state.v(node.id, 1);
-            z[idx] = this->host_state.v(node.id, 2);
-            i[idx] = this->host_state.v(node.id, 3);
-            j[idx] = this->host_state.v(node.id, 4);
-            k[idx] = this->host_state.v(node.id, 5);
-        }
-        output_writer_->WriteStateDataAtTimestep(current_timestep_, "v", x, y, z, i, j, k);
-
-        // write acceleration
-        for (size_t idx = 0; idx < num_blade_nodes; ++idx) {
-            const auto& node = this->blade.nodes[idx];
-            x[idx] = this->host_state.vd(node.id, 0);
-            y[idx] = this->host_state.vd(node.id, 1);
-            z[idx] = this->host_state.vd(node.id, 2);
-            i[idx] = this->host_state.vd(node.id, 3);
-            j[idx] = this->host_state.vd(node.id, 4);
-            k[idx] = this->host_state.vd(node.id, 5);
-        }
-        output_writer_->WriteStateDataAtTimestep(current_timestep_, "a", x, y, z, i, j, k);
     }
 };
 
