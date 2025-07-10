0dac028577bb70d1f55d715da130635762e7eac7
dcdemen <dcdemen@sandia.gov>
2025-07-10 11:11:53 -0600

    Change KLU to refactor the matrix rather than factor from scratch on subsequent solves.
    This speeds up the Full Turbine problem by about 30%

--- src/solver/linear_solver/dss_symbolic_klu.hpp
+++ src/solver/linear_solver/dss_symbolic_klu.hpp
@@ -1,8 +1,7 @@
 #pragma once
 
+#include <Kokkos_Core.hpp>
 #include <klu.h>
-
-#include "dss_handle_klu.hpp"
 
 namespace openturbine {
 template <typename CrsMatrixType>
@@ -14,10 +13,15 @@ struct DSSSymbolicFunction<DSSHandle<DSSAlgorithm::KLU>, CrsMatrixType> {
         auto col_inds = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace(), A.graph.entries);
 
         auto*& symbolic = dss_handle.get_symbolic();
+        auto*& numeric = dss_handle.get_numeric();
         auto& common = dss_handle.get_common();
+
         if (symbolic != nullptr) {
             klu_free_symbolic(&symbolic, &common);
         }
+	if (numeric != nullptr) {
+	    klu_free_numeric(&numeric, &common);
+	}
         symbolic =
             klu_analyze(num_rows, const_cast<int*>(row_ptrs.data()), col_inds.data(), &common);
     }
